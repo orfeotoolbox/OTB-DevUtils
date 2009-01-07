e291bfc08b35bf67cbe53347ce42c67d2c9e4fa4
Thomas Feuvrier <thomas.feuvrier@c-s.fr>
2009-01-07 10:38:13 +0100

    ENH: add new RAOUL files config and Validation.py modifications

---
+++ Scripts/raoul-VisualExpress2008.py
@@ -0,0 +1,43 @@
+import sys
+import os
+import platform
+import socket
+
+if __name__ == "__main__":
+        sys.path.append(os.getcwd()+"/OTB-DevUtils/Scripts")
+        try:
+                import Validation
+        except:
+                print 'Impossible to find Validation module (import Validation abort!!)'
+                exit(1)
+        if len(sys.argv) != 2:
+                print "Error  -->   Usage: ", sys.argv[0], " WEEK/WEEKEND"
+                exit(1)
+
+        x=Validation.TestProcessing()
+
+        x.SetOutilsDir("Z:\\Outils")
+        x.SetRunDir("Z:\\Outils")
+        x.SetOtbDataLargeInputDir("Z:\\Outils\\OTB-Data-LargeInput")
+        x.EnableUseOtbDataLargeInput()
+        x.SetSourcesDir("Z:\\Outils")
+#        x.EnableUpdateSources()
+        x.DisableUpdateSources()
+        
+        # -> Active generation makefiles
+        if sys.argv[1] == "WEEKEND":
+                x.DisableBuildExamples()
+                x.DisableTestOTBApplicationsWithInstallOTB()
+                x.DisableGlUseAccel()
+                x.DisableUseVtk()
+                x.EnableGenerateMakefiles()
+        else:
+                x.DisableGenerateMakefiles()
+
+        # List of platform must been tested
+	x.Run("visualExpress2008-static-debug-itk-internal-fltk-internal")
+#        if sys.argv[1] == "WEEKEND":
+#	        x.Run("visual8-static-release-itk-internal-fltk-internal")
+#	        x.Run("visual8-static-debug-itk-external-fltk-external")
+#	        x.Run("visual8-static-release-itk-external-fltk-external")
+
