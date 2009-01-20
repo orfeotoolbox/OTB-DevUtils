f66e072a392fc1270be49722fce14b462b7429ef
Emmanuel Christophe <emmanuel.christophe@orfeo-toolbox.org>
2009-01-20 12:56:09 +0800

    TEST: add cmake configuration files for singaporecrisp

---
+++ Config/Debian-Release-gcc34-singaporecrisp-Wrapping.cmake
@@ -0,0 +1,6 @@
+SET( CMAKE_CXX_COMPILER "/usr/bin/g++-3.4" CACHE STRING "debianRelease" FORCE )
+SET( CMAKE_C_COMPILER "/usr/bin/gcc-3.4" CACHE STRING "debianRelease" FORCE )
+SET( COVERAGE_COMMAND "/usr/bin/gcov-3.4" CACHE STRING "debianRelease" FORCE )
+SET( CMAKE_BUILD_TYPE "Release" CACHE STRING "debianRelease" FORCE )
+SET( OTB_DIR "/home/otbtesting/OTB/OTB-Binary-Nightly-Gcc3.4" CACHE STRING "debianRelease" FORCE )
+
---
+++ Config/Debian-Release-singaporecrisp-Wrapping.cmake
@@ -0,0 +1,3 @@
+SET( CMAKE_BUILD_TYPE "Release" CACHE STRING "debianRelease" FORCE )
+SET( OTB_DIR "/home/otbtesting/OTB/OTB-Binary-Nightly" CACHE STRING "debianRelease" FORCE )
+
