52c09c60de19891942d6567bca24a0d34f1349bb
Emmanuel Christophe <emmanuel.christophe@orfeo-toolbox.org>
2009-01-30 15:30:19 +0800

    TEST: update config singaporecrisp

--- Config/Debian-Release-gcc34-singaporecrisp-Wrapping.cmake
+++ Config/Debian-Release-gcc34-singaporecrisp-Wrapping.cmake
@@ -3,4 +3,4 @@ SET( CMAKE_C_COMPILER "/usr/bin/gcc-3.4" CACHE STRING "debianRelease" FORCE )
 SET( COVERAGE_COMMAND "/usr/bin/gcov-3.4" CACHE STRING "debianRelease" FORCE )
 SET( CMAKE_BUILD_TYPE "Release" CACHE STRING "debianRelease" FORCE )
 SET( OTB_DIR "/home/otbtesting/OTB/OTB-Binary-Nightly-Gcc3.4" CACHE STRING "debianRelease" FORCE )
-
+SET( WRAP_ITK_DIMS "2" CACHE STRING "debianRelease" FORCE )
---
+++ Config/Debian-Release-gcc41-singaporecrisp-Wrapping.cmake
@@ -0,0 +1,6 @@
+SET( CMAKE_CXX_COMPILER "/usr/bin/g++-4.1" CACHE STRING "debianRelease" FORCE )
+SET( CMAKE_C_COMPILER "/usr/bin/gcc-4.1" CACHE STRING "debianRelease" FORCE )
+SET( COVERAGE_COMMAND "/usr/bin/gcov-4.1" CACHE STRING "debianRelease" FORCE )
+SET( CMAKE_BUILD_TYPE "Release" CACHE STRING "debianRelease" FORCE )
+SET( OTB_DIR "/home/otbtesting/OTB/OTB-Binary-Nightly-Gcc4.1" CACHE STRING "debianRelease" FORCE )
+SET( WRAP_ITK_DIMS "2" CACHE STRING "debianRelease" FORCE )
