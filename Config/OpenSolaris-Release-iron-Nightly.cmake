e09e0ca5139b884159c59836b4da388b9c3177fc
Emmanuel Christophe <emmanuel.christophe@orfeo-toolbox.org>
2009-04-24 07:30:41 +0800

    TEST: add configuration for OpenSolaris

---
+++ Config/OpenSolaris-Release-iron-Nightly.cmake
@@ -0,0 +1,9 @@
+SET( CMAKE_BUILD_TYPE "Release" CACHE STRING "openSolaris" FORCE )
+SET( BUILD_TESTING ON CACHE BOOL "openSolaris" FORCE )
+SET( BUILD_EXAMPLES OFF CACHE BOOL "openSolaris" FORCE )
+
+SET( OTB_DATA_ROOT "/export/home/christop/OTB/trunk/OTB-Data" CACHE STRING "openSolaris" FORCE )
+
+SET( MAKECOMMAND "/usr/gnu/bin/make -i -j 2" CACHE STRING "openSolaris" FORCE )
+SET ( OTB_USE_LIBLAS OFF CACHE BOOL "openSolaris" FORCE )
+
