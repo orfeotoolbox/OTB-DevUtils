c565a1f21257fc7a5eee0670a0e306bbcd07a713
Emmanuel Christophe <emmanuel.christophe@orfeo-toolbox.org>
2009-10-14 18:06:05 +0800

    TEST: add monteverdi configuration for nightly tests

---
+++ Config/OpenSolaris-Release-iron-Monteverdi-Nightly.cmake
@@ -0,0 +1,7 @@
+SET( CMAKE_BUILD_TYPE "Release" CACHE STRING "debianRelease" FORCE )
+SET( OTB_DIR "/export/home/christop/OTB/OTB-Binary" CACHE STRING "debianRelease" FORCE )
+SET( OTB_DATA_ROOT "/export/home/christop/OTB/trunk/OTB-Data" CACHE STRING "debianRelease" FORCE )
+SET( BUILD_TESTING "ON" CACHE STRING "debianRelease" FORCE )
+
+SET( MAKECOMMAND "/usr/gnu/bin/make -i -k" CACHE STRING "debianRelease" FORCE )
+
