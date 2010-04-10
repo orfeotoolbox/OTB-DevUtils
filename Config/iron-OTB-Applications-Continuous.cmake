39311bcddec176005902d5f02e842df9df62b1ee
Emmanuel Christophe <emmanuel.christophe@orfeo-toolbox.org>
2010-04-10 15:37:34 +0800

    TEST: switch iron continuous to full ctest scripts

---
+++ Config/iron-OTB-Applications-Continuous.cmake
@@ -0,0 +1,44 @@
+
+SET (CTEST_SOURCE_DIRECTORY "$ENV{HOME}/OTB/trunk/OTB-Applications-Continuous/")
+SET (CTEST_BINARY_DIRECTORY "$ENV{HOME}/OTB/OTB-Binary-Applications-Continuous/")
+
+
+# which ctest command to use for running the dashboard
+SET (CTEST_COMMAND 
+  "ctest -j6 -D Nightly -A $ENV{HOME}/OTB/trunk/OTB-DevUtils/Config/iron-OTB-Applications-Continuous.cmake -V"
+  )
+
+# what cmake command to use for configuring this dashboard
+SET (CTEST_CMAKE_COMMAND 
+  "cmake"
+  )
+
+# should ctest wipe the binary tree before running
+SET (CTEST_START_WITH_EMPTY_BINARY_DIRECTORY TRUE)
+
+# this is the initial cache to use for the binary tree, be careful to escape
+# any quotes inside of this string if you use it
+SET (CTEST_INITIAL_CACHE "
+//Command used to build entire project from the command line.
+MAKECOMMAND:STRING=/usr/bin/make -i -k -j8
+//Name of the build
+BUILDNAME:STRING=zApps-Deb51-64bits-Release
+//Name of the computer/site where compile is being run
+SITE:STRING=iron
+//Data root
+OTB_DATA_ROOT:STRING=/home/otbtesting/OTB/trunk/OTB-Data
+//Compilation options
+CMAKE_C_FLAGS:STRING= -Wall -Wno-uninitialized -Wno-unused-variable
+CMAKE_CXX_FLAGS:STRING= -Wall -Wno-deprecated -Wno-uninitialized -Wno-unused-variable
+//Set up the build options
+CMAKE_BUILD_TYPE:STRING=Release
+OTB_DIR:STRING=/home/otbtesting/OTB/OTB-Binary-Continuous
+BUILD_TESTING:BOOL=ON
+OTB_USE_VTK:BOOL=ON
+")
+
+# set any extra envionment varibles here
+SET (CTEST_ENVIRONMENT
+ "DISPLAY=:0"
+)
+
