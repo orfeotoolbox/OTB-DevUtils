7d2a80317531c10bfc1789b3dcb7458d9a44a7d6
Manuel Grizonnet <manuel.grizonnet@orfeo-toolbox.org>
2010-04-13 10:43:30 +0200

    ADD:otb-app config files for pc-grizonnetm

---
+++ Config/pc-grizonnetm-OTB-Applications-Nightly.cmake
@@ -0,0 +1,49 @@
+
+SET (CTEST_SOURCE_DIRECTORY "/mnt/dd-2/OTB/trunk/OTB-Applications-Nightly/")
+SET (CTEST_BINARY_DIRECTORY "/mnt/dd-2/OTB/OTB-Binary-Applications-Nightly/")
+
+
+# which ctest command to use for running the dashboard
+SET (CTEST_COMMAND 
+  "ctest -j6 -D Nightly -A /mnt/dd-2/OTB/trunk/OTB-DevUtils/Config/pc-grizonnetm-OTB-Applications-Nightly.cmake -V"
+  )
+
+# what cmake command to use for configuring this dashboard
+SET (CTEST_CMAKE_COMMAND 
+  "cmake"
+  )
+
+# should ctest wipe the binary tree before running
+SET (CTEST_START_WITH_EMPTY_BINARY_DIRECTORY TRUE)
+
+# this is the initial cache to use for the binary tree, be careful to escape
+# any quotes inside of this string if you use it
+SET (CTEST_INITIAL_CACHE "
+//Command used to build entire project from the command line.
+MAKECOMMAND:STRING=/usr/bin/make -i -k -j8
+//Name of the build
+BUILDNAME:STRING=zApps-Ubuntu9.10-64bits-Release
+//Name of the computer/site where compile is being run
+SITE:STRING=pc-grizonnetm
+//Data root
+OTB_DATA_ROOT:STRING=/mnt/dd-2/OTB/trunk/OTB-Data
+//Compilation options
+CMAKE_C_FLAGS:STRING= -Wall -Wno-uninitialized -Wno-unused-variable
+CMAKE_CXX_FLAGS:STRING= -Wall -Wno-deprecated -Wno-uninitialized -Wno-unused-variable
+//Set up the build options
+CMAKE_BUILD_TYPE:STRING=Release
+OTB_DIR:STRING=/mnt/dd-2/OTB/OTB-Binary-Nightly
+BUILD_TESTING:BOOL=ON
+OTB_USE_VTK:BOOL=ON
+//CPack configuration
+OTB_USE_CPACK:BOOL=ON
+CMAKE_INSTALL_PREFIX:STRING=/home/otbtesting/OTB/tmp
+CPACK_BINARY_DEB:BOOL=ON
+CPACK_DEBIAN_PACKAGE_ARCHITECTURE:STRING=amd64
+")
+
+# set any extra envionment varibles here
+SET (CTEST_ENVIRONMENT
+ "DISPLAY=:0"
+)
+
