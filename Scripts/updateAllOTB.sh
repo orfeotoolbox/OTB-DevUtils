3d51531bd3754fe51a148954011c5b2fc8e2c43b
Emmanuel Christophe <emmanuel.christophe@orfeo-toolbox.org>
2010-04-19 20:22:07 +0800

    ENH: add argument checking

--- Scripts/updateAllOTB.sh
+++ Scripts/updateAllOTB.sh
@@ -3,6 +3,14 @@
 
 TRUNK_DIR=$1
 
+die () {
+  echo >&2 "$@"
+  exit 1
+}
+
+[ "$#" -eq 1 ] || die "1 argument required, $# provided"
+
+
 for REP in "OTB" "OTB-Applications" "OTB-Data" "OTB-DevUtils" "OTB-Documents" "Monteverdi" "OTB-Wrapping"
 do
   cd $TRUNK_DIR/$REP
