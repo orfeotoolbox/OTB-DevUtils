4a60dd1232c46ab29cf72504782e7579c7d3b394
Manuel Grizonnet <manuel.grizonnet@orfeo-toolbox.org>
2010-06-22 10:18:25 +0200

    ADD:otb nightly with new itk staistics framework on pc-grizonnetm

---
+++ Config/pc-grizonnetm-OTB-Nightly-ITK-Statistics-ON.cmake
@@ -0,0 +1,69 @@
+
+SET (CTEST_SOURCE_DIRECTORY "/mnt/dd-2/OTB/trunk/OTB-Nightly/")
+SET (CTEST_BINARY_DIRECTORY "/mnt/dd-2/OTB/OTB-Binary-Nightly/")
+
+
+# which ctest command to use for running the dashboard
+SET (CTEST_COMMAND 
+  "ctest -j6 -D Nightly -A /mnt/dd-2/OTB/trunk/OTB-DevUtils/Config/pc-grizonnetm-OTB-Nightly-ITK-Statistics.cmake -V"
+  )
+
+# what cmake command to use for configuring this dashboard
+SET (CTEST_CMAKE_COMMAND 
+  "cmake"
+  )
+
+# should ctest wipe the binary tree before running
+SET (CTEST_START_WITH_EMPTY_BINARY_DIRECTORY TRUE)
+
+# this is the initial cache to use for the binary tree, be careful to escape
+# any quotes inside of this string if you use it
+SET (CTEST_INITIAL_CACHE "
+//Command used to build entire project from the command line.
+MAKECOMMAND:STRING=/usr/bin/make -i -k -j8
+//Name of the build
+BUILDNAME:STRING=Ubuntu9.10-64bits-Release-ITK-Statistics-ON
+//Name of the computer/site where compile is being run
+SITE:STRING=pc-grizonnetm
+//Data root
+OTB_DATA_ROOT:STRING=/mnt/dd-2/OTB/trunk/OTB-Data
+//Compilation options
+CMAKE_C_FLAGS:STRING= -Wall -Wno-uninitialized -Wno-unused-variable
+CMAKE_CXX_FLAGS:STRING= -Wall -Wno-deprecated -Wno-uninitialized -Wno-unused-variable
+//Set up the build options
+CMAKE_BUILD_TYPE:STRING=Release
+BUILD_TESTING:BOOL=ON
+BUILD_EXAMPLES:BOOL=ON
+OTB_USE_CURL:BOOL=ON
+OTB_USE_PQXX:BOOL=ON
+OTB_USE_EXTERNAL_BOOST:BOOL=ON
+OTB_USE_EXTERNAL_EXPAT:BOOL=ON
+USE_FFTWD:BOOL=ON
+USE_FFTWF:BOOL=ON
+OTB_GL_USE_ACCEL:BOOL=ON 
+ITK_USE_REVIEW:BOOL=ON 
+
+#ITK statisctics framework
+ITK_USE_REVIEW_STATISTICS:BOOL=ON
+
+ITK_USE_OPTIMIZED_REGISTRATION_METHODS:BOOL=ON 
+OTB_USE_MAPNIK:BOOL=ON 
+MAPNIK_INCLUDE_DIR:STRING=/usr/include
+MAPNIK_LIBRARY:STRING=/usr/lib/libmapnik.so
+//CPack configuration
+OTB_USE_CPACK:BOOL=ON
+CMAKE_INSTALL_PREFIX:STRING=/home/otbtesting/OTB/tmp
+CPACK_BINARY_DEB:BOOL=ON
+CPACK_DEBIAN_PACKAGE_ARCHITECTURE:STRING=amd64
+//Set GDAL options
+GDAL_CONFIG:STRING=/home/grizonnetm/Local/gdal-1.7.1-build/bin/gdal-config
+GDAL_INCLUDE_DIR:STRING=/home/grizonnetm/Local/gdal-1.7.1-build/include
+GDAL_LIBRARY:STRING=/home/grizonnetm/Local/gdal-1.7.1-build/lib/libgdal.so
+OGR_INCLUDE_DIRS:STRING=/home/grizonnetm/Local/gdal-1.7.1-build/include
+")
+
+# set any extra envionment varibles here
+SET (CTEST_ENVIRONMENT
+ "DISPLAY=:0"
+)
+
