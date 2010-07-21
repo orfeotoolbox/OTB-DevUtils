1d0584aa8d8b05a1d825880979e381a9f7b95211
Manuel Grizonnet <manuel.grizonnet@orfeo-toolbox.org>
2010-07-21 17:44:15 +0200

    update config on pc-grizonnetm dev account

--- Config/Ubuntu-Release-pc-inglada-Nightly.cmake
+++ Config/Ubuntu-Release-pc-inglada-Nightly.cmake
@@ -1,5 +1,5 @@
 SET( SITE "pc-grizonnetm" CACHE STRING "ubuntuRelease" FORCE )
-SET( CMAKE_BUILD_TYPE "Release" CACHE STRING "ubuntuRelease" FORCE )
+#SET( CMAKE_BUILD_TYPE "Release" CACHE STRING "ubuntuRelease" FORCE )
 SET( BUILD_TESTING ON CACHE BOOL "ubuntuRelease" FORCE )
 SET( BUILD_EXAMPLES ON CACHE BOOL "ubuntuRelease" FORCE )
 SET( OTB_USE_CURL ON CACHE BOOL "ubuntuRelease" FORCE )
