ca1532dc79aa58fc40ee3cc27ed9c2935cf0450c
Julien Malik <julien.malik@c-s.fr>
2010-07-30 16:02:20 +0200

    BUG: Wrapping do not compile out of the box with cmake 2.8.2

--- Config/leod-OTB-Wrapping-Nightly.cmake
+++ Config/leod-OTB-Wrapping-Nightly.cmake
@@ -44,6 +44,10 @@ CableSwig_DIR:PATH=$ENV{HOME}/OTB-OUTILS/cableswig/binaries-macosx-release-cable
 WRAP_ITK_PYTHON:BOOL=ON
 WRAP_ITK_JAVA:BOOL=ON
 
+# We need the following to be compatible with cmake 2.8.2
+JAVA_ARCHIVE:FILEPATH=/usr/bin/jar
+JAVA_COMPILE:FILEPATH=/usr/bin/javac
+JAVA_RUNTIME:FILEPATH=/usr/bin/java
 ")
 
 SET( PULL_RESULT_FILE "${CTEST_BINARY_DIRECTORY}/pull_result.txt" )
