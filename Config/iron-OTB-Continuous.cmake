8cab52bd1da72e2281205fc7eecabafd187b5079
Emmanuel Christophe <emmanuel.christophe@orfeo-toolbox.org>
2010-08-17 18:10:15 +0800

    ENH: parallel build with cmake 2.8

--- Config/iron-OTB-Continuous.cmake
+++ Config/iron-OTB-Continuous.cmake
@@ -1,17 +1,14 @@
 
+set(CTEST_SITE "iron")
+set(CTEST_BUILD_NAME "Deb51-64bits-Release")
+set(CTEST_BUILD_CONFIGURATION Release)
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set(CTEST_BUILD_FLAGS -j8)
+set(CTEST_TEST_ARGS PARALLEL_LEVEL 6)
+
 SET (CTEST_SOURCE_DIRECTORY "$ENV{HOME}/OTB/trunk/OTB-Continuous/")
 SET (CTEST_BINARY_DIRECTORY "$ENV{HOME}/OTB/OTB-Binary-Continuous/")
 
-
-# which ctest command to use for running the dashboard
-SET (CTEST_COMMAND 
-  "ctest -j6 -D Continuous -A ${CTEST_BINARY_DIRECTORY}/CMakeCache.txt -V"
-  )
-
-# what cmake command to use for configuring this dashboard
-SET (CTEST_CMAKE_COMMAND 
-  "cmake"
-  )
 
 # should ctest wipe the binary tree before running
 SET (CTEST_START_WITH_EMPTY_BINARY_DIRECTORY TRUE)
@@ -19,12 +16,6 @@ SET (CTEST_START_WITH_EMPTY_BINARY_DIRECTORY TRUE)
 # this is the initial cache to use for the binary tree, be careful to escape
 # any quotes inside of this string if you use it
 SET (CTEST_INITIAL_CACHE "
-//Command used to build entire project from the command line.
-MAKECOMMAND:STRING=/usr/bin/make -i -k -j8
-//Name of the build
-BUILDNAME:STRING=Deb51-64bits-Release
-//Name of the computer/site where compile is being run
-SITE:STRING=iron
 //Data root
 OTB_DATA_ROOT:STRING=/home/christop/OTB/trunk/OTB-Data
 //Compilation options
@@ -45,9 +36,4 @@ ITK_USE_REVIEW:BOOL=ON
 ITK_USE_OPTIMIZED_REGISTRATION_METHODS:BOOL=ON 
 OTB_USE_MAPNIK:BOOL=ON 
 ")
-
-# set any extra envionment varibles here
-SET (CTEST_ENVIRONMENT
- "DISPLAY=:0"
-)
 
