1222ff04f068e42a5f3f705cd61f24a3467102aa
Sebastien Dinot <sebastien.dinot@c-s.fr>
2010-12-16 14:23:32 +0100

    ENH: Scripts and configuration files used to create an Ubuntu package for OTB Applications

---
+++ Packaging/ubuntu/OTB-Applications/make_ubuntu_packages.sh
@@ -0,0 +1,240 @@
+#!/bin/bash
+# Script to automate the Orfeo Toolbox library packaging for Ubuntu.
+#
+# Copyright (C) 2010 CNES - Centre National d'Etudes Spatiales
+# by Sebastien DINOT <sebastien.dinot@c-s.fr>
+#
+# OTB Applications are distributed under the CeCILL license version 2. See
+# files Licence_CeCILL_V2-en.txt (english version) or Licence_CeCILL_V2-fr.txt
+# (french version) in 'Copyright' directory for details. This licenses are
+# also available online:
+# http://www.cecill.info/licences/Licence_CeCILL_V2-en.txt
+# http://www.cecill.info/licences/Licence_CeCILL_V2-fr.txt
+
+
+SCRIPT_VERSION="1.0"
+
+export DEBFULLNAME="OTB Team"
+export DEBEMAIL="contact@orfeo-toolbox.org"
+
+TMPDIR="/tmp"
+CMDDIR=`pwd`/`dirname $0`
+DEBDIR=`echo $CMDDIR | sed -e 's,^\./,,;s,/\.\?$,,;s,/\./,/,g'`/debian
+
+
+display_version ()
+{
+    cat <<EOF
+
+make_ubuntu_packages.sh, version ${SCRIPT_VERSION}
+Copyright (C) 2010 CNES (Centre National d'Etudes Spatiales)
+by Sebastien DINOT <sebastien.dinot@c-s.fr>
+
+EOF
+}
+
+
+display_help ()
+{
+    cat <<EOF
+
+This script is used to automate the OTB applications packaging for Ubuntu.
+Source packages are created in ${TMPDIR} directory.
+
+Usage:
+  ./make_ubuntu_packages.sh [options]
+
+Options:
+  -h            Display this short help message.
+
+  -v            Display version and copyright informations.
+
+  -d directory  Top directory of the local repository clone
+
+  -r tag        Revision to extract.
+
+  -o version    External version of the OTB library an applications (ex. 3.8.0)
+
+  -p version    Version of the package (ex. 2)
+
+  -c message    Changelog message
+
+Example:
+  ./make_ubuntu_packages.sh -d ~/otb/src/OTB-Applications -r 1891 -o 3.8-RC1 -p 2
+
+EOF
+}
+
+
+check_src_top_dir ()
+{
+    if [ -z "$topdir" ] ; then
+        echo "*** ERROR: missing top directory of the Mercurial working copy (option -d)"
+        echo "*** Use ./make_ubuntu_packages.sh -h to show command line syntax"
+        exit 3
+    fi
+    if [ ! -d "$topdir" ] ; then
+        echo "*** ERRROR: directory '$topdir' doesn't exist"
+        exit 2
+    fi
+    if [ ! -d "$topdir/.hg" ] ; then
+        echo "*** ERRROR: No Mercurial working copy found in '$topdir' directory"
+        exit 2
+    fi
+    if [ "`hg identify $topdir`" == "000000000000 tip" ] ; then
+        echo "*** ERROR: Mercurial failed to identify a valid repository in '$topdir'"
+        exit 2
+    fi
+    topdir=`( cd $topdir ; pwd )`
+}
+
+
+check_src_revision ()
+{
+    if [ -z "$revision" ] ; then
+        echo "*** ERROR: missing revision identifier of the repository (option -r)"
+        echo "*** Use ./make_ubuntu_packages.sh -h to show command line syntax"
+        exit 3
+    fi
+    olddir=`pwd`
+    cd "$topdir"
+    if ! hg log -r "$revision" &>/dev/null ; then
+        echo "*** ERROR: Revision $revision unknown"
+        exit 2
+    fi
+    cd "$olddir"
+}
+
+
+check_external_version ()
+{
+    if [ -z "$otb_version_full" ] ; then
+        echo "*** ERROR: missing version number of OTB (option -o)"
+        echo "*** Use ./make_ubuntu_packages.sh -h to show command line syntax"
+        exit 3
+    fi
+    if [ "`echo $otb_version_full | sed -e 's/^[0-9]\+\.[0-9]\+\(\.[0-9]\+\|-RC[0-9]\+\)$/OK/'`" != "OK" ] ; then
+        echo "*** ERROR: OTB full version ($otb_version_full) has an unexpected format"
+        exit 3
+    fi
+
+    otb_version_major=`echo $otb_version_full | sed -e 's/^\([0-9]\+\)\..*$/\1/'`
+    otb_version_minor=`echo $otb_version_full | sed -e 's/^[^\.]\+\.\([0-9]\+\)[\.-].*$/\1/'`
+    otb_version_patch=`echo $otb_version_full | sed -e 's/^.*[\.-]\(\(RC\)\?[0-9]\+\)$/\1/'`
+    otb_version_soname="${otb_version_major}.${otb_version_minor}"
+}
+
+
+set_ubuntu_code_name ()
+{
+    case "$1" in
+        "natty" )
+            ubuntu_codename="Natty Narwhal"
+            ubuntu_version="11.04"
+            ;;
+        "maverick" )
+            ubuntu_codename="Maverick Meerkat"
+            ubuntu_version="10.10"
+            ;;
+        "lucid" )
+            ubuntu_codename="Lucid Lynx"
+            ubuntu_version="10.04 LTS"
+            ;;
+        "karmic" )
+            ubuntu_codename="Karmic Koala"
+            ubuntu_version="9.10"
+            ;;
+        "hardy" )
+            ubuntu_codename="Hardy Heron"
+            ubuntu_version="08.04 LTS"
+            ;;
+        * )
+            echo "*** ERROR: Unknown Ubuntu version name"
+            exit 4
+            ;;
+    esac
+}
+
+
+while getopts ":r:d:o:p:c:hv" option
+do
+    case $option in
+        d ) topdir=$OPTARG
+            ;;
+        r ) revision=$OPTARG
+            ;;
+        o ) otb_version_full=$OPTARG
+            ;;
+        p ) pkg_version=$OPTARG
+            ;;
+        c ) changelog_message=$OPTARG
+            ;;
+        v ) display_version
+            exit 0
+            ;;
+        h ) display_help
+            exit 0
+            ;;
+        * ) echo "*** ERROR: Unknown option -$OPTARG (arg #"$(($OPTIND - 1))")"
+            display_help
+            exit 0
+            ;;
+    esac
+done
+
+if [ "$OPTIND" -eq 1 ] ; then
+    display_help
+    exit 1
+fi
+
+echo "Command line checking..."
+check_src_top_dir
+check_src_revision
+check_external_version
+
+# echo "Debian templates directory: $DEBDIR"
+# echo "Working copy directory:     $topdir"
+# echo "OTB version:  $otb_version_full"
+# echo "- Soname version number: $otb_version_soname"
+# echo "- Major version number:  $otb_version_major"
+# echo "- Minor version number:  $otb_version_minor"
+# echo "- Patch version number:  $otb_version_patch"
+
+echo "Archive export..."
+cd "$topdir"
+hg archive -r "$revision" -t tgz "$TMPDIR/otbapp-$otb_version_full.tar.gz"
+
+echo "Archive extraction..."
+cd "$TMPDIR"
+tar xzf "otbapp-$otb_version_full.tar.gz"
+mv "otbapp-$otb_version_full.tar.gz" "otbapp_$otb_version_full.orig.tar.gz"
+
+echo "Debian scripts import..."
+cd "$TMPDIR/otbapp-$otb_version_full"
+cp -a "$DEBDIR" .
+cd debian
+for f in control rules ; do
+    sed -e "s/@OTB_VERSION_MAJOR@/$otb_version_major/g" \
+        -e "s/@OTB_VERSION_MINOR@/$otb_version_minor/g" \
+        -e "s/@OTB_VERSION_PATCH@/$otb_version_patch/g" \
+        -e "s/@OTB_VERSION_SONAME@/$otb_version_soname/g" \
+        -e "s/@OTB_VERSION_FULL@/$otb_version_full/g" \
+        < "$f.in" > "$f"
+    rm -f "$f.in"
+done
+
+echo "Source package generation..."
+cd "$TMPDIR/otbapp-$otb_version_full"
+for target in karmic maverick lucid ; do
+    set_ubuntu_code_name "$target"
+    echo "Package for $ubuntu_codename ($ubuntu_version)"
+    cp -f "$DEBDIR/changelog" debian
+    if [ -n "$changelog_message" ] ; then
+        dch_message="$changelog_message"
+    else
+        dch_message="Automated update for $ubuntu_codename ($ubuntu_version)."
+    fi
+    dch --force-distribution --distribution "$target" \
+        -v "${otb_version_full}-0ppa~${target}${pkg_version}" $dch_message
+    debuild -k0x46047121 -S -sa --lintian-opts -i
+done
