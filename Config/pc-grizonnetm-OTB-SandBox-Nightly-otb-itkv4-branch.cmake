92b8aa814bcaea5ab2fb1803c6dc3e1c5afb67be
Manuel Grizonnet <manuel.grizonnet@orfeo-toolbox.org>
2011-02-10 14:12:18 +0100

    ENH:change configuration on otb-sandbox nightly to use otb-itkv4 branch

--- Config/pc-grizonnetm-OTB-SandBox-Nightly-otb-itkv4-branch.cmake
+++ Config/pc-grizonnetm-OTB-SandBox-Nightly-otb-itkv4-branch.cmake
@@ -5,7 +5,7 @@ SET (CTEST_BINARY_DIRECTORY "/mnt/dd-2/OTB/OTB-Binary-Nightly-ITK-Statistics/")
 
 # which ctest command to use for running the dashboard
 SET (CTEST_COMMAND 
-  "ctest -j6 -D Nightly -A /mnt/dd-2/OTB/trunk/OTB-DevUtils/Config/pc-grizonnetm-OTB-Nightly-ITK-Statistics-ON.cmake -V"
+  "ctest -j6 -D Nightly -A /mnt/dd-2/OTB/trunk/OTB-DevUtils/Config/pc-grizonnetm-OTB-SandBox-Nightly-otb-itkv4-branch.cmake -V"
   )
 
 # what cmake command to use for configuring this dashboard
