512b19335e3ed6d7e95cc925a0891a137efce654
Manuel Grizonnet <manuel.grizonnet@orfeo-toolbox.org>
2011-03-24 14:07:35 +0100

    ENH:update otb witk itkv4 patched version script to be compliant with the new modular architecture

--- Config/pc-grizonnetm-ITK-V4-patched.cmake
+++ Config/pc-grizonnetm-ITK-V4-patched.cmake
@@ -6,7 +6,9 @@ SET (CTEST_BINARY_DIRECTORY "/home/otbtesting/Dashboards/My\ Tests/ITK-patched-b
 # path to the OTB-NewStatistics patch (otb-itkv4 branch)
 # be sure that the script launching this configuration file 
 # update OTB-SandBox directory first to work with the last patch
-SET(OTB_PATCH_PATH "/mnt/dd-2/OTB/trunk/OTB-SandBox/Patch/itkv4_patch.diff")
+SET(OTB_PATCH_PATH "/mnt/dd-2/OTB/trunk/OTB-SandBox/Patch/itkv4_modular_patch.diff")
+
+
 
 # cmake and git commands
 SET (CTEST_CMAKE_COMMAND "cmake" )
@@ -25,7 +27,9 @@ BUILDNAME:STRING=${CTEST_BUILD_NAME}
 CMAKE_BUILD_TYPE:STRING=Release
 BUILD_TESTING:BOOL=OFF
 BUILD_EXAMPLES:BOOL=OFF
-ITK_USE_REVIEW:BOOL=ON 
+ITK_BUILD_ALL_MODULES:BOOL=ON
+Module_ITK-Review:BOOL=ON
+ITK_USE_REVIEW:BOOL=ON
 ")
 
 SET( ITK_RESULT_FILE "${CTEST_BINARY_DIRECTORY}/pull_result.txt" )
@@ -35,18 +39,16 @@ ${ITK_RESULT_FILE}
 ${CTEST_BINARY_DIRECTORY}/CMakeCache.txt
 )
 
-ctest_empty_binary_directory (${CTEST_BINARY_DIRECTORY})
+# remove the src directory
+execute_process( COMMAND ${CTEST_CMAKE_COMMAND} -E remove_directory ${CTEST_SOURCE_DIRECTORY}
+                 WORKING_DIRECTORY "" 
+                 OUTPUT_VARIABLE   CHECKOUT_STATUS
+                 ERROR_VARIABLE    CHECKOUT_STATUS )
 
-ctest_start(Nightly)
-ctest_update(SOURCE "${CTEST_SOURCE_DIRECTORY}")
-file(WRITE "${CTEST_BINARY_DIRECTORY}/CMakeCache.txt" ${ITK_INITIAL_CACHE})
-
-# clean the directory : remove the non tracked files if any
-execute_process( COMMAND ${CTEST_GIT_COMMAND} clean -f
-                 WORKING_DIRECTORY "${CTEST_SOURCE_DIRECTORY}" 
-                 OUTPUT_VARIABLE   CLEAN_STATUS
-                 ERROR_VARIABLE    CLEAN_STATUS
-                 )
+# update to current tag : Modulurization is almost complete
+execute_process( COMMAND ${CTEST_GIT_COMMAND} clone http://itk.org/ITK.git  ${CTEST_SOURCE_DIRECTORY}
+                 OUTPUT_VARIABLE CLEAN_STATUS
+                 ERROR_VARIABLE  CLEAN_STATUS )
 
 # Apply the patch after updating the repository
 # print the changes added with the patch
@@ -62,8 +64,10 @@ execute_process( COMMAND ${CTEST_GIT_COMMAND} apply  ${OTB_PATCH_PATH}
                  OUTPUT_VARIABLE   APPLY_PATCH_STATUS
                  ERROR_VARIABLE    APPLY_PATCH_STATUS
                  )
-file(WRITE ${ITK_RESULT_FILE} ${CLEAN_STATUS} ${PATCH_STATUS} ${APPLY_PATCH_STATUS} )
+file(WRITE ${ITK_RESULT_FILE} ${CLEAN_STATUS} ${CHECKOUT_STATUS} ${PATCH_STATUS} ${APPLY_PATCH_STATUS} )
 
+ctest_empty_binary_directory (${CTEST_BINARY_DIRECTORY})
+ctest_start(Experimental)
+file(WRITE "${CTEST_BINARY_DIRECTORY}/CMakeCache.txt" ${ITK_INITIAL_CACHE})
 ctest_configure (BUILD "${CTEST_BINARY_DIRECTORY}")
 ctest_build (BUILD "${CTEST_BINARY_DIRECTORY}")
-
