086a0d4a7bd0024010bed67b433f5efb2e32513e
Aurelien Bricier <aurelien.bricier@c-s.fr>
2011-04-13 15:08:39 +0200

    ENH: removed obsolete scripts

--- Config/PC8413-Monteverdi-Nightly.cmake
+++
@@ -1,8 +0,0 @@
-SET(BUILDNAME "PC8413-VC2008Express-OSGEO-Release" CACHE STRING "" FORCE)
-SET(CMAKE_BUILD_TYPE "Release" CACHE STRING "" FORCE)
-SET(MAKECOMMAND "C:\PROGRA~1\MICROS~1.0\Common7\IDE\VCExpress.exe Monteverdi.sln /build Release /project ALL_BUILD" CACHE STRING "" FORCE)
-SET(BUILD_TESTING ON CACHE BOOL "" FORCE)
-SET(OTB_DIR "C:\Users\jmalik\dev\nightly\build\OTB" CACHE PATH "" FORCE)
-SET(OTB_DATA_ROOT "C:/Users/jmalik/dev/src/OTB-Data" CACHE STRING "" FORCE)
-SET(GDAL_INCLUDE_DIR "C:/OSGeo4W/apps/gdal-16/include" CACHE PATH "" FORCE)
-SET(CMAKE_INSTALL_PREFIX "C:\Users\jmalik\dev\nightly\install\Monteverdi" CACHE PATH "" FORCE)
