729a47107ece4328ef97f188749828e853ea424c
Otmane Lahlou <otmane.lahlou@c-s.fr>
2011-04-14 17:46:32 +0200

    ENH : typo

--- Config/leod-OTB-Wrapping-Nightly.cmake
+++ Config/leod-OTB-Wrapping-Nightly.cmake
@@ -53,7 +53,7 @@ JAVA_COMPILE:FILEPATH=/usr/bin/javac
 JAVA_RUNTIME:FILEPATH=/usr/bin/java
 
 #Python Excecutable
-PYTHON_EXCECUTABLE:FILEPATH=/usr/bin/python
+PYTHON_EXECUTABLE:FILEPATH=/usr/bin/python
 ")
 
 SET( PULL_RESULT_FILE "${CTEST_BINARY_DIRECTORY}/pull_result.txt" )
