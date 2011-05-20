51e816397c70af28219f9bc797655e4fdd42ef45
Sebastien Dinot <sebastien.dinot@c-s.fr>
2011-05-20 14:32:26 +0200

    Packages for Karmic Koala are no longer provided

--- Packaging/ubuntu/Monteverdi/make_ubuntu_packages.sh
+++ Packaging/ubuntu/Monteverdi/make_ubuntu_packages.sh
@@ -266,7 +266,7 @@ done
 
 echo "Source package generation..."
 cd "$TMPDIR/monteverdi-$src_version_full"
-for target in karmic lucid maverick natty ; do
+for target in lucid maverick natty ; do
     set_ubuntu_code_name "$target"
     echo "Package for $ubuntu_codename ($ubuntu_version)"
     cp -f "$DEBDIR/changelog" debian
