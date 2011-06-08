9e3201a1acc486e6275cdae4952b52845ba0cf01
Manuel Grizonnet <manuel.grizonnet@orfeo-toolbox.org>
2011-06-08 13:45:55 +0200

    ENH:add clang experimental build on pc-grizonnetm

---
+++ Config/pc-grizonnetm-OTB-Clang-Experimental.cmake
@@ -0,0 +1,46 @@
+#compilation command
+# ctest -S /mnt/dd-2/OTB/trunk/OTB-DevUtils/Config/pc-grizonnetm-OTB-Clang-Experimental.cmake
+SET (CTEST_SOURCE_DIRECTORY "/mnt/dd-2/OTB/trunk/OTB-Experimental/")
+SET (CTEST_BINARY_DIRECTORY "/mnt/dd-2/OTB/OTB-Binary-Clang-Experimental/")
+
+
+# which ctest command to use for running the dashboard
+SET (CTEST_COMMAND 
+  "ctest -j6 -D Experimental -A /mnt/dd-2/OTB/trunk/OTB-DevUtils/Config/pc-grizonnetm-OTB-Clang-Experimental.cmake -V"
+  )
+
+# what cmake command to use for configuring this dashboard
+SET (CTEST_CMAKE_COMMAND 
+  "cmake"
+  )
+
+# should ctest wipe the binary tree before running
+SET (CTEST_START_WITH_EMPTY_BINARY_DIRECTORY TRUE)
+
+# this is the initial cache to use for the binary tree, be careful to escape
+# any quotes inside of this string if you use it
+SET (CTEST_INITIAL_CACHE "
+CMAKE_CXX_COMPILER:STRING=/usr/local/bin/clang++
+// Use Launchers for CDash reporting
+CTEST_USE_LAUNCHERS:BOOL=ON
+//Command used to build entire project from the command line.
+MAKECOMMAND:STRING=/usr/bin/make -i -k -j8
+//Name of the build
+BUILDNAME:STRING=CLang-Ubuntu10.4-64bits
+//Name of the computer/site where compile is being run
+SITE:STRING=pc-grizonnetm
+//LargeInput
+OTB_DATA_USE_LARGEINPUT:BOOL=ON
+OTB_DATA_LARGEINPUT_ROOT:STRING=/mnt/dd-2/OTB/trunk/OTB-Data/LargeInput
+//Data root
+OTB_DATA_ROOT:STRING=/mnt/dd-2/OTB/trunk/OTB-Data
+BUILD_TESTING:BOOL=ON
+BUILD_EXAMPLES:BOOL=ON
+")
+
+# set any extra envionment varibles here
+SET (CTEST_ENVIRONMENT
+ "DISPLAY=:0"
+)
+
+
