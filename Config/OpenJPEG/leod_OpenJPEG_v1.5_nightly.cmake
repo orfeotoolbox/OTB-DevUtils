58683b40f2ab60a0b3cdc2da5fb5f0cd53f94028
Julien Malik <julien.malik@c-s.fr>
2011-07-12 18:09:33 +0200

    ADD: add  OpenJPEG nightly config files with leod

---
+++ Config/OpenJPEG/leod_OpenJPEG_v1.5_nightly.cmake
@@ -0,0 +1,59 @@
+# -----------------------------------------------------------------------------
+# Nightly script for OpenJPEG trunk
+# This will retrieve/compile/run tests/upload to cdash OpenJPEG
+# Results will be available at: http://my.cdash.org/index.php?project=OPENJPEG
+# ctest -S leod_openJPEG_v1.5_nightly.cmake -V
+# Author: mickael.savinaud@c-s.fr
+# Date: 2011-06-17
+# -----------------------------------------------------------------------------
+
+cmake_minimum_required(VERSION 2.8)
+SET (ENV{DISPLAY} ":0.0")
+
+# Set where to find srr and test data and where to build binaries.
+SET (CTEST_SOURCE_DIRECTORY       "$ENV{HOME}/OpenJPEG/src/openjpeg-1.5")
+SET (CTEST_BINARY_DIRECTORY       "$ENV{HOME}/OpenJPEG/build/OpenJPEG_v1.5")
+SET (CTEST_SOURCE_DATA_DIRECTORY  "$ENV{HOME}/OpenJPEG/src/opj-data")
+
+# User inputs:
+SET( CTEST_CMAKE_GENERATOR      "Unix Makefiles" )    # What is your compilation apps ?
+SET( CTEST_CMAKE_COMMAND        "cmake" )
+SET( CTEST_BUILD_COMMAND        "/usr/bin/make -j6" )
+SET( CTEST_SITE                 "leod.c-s.fr" )       # Generally the output of hostname
+SET( CTEST_BUILD_CONFIGURATION  Debug)                # What type of build do you want ?
+SET( CTEST_BUILD_NAME           "MacOSX10.5-32bits-v1.5-${CTEST_BUILD_CONFIGURATION}") # Build Name
+SET( ENV{CFLAGS} "-Wall" )                            # All warnings ...
+
+# FIXME: For the moment, I need to build my own version of liblcms2 (not done here)
+set( CACHE_CONTENTS "
+CMAKE_BUILD_TYPE:STRING=${CTEST_BUILD_CONFIGURATION}
+
+BUILD_TESTING:BOOL=TRUE
+BUILD_EXAMPLES:BOOL=TRUE
+
+JPEG2000_CONFORMANCE_DATA_ROOT:PATH=${CTEST_SOURCE_DATA_DIRECTORY}
+
+BUILD_THIRDPARTY:BOOL=ON
+LCMS2_INCLUDE_DIR:PATH=
+LCMS2_LIBRARY:PATH=
+" )
+
+# Update method 
+# repository: http://openjpeg.googlecode.com/svn/branches/openjpeg-1.5 
+# need to use https for CS machine
+set( CTEST_UPDATE_COMMAND   "/usr/bin/svn")
+
+# 3. cmake specific:
+#set( CTEST_NOTES_FILES      "${CTEST_SCRIPT_DIRECTORY}/${CTEST_SCRIPT_NAME}")
+
+ctest_empty_binary_directory( "${CTEST_BINARY_DIRECTORY}" )
+file(WRITE "${CTEST_BINARY_DIRECTORY}/CMakeCache.txt" "${CACHE_CONTENTS}")
+
+# Perform the Nightly build
+ctest_start(Experimental)#Nightly TRACK Nightly-v1.5)
+ctest_update(SOURCE "${CTEST_SOURCE_DIRECTORY}")
+ctest_configure(BUILD "${CTEST_BINARY_DIRECTORY}")
+ctest_build(BUILD "${CTEST_BINARY_DIRECTORY}")
+ctest_test(BUILD "${CTEST_BINARY_DIRECTORY}")
+ctest_submit()
+
