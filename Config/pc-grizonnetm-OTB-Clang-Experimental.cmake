3b8b259e374b4fadabb773b5e98bc713d6318894
Manuel Grizonnet <manuel.grizonnet@orfeo-toolbox.org>
2011-10-13 17:52:56 +0200

    ENH:update nightly clang submission config

--- Config/pc-grizonnetm-OTB-Clang-Experimental.cmake
+++ Config/pc-grizonnetm-OTB-Clang-Experimental.cmake
@@ -1,49 +1,68 @@
-#compilation command
-# ctest -S /mnt/dd-2/OTB/trunk/OTB-DevUtils/Config/pc-grizonnetm-OTB-Clang-Experimental.cmake
-SET (CTEST_SOURCE_DIRECTORY "/mnt/dd-2/OTB/trunk/OTB-Experimental/")
-SET (CTEST_BINARY_DIRECTORY "/mnt/dd-2/OTB/OTB-Binary-Clang-Experimental/")
+# Client maintainer: manuel.grizonnet@cnes.fr
+set(dashboard_model Nightly)
+set(CTEST_DASHBOARD_ROOT "/mnt/dd-2/OTB")
+SET (CTEST_SITE "pc-grizonnetm.cst.cnes.fr")
+set(CTEST_BUILD_CONFIGURATION Release)
+set(CTEST_BUILD_NAME "Clang-Ubuntu10.04-64bits-${CTEST_BUILD_CONFIGURATION}")
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set(CTEST_BUILD_COMMAND "/usr/bin/make -j8 -i -k" )
+set(CTEST_TEST_ARGS PARALLEL_LEVEL 4)
+set(CTEST_TEST_TIMEOUT 1500)
+set(CTEST_HG_COMMAND "/usr/bin/hg")
 
+set(dashboard_root_name "tests")
+set(dashboard_source_name "trunk/OTB-Nightly")
+set(dashboard_binary_name "OTB-Binary-Clang-Experimental")
 
-# which ctest command to use for running the dashboard
-SET (CTEST_COMMAND 
-  "ctest -j6 -D Experimental -A /mnt/dd-2/OTB/trunk/OTB-DevUtils/Config/pc-grizonnetm-OTB-Clang-Experimental.cmake -V"
-  )
+#set(dashboard_fresh_source_checkout TRUE)
+set(dashboard_hg_url "http://hg.orfeo-toolbox.org/OTB-Nightly")
+set(dashboard_hg_branch "default")
 
-# what cmake command to use for configuring this dashboard
-SET (CTEST_CMAKE_COMMAND 
-  "cmake"
-  )
-SET (CTEST_BUILD_COMMAND "/usr/bin/make -j8 -i -k")
-# should ctest wipe the binary tree before running
-SET (CTEST_START_WITH_EMPTY_BINARY_DIRECTORY TRUE)
+#set(ENV{DISPLAY} ":0.0")
 
-# this is the initial cache to use for the binary tree, be careful to escape
-# any quotes inside of this string if you use it
-SET (CTEST_INITIAL_CACHE "
+macro(dashboard_hook_init)
+  set(dashboard_cache "${dashboard_cache}
+
 CMAKE_CXX_COMPILER:STRING=/usr/local/bin/clang++
-// Use Launchers for CDash reporting
-CTEST_USE_LAUNCHERS:BOOL=ON
-//Name of the build
-BUILDNAME:STRING=CLang-Ubuntu10.4-64bits
-//Name of the computer/site where compile is being run
-SITE:STRING=pc-grizonnetm
-//LargeInput
+
 OTB_DATA_USE_LARGEINPUT:BOOL=ON
 OTB_DATA_LARGEINPUT_ROOT:STRING=/mnt/dd-2/OTB/trunk/OTB-Data/LargeInput
-//Data root
+
 OTB_DATA_ROOT:STRING=/mnt/dd-2/OTB/trunk/OTB-Data
+
 BUILD_TESTING:BOOL=ON
 BUILD_EXAMPLES:BOOL=ON
-//build applications
+OTB_USE_CURL:BOOL=ON
+OTB_USE_PQXX:BOOL=ON
+OTB_USE_EXTERNAL_BOOST:BOOL=ON
+OTB_USE_EXTERNAL_EXPAT:BOOL=ON
+OTB_USE_DEPRECATED:BOOL=ON
+ITK_USE_PATENTED:BOOL=ON
+OTB_USE_PATENTED:BOOL=ON
+USE_FFTWD:BOOL=ON
+USE_FFTWF:BOOL=ON
+OTB_GL_USE_ACCEL:BOOL=ON 
+OTB_USE_VISU_GUI:BOOL=OFF
+ITK_USE_REVIEW:BOOL=ON 
+ITK_USE_OPTIMIZED_REGISTRATION_METHODS:BOOL=ON 
+OTB_USE_MAPNIK:BOOL=ON 
+
+MAPNIK_INCLUDE_DIR:STRING=/usr/include
+MAPNIK_LIBRARY:STRING=/usr/lib/libmapnik.so
+
+GDAL_CONFIG:STRING=/home/grizonnetm/Local/gdal-1.7.1-build/bin/gdal-config
+GDALCONFIG_EXECUTABLE:STRING=/home/grizonnetm/Local/gdal-1.7.1-build/bin/gdal-config
+GDAL_INCLUDE_DIR:STRING=/home/grizonnetm/Local/gdal-1.7.1-build/include
+GDAL_LIBRARY:STRING=/home/grizonnetm/Local/gdal-1.7.1-build/lib/libgdal.so
+OGR_INCLUDE_DIRS:STRING=/home/grizonnetm/Local/gdal-1.7.1-build/include
 BUILD_APPLICATIONS:BOOL=ON
 WRAP_PYTHON:BOOL=ON
 WRAP_QT:BOOL=ON
 WRAP_PYQT:BOOL=ON
-")
 
-# set any extra envionment varibles here
-#SET (CTEST_ENVIRONMENT
-# "DISPLAY=:0"
-#)
+    ")
+endmacro()
+
+include(${CTEST_SCRIPT_DIRECTORY}/otb_common.cmake)
 
 
