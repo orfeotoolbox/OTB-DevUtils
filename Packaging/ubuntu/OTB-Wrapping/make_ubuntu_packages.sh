db58b9a14f60bf1d42a232c6900b21c87094b89e
Julien Malik <julien.malik@c-s.fr>
2011-11-27 18:35:42 +0100

    ENH: debian package - generate only for lucid natty oneiric

--- Packaging/ubuntu/OTB-Wrapping/make_ubuntu_packages.sh
+++ Packaging/ubuntu/OTB-Wrapping/make_ubuntu_packages.sh
@@ -266,7 +266,7 @@ done
 
 echo "Source package generation..."
 cd "$TMPDIR/otb-wrapping-$src_version_full"
-for target in lucid maverick natty ; do
+for target in lucid natty oneiric ; do
     set_ubuntu_code_name "$target"
     echo "Package for $ubuntu_codename ($ubuntu_version)"
     cp -f "$DEBDIR/changelog" debian
