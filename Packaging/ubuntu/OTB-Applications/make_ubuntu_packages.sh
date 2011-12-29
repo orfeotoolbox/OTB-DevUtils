25e524a7d218e447d99d0695604124760d8d5dc2
Julien Malik <julien.malik@c-s.fr>
2011-12-29 17:37:29 +0100

    ENH: maverick is back

--- Packaging/ubuntu/OTB-Applications/make_ubuntu_packages.sh
+++ Packaging/ubuntu/OTB-Applications/make_ubuntu_packages.sh
@@ -253,7 +253,7 @@ echo "Source package generation..."
 cd "$TMPDIR/otbapp-$otb_version_full"
 # No oneiric because of unsupported fluid1.3
 #for target in lucid natty oneiric ; do
-for target in lucid natty ; do
+for target in lucid maverick natty ; do
     set_ubuntu_code_name "$target"
     echo "Package for $ubuntu_codename ($ubuntu_version)"
     cp -f "$DEBDIR/changelog" debian
