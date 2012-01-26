1e6541bdb8fa382b2379708917066da6376ccff9
Julien Malik <julien.malik@c-s.fr>
2012-01-26 18:20:52 +0100

    ENH: add packaging of monteverdi in osgeo4W

--- Packaging/osgeo4w/osgeo4w-otb.py
+++ Packaging/osgeo4w/osgeo4w-otb.py
@@ -1,22 +1,24 @@
 import os, sys, shutil, datetime, subprocess
 
 
-OTB_INSTALL="C:\\Users\\jmalik\\Dashboard\\install\\OTB-Release-VC2008"
+OTB_INSTALL="C:\\Users\\jmalik\\Dashboard\\install\\OTB-Release-VC2008-ExternalOssim"
+MONTEVERDI_INSTALL="C:\\Users\\jmalik\\Dashboard\\install\\Monteverdi-Release-VC2008"
+OSSIM_INSTALL="C:\\Users\\jmalik\\Dashboard\\install\\ossim-trunk"
 OSGEO4W_STAGING="C:\\Users\\jmalik\\Dashboard\\osgeo4w"
 OSGEO4W_TEMPLATE="C:\\Users\\jmalik\\Dashboard\\src\\OTB-DevUtils\\Packaging\\osgeo4w"
+TAREXE="C:\\OSGeo4W\\apps\\msys\\bin\\tar.exe"
 
 # TODO extract this automatically
 OTB_VERSION="3.11.0"
+MONTEVERDI_VERSION="1.9.0"
 
-#COMPRESS7Z="C:\\Program Files\\7-Zip\\7z.exe"
-TAREXE="C:\\OSGeo4W\\apps\\msys\\bin\\tar.exe"
 
 todayiso = datetime.date.today().isoformat().replace('-','')
 yesterdayiso = (datetime.date.today()-datetime.timedelta(1)).isoformat().replace('-','')
 
 def initialize_package(packagebasename, src_version, pkg_version, stage_root):
     # directory where installation files will be staged
-    package_versioned_name = packagebasename + "-" + OTB_VERSION + "-" + yesterdayiso + "-1"
+    package_versioned_name = packagebasename + "-" + src_version + "-" + yesterdayiso + "-1"
     dstdir = os.path.join(OSGEO4W_STAGING, package_versioned_name)
 
     # copy the template to the dstdir
@@ -35,8 +37,10 @@ def remove_placeholder_files(dirname):
             os.remove(os.path.join(root, name))
 
 def make_tarbz2(package_versioned_name):
-    os.chdir(OSGEO4W_STAGING)
-    subprocess.call( [TAREXE, "-cvjf", package_versioned_name + ".tar.bz2",  package_versioned_name + "/*" ] )
+    current_dir = os.getcwd()
+    os.chdir( os.path.join(OSGEO4W_STAGING, package_versioned_name) )
+    subprocess.call( [TAREXE, "-cvjf", "../" + package_versioned_name + ".tar.bz2",  "*" ] )
+    os.chdir( current_dir )
 
 def make_otb_bin():
     package_name = "otb-bin"
@@ -46,7 +50,14 @@ def make_otb_bin():
     inputdir = os.path.join(OTB_INSTALL, "lib\\otb\\applications")
     for fic in os.listdir( inputdir ) :
       shutil.copy( os.path.join(inputdir, fic), \
-                   os.path.join(os.path.join(OSGEO4W_STAGING, package_versioned_name), "apps", "orfeotoolbox", "applications" ) )
+                   os.path.join(OSGEO4W_STAGING, package_versioned_name, "apps", "orfeotoolbox", "applications" ) )
+
+    # copy ossim and ossimplugin dll in the otbbin package for now
+    shutil.copy( os.path.join(OTB_INSTALL, "bin", "otbossimplugins.dll"),
+                 os.path.join(OSGEO4W_STAGING, package_versioned_name, "bin" ) )
+    shutil.copy( os.path.join(OSSIM_INSTALL, "bin", "ossim.dll"),
+                 os.path.join(OSGEO4W_STAGING, package_versioned_name, "bin" ) )
+    
     make_tarbz2(package_versioned_name)
                    
 def make_otb_python():
@@ -59,6 +70,18 @@ def make_otb_python():
       shutil.copy( os.path.join(inputdir, fic), \
                    os.path.join(os.path.join(OSGEO4W_STAGING, package_versioned_name), "apps", "orfeotoolbox", "python" ) )
     make_tarbz2(package_versioned_name)
+    
+def make_monteverdi():
+    package_name = "otb-monteverdi"
+    package_versioned_name = initialize_package(package_name, MONTEVERDI_VERSION, yesterdayiso, OSGEO4W_STAGING)
+
+    # copy the content of lib/otb/applications to apps/orfeotoolbox/applications
+    inputdir = os.path.join(MONTEVERDI_INSTALL, "bin")
+    shutil.copy( os.path.join(MONTEVERDI_INSTALL, "bin", "monteverdi.exe"),
+                 os.path.join(OSGEO4W_STAGING, package_versioned_name, "apps", "orfeotoolbox", "monteverdi", "bin" ) )
+   
+    make_tarbz2(package_versioned_name)
 
 make_otb_bin()
 make_otb_python()
+make_monteverdi()
---
+++ Packaging/osgeo4w/template-otb-monteverdi/etc/preremove/monteverdi.bat
@@ -0,0 +1,4 @@
+del "%OSGEO4W_STARTMENU%\Monteverdi (1.10.0).lnk"
+del "%ALLUSERSPROFILE%\Desktop\Monteverdi (1.10.0).lnk"
+del "%OSGEO4W_ROOT%"\bin\monteverdi.bat
+
