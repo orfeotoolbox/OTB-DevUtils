2194b4e071169e8c66d4ccf7b87164a2306237cf
Jonathan Guinet <jonathan.guinet@c-s.fr>
2012-04-11 16:19:44 +0200

    COMP: OTB-Debug-VC2008 is generated with mapnik.

--- Config/raoul-Win7-OTB-Debug-VC2008.cmake
+++ Config/raoul-Win7-OTB-Debug-VC2008.cmake
@@ -34,6 +34,17 @@ OTB_DATA_LARGEINPUT_ROOT:PATH=C:/Users/jmalik/Dashboard/src/OTB-LargeInput
 GDAL_INCLUDE_DIR:PATH=C:/OSGeo4W/include
 GDAL_LIBRARY:FILEPATH=C:/OSGeo4W/lib/gdal_i.lib
 
+
+OTB_USE_EXTERNAL_BOOST:BOOL=ON
+OTB_USE_MAPNIK:BOOL=ON
+MAPNIK_INCLUDE_DIR:PATH=C:/OSGeo4W/include/mapnik
+MAPNIK_LIBRARY:FILEPATH=C:/OSGeo4W/lib/mapnik.lib
+FREETYPE2_INCLUDE_DIR:PATH=C:/OSGeo4W/include/freetype
+ICUUC_INCLUDE_DIR:PATH=C:/Program Files (x86)/icu4c-4_2_1-Win32-msvc9/icu/include
+ICUUC_LIBRARY:FILEPATH=C:/Program Files (x86)/icu4c-4_2_1-Win32-msvc9/icu/lib/icuuc.lib
+LTDL_INCLUDE_DIR:PATH=C:/Program Files (x86)/GnuWin32/include
+LTDL_LIBRARY:FILEPATH=C:/Program Files (x86)/GnuWin32/lib/ltdl.lib
+
 GEOTIFF_INCLUDE_DIRS:PATH=C:/OSGeo4W/include
 GEOTIFF_LIBRARY:FILEPATH=C:/OSGeo4W/lib/geotiff_i.lib
 
