4bd0aaa068307f468752915786a0414b99899de6
Luc Hermitte <luc.hermitte@c-s.fr>
2012-04-26 18:08:38 +0200

    INST: Update internal boost for matching OTB-134/OGR requirements

--- Scripts/updateInternalBoost.sh
+++ Scripts/updateInternalBoost.sh
@@ -3,11 +3,10 @@
 # - run the bootstrap script (it creates an executable called b2, the boost build tool)
 # - build the bcp tool ( cd BOOST_ROOT/tools/bcp; BOOST_ROOT/b2 )
 # - customize the following variables :
-BOOST_ROOT=/home/otbval/tools/src/boost_1_47_0
-BOOST_VER=1.47.0
+BOOST_ROOT=/home/otbval/tools/src/boost_1_49_0
+BOOST_VER=1.49.0
 OTB_ROOT=/home/otbval/Dashboard/src/OTB
 MVD_ROOT=/home/otbval/Dashboard/src/Monteverdi
-
 
 TMP1=/tmp/boost_subpart1
 TMP2=/tmp/boost_subpart2
@@ -16,16 +15,16 @@ BOOST_SUBPARTROOT=/tmp/boost_subparts
 mkdir $BOOST_SUBPARTROOT
 
 echo 'Extract what we need from Code'
-grep -r "#include <boost/" $OTB_ROOT/Code | cut -d ' ' -f 2 | sed 's/<\(.*\)>/\1/' > $TMP1
+grep -r "^#include <boost/" $OTB_ROOT/Code | cut -d ' ' -f 2 | sed 's/<\(.*\)>/\1/' > $TMP1
 
 echo 'Extract what we need from Utilities/otbliblas'
-grep -r "#include <boost/" $OTB_ROOT/Utilities/otbliblas | cut -d ' ' -f 2 | sed 's/<\(.*\)>/\1/' >> $TMP1
+grep -r "^#include <boost/" $OTB_ROOT/Utilities/otbliblas | cut -d ' ' -f 2 | sed 's/<\(.*\)>/\1/' >> $TMP1
 
 echo 'Extract what we need from Utilities/otbkml'
-grep -r "#include \"boost/" $OTB_ROOT/Utilities/otbkml | cut -d ' ' -f 2 | sed 's/\"\(.*\)\"/\1/' >> $TMP1
+grep -r "^#include \"boost/" $OTB_ROOT/Utilities/otbkml | cut -d ' ' -f 2 | sed 's/\"\(.*\)\"/\1/' >> $TMP1
 
 echo 'Extract what we need for Monteverdi'
-grep -r "#include <boost/" $MVD_ROOT/Code | cut -d ' ' -f 2 | sed 's/<\(.*\)>/\1/' >> $TMP1
+grep -r "^#include <boost/" $MVD_ROOT/Code | cut -d ' ' -f 2 | sed 's/<\(.*\)>/\1/' >> $TMP1
 
 echo 'Remove duplicates'
 awk ' { arr[$1]=$0 } END { for ( key in arr ) { print arr[key] } } ' $TMP1 > $TMP2
@@ -36,7 +35,6 @@ do
   echo $l
   $BOOST_ROOT/dist/bin/bcp --boost=$BOOST_ROOT $l $BOOST_SUBPARTROOT
 done
-
 
 rm -rf $OTB_ROOT/Utilities/BGL/boost
 cp -r  $BOOST_SUBPARTROOT/boost $OTB_ROOT/Utilities/BGL
@@ -49,6 +47,6 @@ hg revert $OTB_ROOT/Utilities/BGL/boost/CMakeLists.txt
 hg st Utilities/BGL | grep '^! ' | sed -e 's/^! //' | xargs hg rm
 hg st Utilities/BGL | grep '^? ' | sed -e 's/^? //' | xargs hg add
 
-hg commit -m "ENH: update Boost to $BOOST_VER"
+hg commit -m "ENH: update Boost to $BOOST_VER" $OTB_ROOT/Utilities/BGL
 
 
