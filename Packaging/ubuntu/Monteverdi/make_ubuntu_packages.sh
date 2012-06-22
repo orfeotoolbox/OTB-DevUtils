3ebdd4696e60e94fdf3ae451205d99658d85e36b
Julien Malik <julien.malik@c-s.fr>
2012-06-22 09:28:35 +0200

    ENH: definitely remove natty. it won't build...

--- Packaging/ubuntu/Monteverdi/make_ubuntu_packages.sh
+++ Packaging/ubuntu/Monteverdi/make_ubuntu_packages.sh
@@ -270,7 +270,7 @@ done
 
 echo "Source package generation..."
 cd "$TMPDIR/monteverdi-$src_version_full"
-for target in lucid maverick natty oneiric precise ; do
+for target in lucid maverick oneiric precise ; do
     set_ubuntu_code_name "$target"
     echo "Package for $ubuntu_codename ($ubuntu_version)"
     cp -f "$DEBDIR/changelog" debian
--- Packaging/ubuntu/OTB-Wrapping/make_ubuntu_packages.sh
+++ Packaging/ubuntu/OTB-Wrapping/make_ubuntu_packages.sh
@@ -270,7 +270,7 @@ done
 
 echo "Source package generation..."
 cd "$TMPDIR/otb-wrapping-$src_version_full"
-for target in lucid maverick natty oneiric precise ; do
+for target in lucid maverick oneiric precise ; do
 #for target in precise ; do
     set_ubuntu_code_name "$target"
     echo "Package for $ubuntu_codename ($ubuntu_version)"
