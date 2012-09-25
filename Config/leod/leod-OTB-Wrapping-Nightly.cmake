9be209944bddb702cce5336893339b5525e50b03
Julien Malik <julien.malik@c-s.fr>
2012-09-25 19:14:18 +0200

    ENH: remove CMAKE_C_COMPILER definition

--- Config/leod/leod-OTB-Wrapping-Nightly.cmake
+++ Config/leod/leod-OTB-Wrapping-Nightly.cmake
@@ -19,13 +19,8 @@ CTEST_USE_LAUNCHERS:BOOL=ON
 #OTB_DATA_LARGEINPUT_ROOT:STRING=/Users/otbval/Data/OTB-LargeInput
 OTB_DATA_ROOT:STRING=$ENV{HOME}/Data/OTB-Data
 
-
-CMAKE_C_COMPILER:FILEPATH=/usr/bin/gcc
-CMAKE_CXX_COMPILER:FILEPATH=/usr/bin/g++
-
 CMAKE_C_FLAGS:STRING= -Wall -Wno-uninitialized -Wno-unused-variable
 CMAKE_CXX_FLAGS:STRING= -Wall -Wno-deprecated -Wno-uninitialized -Wno-unused-variable
-#CMAKE_OSX_ARCHITECTURES:STRING=i386
 CMAKE_BUILD_TYPE:STRING=Release
 
 OTB_DIR:STRING=$ENV{HOME}/Dashboard/nightly/OTB-Release/build
