a6c68b50d964f547838c351dca4af2db211fe1ea
Mickael Savinaud <mickael.savinaud@c-s.fr>
2012-10-01 10:03:31 +0200

    Move to experimental the build with VS11 Beta Rename scripts with the number of VS version and the year

--- Config/PO9450/PO9450-Win7-OTB-VS11-2012Beta-Debug.cmake
+++ Config/PO9450/PO9450-Win7-OTB-VS11-2012Beta-Debug.cmake
@@ -1,11 +1,11 @@
-SET (CTEST_SOURCE_DIRECTORY "C:/Users/msavinau/dev/nightly/OTB-VS2011Beta-Release/src")
-SET (CTEST_BINARY_DIRECTORY "C:/Users/msavinau/dev/nightly/OTB-VS2011Beta-Release/build")
+SET (CTEST_SOURCE_DIRECTORY "C:/Users/msavinau/dev/nightly/OTB-VS2011Beta-Debug/src")
+SET (CTEST_BINARY_DIRECTORY "C:/Users/msavinau/dev/nightly/OTB-VS2011Beta-Debug/build")
 
 SET (CTEST_CMAKE_GENERATOR  "Visual Studio 11" )
 SET (CTEST_CMAKE_COMMAND "C:/Program Files/CMake 2.8/bin/cmake.exe")
 SET (CTEST_SITE "PO9450.c-s.fr" )
-SET (CTEST_BUILD_NAME "Win7-Visual2012Beta-Release-Static")
-SET (CTEST_BUILD_CONFIGURATION "Release")
+SET (CTEST_BUILD_NAME "Win7-VS11-2012Beta-Debug-Static")
+SET (CTEST_BUILD_CONFIGURATION "Debug")
 SET (CTEST_HG_COMMAND "C:/Program Files/TortoiseHg/hg.exe")
 SET (CTEST_HG_UPDATE_OPTIONS "-C")
 
@@ -13,12 +13,9 @@ SET (OTB_INITIAL_CACHE "
 BUILDNAME:STRING=${CTEST_BUILD_NAME}
 SITE:STRING=${CTEST_SITE}
 
-CMAKE_CONFIGURATION_TYPES:STRING=Release
+CMAKE_CONFIGURATION_TYPES:STRING=Debug
 
-CMAKE_INSTALL_PREFIX:PATH=C:/Users/msavinau/dev/nightly/OTB-VS2011Beta-Release/install
-OTB_USE_CPACK:BOOL=OFF
-
-BUILD_TESTING:BOOL=ON
+BUILD_TESTING:BOOL=OFF
 BUILD_EXAMPLES:BOOL=ON
 
 # Applications
@@ -37,11 +34,7 @@ OTB_DATA_USE_LARGEINPUT:BOOL=OFF
 
 ")
 
-#Remove install dir
-execute_process(COMMAND ${CTEST_CMAKE_COMMAND} -E remove_directory C:/Users/msavinau/dev/nightly/OTB-VS2011Beta-Release/install )
-execute_process(COMMAND ${CTEST_CMAKE_COMMAND} -E make_directory C:/Users/msavinau/dev/nightly/OTB-VS2011Beta-Release/install )
-
-#remove build dir
+#Remove build dir
 ctest_empty_binary_directory (${CTEST_BINARY_DIRECTORY})
 
 SET (CTEST_NOTES_FILES
@@ -49,12 +42,11 @@ ${CTEST_SCRIPT_DIRECTORY}/${CTEST_SCRIPT_NAME}
 ${CTEST_BINARY_DIRECTORY}/CMakeCache.txt
 )
 
-ctest_start(Nightly)
+ctest_start(Experimental)
 ctest_update(SOURCE "${CTEST_SOURCE_DIRECTORY}")
 file(WRITE "${CTEST_BINARY_DIRECTORY}/CMakeCache.txt" ${OTB_INITIAL_CACHE})
 ctest_configure (BUILD "${CTEST_BINARY_DIRECTORY}")
 ctest_read_custom_files(${CTEST_BINARY_DIRECTORY})
-ctest_build (BUILD "${CTEST_BINARY_DIRECTORY}"
-             TARGET INSTALL)
-ctest_test (BUILD "${CTEST_BINARY_DIRECTORY}" PARALLEL_LEVEL 4)
+ctest_build (BUILD "${CTEST_BINARY_DIRECTORY}")
+#ctest_test (BUILD "${CTEST_BINARY_DIRECTORY}" PARALLEL_LEVEL 4)
 ctest_submit ()
