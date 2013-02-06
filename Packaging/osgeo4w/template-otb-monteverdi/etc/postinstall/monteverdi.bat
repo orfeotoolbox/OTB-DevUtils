bb23a52f9acea994a5f757e40f5800af921092d0
Julien Malik <julien.malik@c-s.fr>
2013-02-06 11:19:17 +0100

    ENH: update the osgeo4W template of monteverdi about postinstall instruction: now the start menu folder is monteverdi whitout version number

--- Packaging/osgeo4w/template-otb-monteverdi/etc/postinstall/monteverdi.bat
+++ Packaging/osgeo4w/template-otb-monteverdi/etc/postinstall/monteverdi.bat
@@ -1,6 +1,6 @@
 textreplace -std -t bin\monteverdi.bat
 
 mkdir "%OSGEO4W_STARTMENU%"
-xxmklink "%OSGEO4W_STARTMENU%\Monteverdi (1.12.0).lnk"       "%OSGEO4W_ROOT%\bin\monteverdi.bat" " " \ "Monteverdi (1.12.0)" 1 "%OSGEO4W_ROOT%\apps\orfeotoolbox\monteverdi\icons\monteverdi.ico"
-xxmklink "%ALLUSERSPROFILE%\Desktop\Monteverdi (1.12.0).lnk" "%OSGEO4W_ROOT%\bin\monteverdi.bat" " " \ "Monteverdi (1.12.0)" 1 "%OSGEO4W_ROOT%\apps\orfeotoolbox\monteverdi\icons\monteverdi.ico"
+xxmklink "%OSGEO4W_STARTMENU%\Monteverdi.lnk" "%OSGEO4W_ROOT%\bin\monteverdi.bat" " " \ "Monteverdi" 1 "%OSGEO4W_ROOT%\apps\orfeotoolbox\monteverdi\icons\monteverdi.ico"
+xxmklink "%ALLUSERSPROFILE%\Desktop\Monteverdi.lnk" "%OSGEO4W_ROOT%\bin\monteverdi.bat" " " \ "Monteverdi" 1 "%OSGEO4W_ROOT%\apps\orfeotoolbox\monteverdi\icons\monteverdi.ico"
 
