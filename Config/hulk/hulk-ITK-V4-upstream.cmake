771ec1366308b3684647fa4382fb4ad861e24b5b
Mickael Savinaud <mickael.savinaud@c-s.fr>
2013-03-14 18:46:07 +0100

    ENH: add hulk-ITK-V4-upstream config, set up to v4.3.1 branch

---
+++ Config/hulk/hulk-ITK-V4-upstream.cmake
@@ -0,0 +1,53 @@
+# Client maintainer: julien.malik@c-s.fr
+SET(ENV{DISPLAY} ":0.0")
+
+set(dashboard_model Experimental)
+set(CTEST_DASHBOARD_ROOT "/home/otbval/Dashboard")
+set(CTEST_SITE "hulk.c-s.fr")
+set(CTEST_BUILD_CONFIGURATION Debug)
+set(CTEST_BUILD_NAME "OrfeoToolbox-Ubuntu10.04-64bits-${CTEST_BUILD_CONFIGURATION}")
+set(CTEST_CMAKE_GENERATOR "Eclipse CDT4 - Unix Makefiles")
+set(CTEST_TEST_ARGS PARALLEL_LEVEL 4)
+set(CTEST_TEST_TIMEOUT 500)
+set(CTEST_BUILD_COMMAND "/usr/bin/make -j5 -i -k" )
+
+set(dashboard_root_name "tests")
+set(dashboard_source_name "src/ITKv4-upstream")
+set(dashboard_binary_name "build/ITKv4-upstream-${CTEST_BUILD_CONFIGURATION}")
+
+set(dashboard_fresh_source_checkout OFF)
+set(dashboard_git_url "https://github.com/julienmalik/ITK.git")
+set(dashboard_git_branch "v4.3.1")
+
+macro(dashboard_hook_init)
+  set(dashboard_cache "${dashboard_cache}
+
+CMAKE_CXX_FLAGS:STRING=-fPIC -Wall -Wextra
+CMAKE_C_FLAGS:STRING=-fPIC -Wall -Wextra
+
+BUILD_SHARED_LIBS:BOOL=ON
+BUILD_TESTING:BOOL=ON
+BUILD_EXAMPLES:BOOL=OFF
+
+ExternalData_OBJECT_STORES:PATH=/home/otbval/Dashboard/src/ITKv4-ExternalObjectStores
+
+# ITK_BUILD_ALL_MODULES:BOOL=ON # ON by default
+
+# as much external libraries as possible
+# libtiff on ubuntu does not support BigTIFF and is incompatible with ITK
+ITK_USE_SYSTEM_HDF5:BOOL=ON
+ITK_USE_SYSTEM_GDCM:BOOL=OFF
+ITK_USE_SYSTEM_JPEG:BOOL=ON
+ITK_USE_SYSTEM_PNG:BOOL=ON
+ITK_USE_SYSTEM_TIFF:BOOL=OFF
+ITK_USE_SYSTEM_ZLIB:BOOL=ON
+
+# OTB depends on this
+ITK_USE_FFTWF:BOOL=ON
+ITK_USE_FFTWD:BOOL=ON
+ITK_USE_SYSTEM_FFTW:BOOL=ON
+
+    ")
+endmacro()
+
+include(${CTEST_SCRIPT_DIRECTORY}/itk_common.cmake)
