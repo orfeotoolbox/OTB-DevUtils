b15c7a51bb7a22bdaa63fa1f3af70b9b0304a72d
Julien Malik <julien.malik@c-s.fr>
2013-03-20 19:49:16 +0100

    ENH: update raoul script

--- Config/raoul/raoul-OTB-ITK-V4.cmake
+++ Config/raoul/raoul-OTB-ITK-V4.cmake
@@ -1,4 +1,71 @@
 # Client maintainer: julien.malik@c-s.fr
+set(dashboard_model Experimental)
+set(CTEST_DASHBOARD_ROOT "C:/Users/jmalik/Dashboard")
+set(CTEST_SITE "raoul.c-s.fr")
+set(CTEST_BUILD_CONFIGURATION Release)
+set(CTEST_BUILD_NAME "ITKv4-Win7-vc10-${CTEST_BUILD_CONFIGURATION}")
+set(CTEST_CMAKE_GENERATOR "Visual Studio 10")
+set(CTEST_BUILD_COMMAND "/usr/bin/make -j14 -i -k" )
+set(CTEST_TEST_ARGS PARALLEL_LEVEL 4)
+set(CTEST_TEST_TIMEOUT 500)
+set(CTEST_CMAKE_COMMAND "C:/Program Files (x86)/CMake 2.8/bin/cmake.exe")
+
+set(CTEST_HG_COMMAND "C:/Program Files (x86)/Mercurial/hg.exe")
+
+set(dashboard_root_name "tests")
+set(dashboard_source_name "src/OTB-ITKv4")
+set(dashboard_binary_name "build/OTB-ITKv4-VC2010-${CTEST_BUILD_CONFIGURATION}")
+
+set(dashboard_fresh_source_checkout ON)
+set(dashboard_hg_url "https://bitbucket.org/julienmalik/otb-itkv4")
+set(dashboard_hg_branch "default")
+
+macro(dashboard_hook_init)
+  set(dashboard_cache "${dashboard_cache}
+
+BUILD_TESTING:BOOL=OFF
+BUILD_EXAMPLES:BOOL=ON
+BUILD_APPLICATIONS:BOOL=ON
+
+OTB_WRAP_PYTHON:BOOL=OFF
+OTB_WRAP_QT:BOOL=ON
+
+OTB_DATA_ROOT:STRING=C:/Users/jmalik/Dashboard/src/OTB-Data
+OTB_DATA_USE_LARGEINPUT:BOOL=OFF
+OTB_DATA_LARGEINPUT_ROOT:PATH=C:/Users/jmalik/Dashboard/src/OTB-LargeInput
+
+OTB_USE_EXTERNAL_FLTK:BOOL=OFF
+OTB_USE_EXTERNAL_OSSIM:BOOL=ON
+
+OTB_USE_CURL:BOOL=ON
+OTB_USE_EXTERNAL_EXPAT:BOOL=ON
+OTB_USE_LIBLAS:BOOL=ON
+OTB_USE_EXTERNAL_LIBLAS:BOOL=ON
+OTB_USE_GETTEXT:BOOL=OFF
+OTB_USE_JPEG2000:BOOL=ON
+
+CMAKE_INCLUDE_PATH:PATH=$ENV{OSGEO4W_ROOT}/include
+CMAKE_LIBRARY_PATH:PATH=$ENV{OSGEO4W_ROOT}/lib
+
+OTB_USE_EXTERNAL_ITK:BOOL=ON
+ITK_DIR:PATH=C:/Users/jmalik/Dashboard/build/ITKv4-Release
+
+    ")
+endmacro()
+
+include(${CTEST_SCRIPT_DIRECTORY}/../otb_common.cmake)
+
+
+
+
+
+
+
+
+
+
+
+# Client maintainer: julien.malik@c-s.fr
 
 SET (CTEST_SOURCE_DIRECTORY "C:/Users/jmalik/Dashboard/src/OTB-ITKv4")
 SET (CTEST_BINARY_DIRECTORY "C:/Users/jmalik/Dashboard/build/OTB-ITKv4-Release-MVSC2010")
