c531cb5e4b590f7aea15179ac17d2fca39faf81b
Julien Malik <julien.malik@c-s.fr>
2013-04-03 13:52:51 +0200

    ENH: add clang 3.1 build

---
+++ Config/dora/dora-Nightly-OTB-ITKV4-clang31.cmake
@@ -0,0 +1,61 @@
+# Client maintainer: julien.malik@c-s.fr
+set(ENV{DISPLAY} ":0.0")
+set(ENV{CC} "/usr/bin/clang-3.1")
+set(ENV{CXX} "/usr/bin/clang++-3.1")
+
+set(dashboard_model Experimental)
+set(CTEST_DASHBOARD_ROOT "/home/otbval/Dashboard/experimental")
+set(CTEST_SITE "dora.c-s.fr")
+set(CTEST_BUILD_CONFIGURATION RelWithDebInfo)
+set(CTEST_BUILD_NAME "ITKv4-Ubuntu12.04-64bits-clang31-${CTEST_BUILD_CONFIGURATION}")
+set(CTEST_CMAKE_GENERATOR "Eclipse CDT4 - Unix Makefiles")
+set(CTEST_BUILD_COMMAND "/usr/bin/make -j9 -i -k" )
+set(CTEST_TEST_ARGS PARALLEL_LEVEL 4)
+set(CTEST_TEST_TIMEOUT 500)
+
+set(CTEST_HG_COMMAND "/usr/bin/hg")
+#set(CTEST_HG_UPDATE_OPTIONS "-r otb-itkv4") 
+
+set(dashboard_root_name "tests")
+set(dashboard_source_name "src/OTB-ITKv4")
+set(dashboard_binary_name "build/OTB-ITKv4-clang31-${CTEST_BUILD_CONFIGURATION}")
+
+set(dashboard_fresh_source_checkout OFF)
+set(dashboard_hg_url "https://bitbucket.org/julienmalik/otb-itkv4")
+set(dashboard_hg_branch "default")
+
+macro(dashboard_hook_init)
+  set(dashboard_cache "${dashboard_cache}
+
+CMAKE_C_FLAGS:STRING=-fPIC -Wall -Wshadow -Wno-uninitialized -Wno-unused-variable
+CMAKE_CXX_FLAGS:STRING=-fPIC -Wall -Wno-deprecated -Wno-uninitialized -Wno-unused-variable
+
+BUILD_TESTING:BOOL=ON
+BUILD_EXAMPLES:BOOL=ON
+BUILD_APPLICATIONS:BOOL=ON
+
+OTB_WRAP_PYTHON:BOOL=ON
+OTB_WRAP_JAVA:BOOL=ON
+OTB_WRAP_QT:BOOL=ON
+
+OTB_DATA_USE_LARGEINPUT:BOOL=ON
+OTB_DATA_LARGEINPUT_ROOT:STRING=$ENV{HOME}/Data/OTB-LargeInput
+OTB_DATA_ROOT:STRING=$ENV{HOME}/Data/OTB-Data
+
+OTB_USE_EXTERNAL_ITK:BOOL=ON
+ITK_DIR:PATH=${CTEST_DASHBOARD_ROOT}/build/ITKv4-clang31-${CTEST_BUILD_CONFIGURATION}
+
+OTB_USE_DEPRECATED:BOOL=OFF
+
+OTB_USE_CURL:BOOL=ON
+OTB_USE_PQXX:BOOL=OFF
+OTB_USE_PATENTED:BOOL=OFF
+OTB_USE_EXTERNAL_BOOST:BOOL=ON
+OTB_USE_EXTERNAL_EXPAT:BOOL=ON
+OTB_USE_EXTERNAL_FLTK:BOOL=ON
+OTB_USE_MAPNIK:BOOL=OFF
+
+    ")
+endmacro()
+
+include(${CTEST_SCRIPT_DIRECTORY}/../otb_common.cmake)
