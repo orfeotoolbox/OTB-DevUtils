7e07cba8f39c85856a09be652b7b3c9399354fa4
Manuel Grizonnet <manuel.grizonnet@orfeo-toolbox.org>
2013-04-29 14:19:27 +0200

    ENH: add ubuntu 13.04 to build packages scripts for mvd

--- Packaging/ubuntu/Monteverdi/make_ubuntu_packages.sh
+++ Packaging/ubuntu/Monteverdi/make_ubuntu_packages.sh
@@ -166,6 +166,10 @@ check_gpgkeyid ()
 set_ubuntu_code_name ()
 {
     case "$1" in
+        "raring" )
+            ubuntu_codename="Raring Ringtail"
+            ubuntu_version="13.04"
+            ;;
         "quantal" )
             ubuntu_codename="Quantal Quetzal"
             ubuntu_version="12.10"
@@ -274,7 +278,7 @@ done
 
 echo "Source package generation..."
 cd "$TMPDIR/monteverdi-$src_version_full"
-for target in lucid oneiric precise quantal ; do
+for target in lucid oneiric precise quantal raring ; do
     set_ubuntu_code_name "$target"
     echo "Package for $ubuntu_codename ($ubuntu_version)"
     cp -f "$DEBDIR/changelog" debian
