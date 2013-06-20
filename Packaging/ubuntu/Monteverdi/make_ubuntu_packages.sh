65706d5914396b6dfaa1e27751b45fad6200f36f
Julien Malik <julien.malik@c-s.fr>
2013-06-20 16:45:09 +0200

    ENH: do not build anymore for lucid (no opencv available)

--- Packaging/ubuntu/Monteverdi/make_ubuntu_packages.sh
+++ Packaging/ubuntu/Monteverdi/make_ubuntu_packages.sh
@@ -278,7 +278,7 @@ done
 
 echo "Source package generation..."
 cd "$TMPDIR/monteverdi-$src_version_full"
-for target in lucid oneiric precise quantal raring ; do
+for target in oneiric precise quantal raring ; do
     set_ubuntu_code_name "$target"
     echo "Package for $ubuntu_codename ($ubuntu_version)"
     cp -f "$DEBDIR/changelog" debian
