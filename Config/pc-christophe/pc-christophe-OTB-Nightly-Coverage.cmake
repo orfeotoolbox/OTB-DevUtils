a86905b0ce397f90d9caef14283b369424b0dda7
Manuel Grizonnet <manuel.grizonnet@orfeo-toolbox.org>
2013-06-26 16:54:36 +0200

    ENH: add coverage submission on pc-christophe.cnes.fr

---
+++ Config/pc-christophe/pc-christophe-OTB-Nightly-Coverage.cmake
@@ -0,0 +1,63 @@
+# Client maintainer: manuel.grizonnet@cnes.fr
+set(dashboard_model Nightly)
+set(CTEST_DASHBOARD_ROOT "/home/otbtesting/OTB")
+SET (CTEST_SITE "pc-christophe.cst.cnes.fr")
+set(CTEST_BUILD_CONFIGURATION Debug)
+set(CTEST_BUILD_NAME "Fedora17-64bits-${CTEST_BUILD_CONFIGURATION}")
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set(CTEST_BUILD_COMMAND "/usr/bin/make -j4 -i -k" )
+set(CTEST_COVERAGE_COMMAND "/usr/bin/gcov")
+set(CTEST_TEST_ARGS PARALLEL_LEVEL 4)
+set(CTEST_TEST_TIMEOUT 1500)
+set(CTEST_HG_COMMAND "/usr/bin/hg")
+
+
+set(dashboard_root_name "tests")
+set(dashboard_source_name "trunk/OTB")
+set(dashboard_binary_name "bin/OTB-Nightly-Coverage")
+
+#set(dashboard_fresh_source_checkout TRUE)
+set(dashboard_hg_url "http://hg.orfeo-toolbox.org/OTB-Nightly")
+set(dashboard_hg_branch "default")
+
+set(dashboard_do_coverage true)
+
+macro(dashboard_hook_init)
+set(dashboard_cache "${dashboard_cache}
+OTB_DATA_USE_LARGEINPUT:BOOL=ON
+OTB_DATA_LARGEINPUT_ROOT:STRING=/media/ssh/pc-inglada/media/TeraDisk2/LargeInput
+OTB_DATA_ROOT:STRING=/home/otbtesting/OTB/trunk/OTB-Data
+
+CMAKE_C_FLAGS:STRING=-g -O0  -fprofile-arcs -ftest-coverage  -Wall -Wno-uninitialized -Wno-unused-variable
+CMAKE_CXX_FLAGS:STRING=-g -O0  -fprofile-arcs -ftest-coverage -Wall -Wno-deprecated -Wno-uninitialized -Wno-unused-variable
+
+ITK_USE_PATENTED:BOOL=ON
+ITK_USE_REVIEW:BOOL=ON 
+ITK_USE_OPTIMIZED_REGISTRATION_METHODS:BOOL=ON 
+OTB_USE_PATENTED:BOOL=ON
+OTB_USE_PQXX:BOOL=OFF
+OTB_USE_CURL:BOOL=ON
+OTB_USE_EXTERNAL_BOOST:BOOL=ON
+OTB_USE_EXTERNAL_EXPAT:BOOL=ON
+OTB_USE_EXTERNAL_FLTK:BOOL=ON
+USE_FFTWD:BOOL=ON
+USE_FFTWF:BOOL=ON
+OTB_GL_USE_ACCEL:BOOL=OFF
+OTB_USE_MAPNIK:BOOL=ON
+
+OTB_USE_OPENCV:BOOL=ON
+
+BUILD_TESTING:BOOL=ON
+BUILD_EXAMPLES:BOOL=ON
+BUILD_APPLICATIONS:BOOL=ON
+OTB_WRAP_PYTHON:BOOL=ON
+PYTHON_EXECUTABLE:FILEPATH=/usr/bin/python
+OTB_WRAP_QT:BOOL=ON
+OTB_WRAP_JAVA:BOOL=ON
+
+OTB_USE_OPENCV:BOOL=ON
+
+    ")
+endmacro()
+
+include(${CTEST_SCRIPT_DIRECTORY}/../otb_common.cmake)
