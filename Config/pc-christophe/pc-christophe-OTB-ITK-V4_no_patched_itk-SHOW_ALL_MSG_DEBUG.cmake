2eb041b8d06b8038c1d48a8fff45c733d758bdfa
Manuel Grizonnet <manuel.grizonnet@orfeo-toolbox.org>
2013-07-08 10:18:29 +0200

    ENH:update OTB-ITKv4 repo and add submission on pc-christophe

--- Config/pc-christophe/pc-christophe-OTB-ITK-V4_no_patched_itk-SHOW_ALL_MSG_DEBUG.cmake
+++ Config/pc-christophe/pc-christophe-OTB-ITK-V4_no_patched_itk-SHOW_ALL_MSG_DEBUG.cmake
@@ -3,7 +3,7 @@ set(dashboard_model Experimental)
 set(CTEST_DASHBOARD_ROOT "/home/otbtesting/OTB")
 SET (CTEST_SITE "pc-christophe.cst.cnes.fr")
 set(CTEST_BUILD_CONFIGURATION Release)
-set(CTEST_BUILD_NAME "ITKv4-nopatch-Fedora17-64bits-${CTEST_BUILD_CONFIGURATION}")
+set(CTEST_BUILD_NAME "ITKv4-nopatch-Fedora17-64bits-${CTEST_BUILD_CONFIGURATION}-SHOW_ALL_MSG_DEBUG")
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
 set(CTEST_BUILD_COMMAND "/usr/bin/make -j4 -i -k" )
 set(CTEST_TEST_ARGS PARALLEL_LEVEL 4)
@@ -14,7 +14,7 @@ set(CTEST_HG_COMMAND "/usr/bin/hg")
 
 set(dashboard_root_name "tests")
 set(dashboard_source_name "trunk/OTB-ITKv4")
-set(dashboard_binary_name "bin/OTB-ITKv4-nopatch-${CTEST_BUILD_CONFIGURATION}")
+set(dashboard_binary_name "bin/OTB-ITKv4-nopatch-SHOW_ALL_MSG_DEBUG")
 
 set(dashboard_fresh_source_checkout ON)
 set(dashboard_hg_url "http://hg.orfeo-toolbox.org/OTB-ITKv4")
@@ -23,16 +23,32 @@ set(dashboard_hg_branch "default")
 macro(dashboard_hook_init)
   set(dashboard_cache "${dashboard_cache}
 
-CMAKE_C_FLAGS:STRING=-fPIC -Wall -Wshadow -Wno-uninitialized -Wno-unused-variable
-CMAKE_CXX_FLAGS:STRING=-fPIC -Wall -Wno-deprecated -Wno-uninitialized -Wno-unused-variable
-
 BUILD_TESTING:BOOL=ON
 BUILD_EXAMPLES:BOOL=ON
 BUILD_APPLICATIONS:BOOL=ON
-
 OTB_WRAP_PYTHON:BOOL=ON
-OTB_WRAP_JAVA:BOOL=ON
 OTB_WRAP_QT:BOOL=ON
+#OTB_WRAP_PYQT:BOOL=ON
+
+
+CMAKE_C_FLAGS:STRING= -Wall -Wno-uninitialized -Wno-unused-variable
+CMAKE_CXX_FLAGS:STRING= -Wall -Wno-deprecated -Wno-uninitialized -Wno-unused-variable
+
+OTB_SHOW_ALL_MSG_DEBUG:BOOL=ON
+
+ITK_USE_PATENTED:BOOL=ON
+ITK_USE_REVIEW:BOOL=ON 
+ITK_USE_OPTIMIZED_REGISTRATION_METHODS:BOOL=ON 
+OTB_USE_PATENTED:BOOL=ON
+OTB_USE_PQXX:BOOL=OFF
+OTB_USE_CURL:BOOL=ON
+OTB_USE_EXTERNAL_BOOST:BOOL=ON
+OTB_USE_EXTERNAL_EXPAT:BOOL=ON
+OTB_USE_EXTERNAL_FLTK:BOOL=ON
+USE_FFTWD:BOOL=ON
+USE_FFTWF:BOOL=ON
+OTB_GL_USE_ACCEL:BOOL=OFF
+OTB_USE_MAPNIK:BOOL=ON
 
 OTB_DATA_USE_LARGEINPUT:BOOL=ON
 OTB_DATA_LARGEINPUT_ROOT:STRING=/media/ssh/pc-inglada/media/TeraDisk2/LargeInput
