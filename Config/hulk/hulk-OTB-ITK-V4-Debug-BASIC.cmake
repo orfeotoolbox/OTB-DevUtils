b234e56541161fe83e96007191689f163114e1b9
Julien Malik <julien.malik@c-s.fr>
2013-07-09 15:56:13 +0200

    ENH: Add a new reference script for OTB-ITKv4

---
+++ Config/hulk/hulk-OTB-ITK-V4-Debug-BASIC.cmake
@@ -0,0 +1,70 @@
+# Client maintainer: julien.malik@c-s.fr
+set(dashboard_model Experimental)
+set(CTEST_DASHBOARD_ROOT "/home/otbval/Dashboard")
+set(CTEST_SITE "hulk.c-s.fr")
+set(CTEST_BUILD_CONFIGURATION Debug)
+set(CTEST_BUILD_NAME "ITKv4-Ubuntu10.04-64bits-${CTEST_BUILD_CONFIGURATION}-BASIC")
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set(CTEST_BUILD_COMMAND "/usr/bin/make -j9 -i -k" )
+set(CTEST_TEST_ARGS PARALLEL_LEVEL 4)
+set(CTEST_TEST_TIMEOUT 1500)
+set(CTEST_HG_COMMAND "/usr/bin/hg")
+set(CTEST_HG_UPDATE_OPTIONS "-C")
+
+set(dashboard_root_name "tests")
+set(dashboard_source_name "src/OTB-ITKv4")
+set(dashboard_binary_name "build/OTB-ITKv4-${CTEST_BUILD_CONFIGURATION}-BASIC")
+
+#set(dashboard_fresh_source_checkout TRUE)
+#set(dashboard_hg_url "https://bitbucket.org/julienmalik/otb-itkv4")
+set(dashboard_hg_url "http://hg.orfeo-toolbox.org/OTB-ITKv4")
+set(dashboard_hg_branch "default")
+
+#set(dashboard_no_test ON)
+#set(dashboard_no_submit ON)
+
+set(ENV{DISPLAY} ":0.0")
+
+macro(dashboard_hook_init)
+  set(dashboard_cache "${dashboard_cache}
+
+BUILD_TESTING:BOOL=ON
+BUILD_EXAMPLES:BOOL=ON
+BUILD_APPLICATIONS:BOOL=ON
+OTB_WRAP_PYTHON:BOOL=ON
+OTB_WRAP_QT:BOOL=ON
+#OTB_WRAP_PYQT:BOOL=ON
+
+
+CMAKE_C_FLAGS:STRING= -Wall -Wno-uninitialized -Wno-unused-variable
+CMAKE_CXX_FLAGS:STRING= -Wall -Wno-deprecated -Wno-uninitialized -Wno-unused-variable
+
+#OTB_SHOW_ALL_MSG_DEBUG:BOOL=ON
+
+OTB_DATA_USE_LARGEINPUT:BOOL=OFF
+OTB_DATA_LARGEINPUT_ROOT:STRING=$ENV{HOME}/Data/OTB-LargeInput
+OTB_DATA_ROOT:STRING=$ENV{HOME}/Dashboard/src/OTB-Data
+
+OTB_USE_EXTERNAL_ITK:BOOL=ON
+ITK_DIR:PATH=${CTEST_DASHBOARD_ROOT}/build/ITKv4-upstream-${CTEST_BUILD_CONFIGURATION}
+
+ITK_USE_PATENTED:BOOL=ON
+ITK_USE_REVIEW:BOOL=ON 
+ITK_USE_OPTIMIZED_REGISTRATION_METHODS:BOOL=ON 
+OTB_USE_PATENTED:BOOL=ON
+OTB_USE_PQXX:BOOL=OFF
+OTB_USE_CURL:BOOL=ON
+OTB_USE_EXTERNAL_BOOST:BOOL=ON
+OTB_USE_EXTERNAL_EXPAT:BOOL=ON
+OTB_USE_EXTERNAL_FLTK:BOOL=ON
+USE_FFTWD:BOOL=ON
+USE_FFTWF:BOOL=ON
+OTB_GL_USE_ACCEL:BOOL=OFF
+OTB_USE_MAPNIK:BOOL=ON
+
+#OTB_USE_OPENCV:BOOL=ON
+#OpenCV_DIR:PATH=/home/otbval/tools/install/opencv-2.4.5/share/OpenCV
+    ")
+endmacro()
+
+include(${CTEST_SCRIPT_DIRECTORY}/../otb_common.cmake)
