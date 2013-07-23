272957ddbe5fea3fefd45a962ecee672e67e72d6
Manuel Grizonnet <manuel.grizonnet@orfeo-toolbox.org>
2013-07-23 10:20:59 +0200

    ENH: update Testing submission using installed OTB-ITKv4 with internal ITK

--- Config/pc-christophe/pc-christophe-OTB-ITK-V4_no_patched_itk-Testing.cmake
+++ Config/pc-christophe/pc-christophe-OTB-ITK-V4_no_patched_itk-Testing.cmake
@@ -1,75 +1,19 @@
 # Client maintainer: manuel.grizonnet@cnes.fr
-set(dashboard_model Experimental)
-set(CTEST_DASHBOARD_ROOT "/home/otbtesting/OTB")
-SET (CTEST_SITE "pc-christophe.cst.cnes.fr")
-set(CTEST_BUILD_CONFIGURATION Debug)
-set(CTEST_BUILD_NAME "ITKv4-nopatch-Internal-Fedora17-64bits-Install-Testing-${CTEST_BUILD_CONFIGURATION}")
-set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
-set(CTEST_BUILD_COMMAND "/usr/bin/make -j4 -i -k" )
-set(CTEST_TEST_ARGS PARALLEL_LEVEL 4)
-set(CTEST_TEST_TIMEOUT 1500)
 
-set(CTEST_HG_COMMAND "/usr/bin/hg")
-#set(CTEST_HG_UPDATE_OPTIONS "-r otb-itkv4")
+SET(MAKECOMMAND "/usr/bin/make -i -k -j 4" CACHE STRING "" FORCE)
+# Name of the build
+SET(BUILDNAME "ITKv4-nopatch-Internal-Fedora17-64bits-Install-Testing-Debug" CACHE STRING "" FORCE)
+# Name of the computer/site where compile is being run
+SET(SITE "pc-christophe.cst.cnes.fr" CACHE STRING "" FORCE)
 
-set(dashboard_root_name "tests")
-set(dashboard_source_name "trunk/OTB-ITKv4/Testing")
-set(dashboard_binary_name "bin/OTB-ITKv4-Testing")
+# Compilation options
+SET(CMAKE_C_FLAGS "-Wall -Wno-uninitialized -Wno-unused-variable" CACHE STRING "" FORCE)
+SET(CMAKE_CXX_FLAGS "-Wall -Wno-deprecated -Wno-uninitialized -Wno-unused-variable" CACHE STRING "" FORCE)
 
-set(dashboard_fresh_source_checkout OFF)
-set(dashboard_hg_url "http://hg.orfeo-toolbox.org/OTB-ITKv4/Testing")
-set(dashboard_hg_branch "default")
-
-#set (OTB_INSTALL_PREFIX ${CTEST_DASHBOARD_ROOT}/bin/OTB-ITKv4-nopatch-SHOW_ALL_MSG_DEBUG-INSTALL/)
-
-macro(dashboard_hook_init)
-  set(dashboard_cache "${dashboard_cache}
-
-OTB_DIR:STRING=${CTEST_DASHBOARD_ROOT}/bin/OTB-ITKv4-nopatch-SHOW_ALL_MSG_DEBUG-INSTALL/
-
-#BUILD_TESTING:BOOL=ON
-#BUILD_EXAMPLES:BOOL=ON
-#BUILD_APPLICATIONS:BOOL=ON
-#OTB_WRAP_PYTHON:BOOL=ON
-#OTB_WRAP_QT:BOOL=ON
-#OTB_WRAP_PYQT:BOOL=ON
-
-
-CMAKE_C_FLAGS:STRING= -Wall -Wno-uninitialized -Wno-unused-variable
-CMAKE_CXX_FLAGS:STRING= -Wall -Wno-deprecated -Wno-uninitialized -Wno-unused-variable
-
-#OTB_SHOW_ALL_MSG_DEBUG:BOOL=ON
-
-#ITK_USE_PATENTED:BOOL=ON
-#ITK_USE_REVIEW:BOOL=ON 
-#ITK_USE_OPTIMIZED_REGISTRATION_METHODS:BOOL=ON 
-#OTB_USE_PATENTED:BOOL=ON
-#OTB_USE_PQXX:BOOL=OFF
-#OTB_USE_CURL:BOOL=ON
-#OTB_USE_EXTERNAL_BOOST:BOOL=ON
-#OTB_USE_EXTERNAL_EXPAT:BOOL=ON
-#OTB_USE_EXTERNAL_FLTK:BOOL=ON
-#USE_FFTWD:BOOL=ON
-#USE_FFTWF:BOOL=ON
-#OTB_GL_USE_ACCEL:BOOL=OFF
-#OTB_USE_MAPNIK:BOOL=ON
-
-OTB_DATA_USE_LARGEINPUT:BOOL=ON
-OTB_DATA_LARGEINPUT_ROOT:STRING=/media/ssh/pc-inglada/media/TeraDisk2/LargeInput
-OTB_DATA_ROOT:STRING=/home/otbtesting/OTB/trunk/OTB-ITKv4-Data
-
-#OTB_USE_EXTERNAL_ITK:BOOL=OFF
-#ITK_DIR:PATH=$ENV{HOME}/OTB/bin/ITKv4-upstream-Release
-
-#OTB_USE_CURL:BOOL=ON
-#OTB_USE_PQXX:BOOL=OFF
-#OTB_USE_PATENTED:BOOL=OFF
-#OTB_USE_EXTERNAL_BOOST:BOOL=ON
-#OTB_USE_EXTERNAL_EXPAT:BOOL=ON
-#OTB_USE_EXTERNAL_FLTK:BOOL=ON
-#OTB_USE_MAPNIK:BOOL=OFF
-
-    ")
-endmacro()
-
-include(${CTEST_SCRIPT_DIRECTORY}/../otb_common.cmake)
+SET(OTB_DATA_USE_LARGEINPUT ON CACHE BOOL "" FORCE)
+SET(OTB_DATA_LARGEINPUT_ROOT "/media/ssh/pc-inglada/media/TeraDisk2/LargeInput" CACHE STRING "" FORCE)
+# Data root
+SET(OTB_DATA_ROOT "$ENV{HOME}/OTB/trunk/OTB-ITKv4-Data" CACHE STRING "" FORCE)
+# Set up the build options
+SET(CMAKE_BUILD_TYPE "Debug" CACHE STRING "" FORCE)
+SET(OTB_DIR "$ENV{HOME}/OTB/bin/OTB-ITKv4-nopatch-SHOW_ALL_MSG_DEBUG-INSTALL" CACHE STRING "" FORCE)
