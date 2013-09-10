5672a7e8404a15e5f3d5eda2d0613dc13a96b5ce
Manuel Grizonnet <manuel.grizonnet@orfeo-toolbox.org>
2013-09-10 16:58:25 +0200

    ENH: several improvments in fusion phr script
    - add ram,dem,geoid parameter
    - add an new step to orthorectify the fusion in utm
    - set default file path for zoom,fusion
    - better management of path concatenation
    - cast nb_lig and nb_col in int not float
    - copy D. Greslou mail with detail of pan/xs shift  as comment

--- Scripts/fusion_phr.py
+++ Scripts/fusion_phr.py
@@ -5,9 +5,67 @@ from optparse import OptionParser
 import xml.etree.ElementTree as ET
 import glob
 
+###########################################################################################################################
+# Les infos importantes a aller lire dans les XML sont:
 
-def Execute(directory):
-    path=directory + "/" + "IMG*PHR*_*" + "/" + "DIM_PHR*.XML"
+#       <Time>
+
+#         <Time_Range>
+
+#           <START>2012-04-01T10:17:34.3190000Z</START>                                => T_i_MS / T_i_P
+
+#           <END>2012-04-01T10:17:36.9860000Z</END>                                                        => T_f_MS / T_f_P
+
+#         </Time_Range>
+
+#         <Time_Stamp>
+
+#          <LINE_PERIOD>0.2968</LINE_PERIOD>                                                                   => Te_MS / Te_P (normalement Te_P = 4 x Te_MS)
+
+#         </Time_Stamp>
+
+#       </Time>
+
+# et  :
+
+#     <Raster_Dimensions>
+
+#       <NROWS>8986</NROWS>                                                                                                 => NB_lig_MS / NB_lig_P
+
+#       <NCOLS>10375</NCOLS>                                                                                                   => NB_col_MS / NB_col_P
+
+#       <NBANDS>4</NBANDS>
+
+# et :
+
+#           <Swath_Range>
+
+#             <FIRST_COL>1</FIRST_COL>                                                                                      => col_i_MS / col_i_P
+
+#             <LAST_COL>10375</LAST_COL>                                                                                => col_f_MS / col_f_P
+
+#           </Swath_Range>
+
+
+# Le decalage en ligne (en pixels PAN) entre les 2 images est:
+
+# dec_lig_MS_P_pixPAN =  int ((T_i_MS - T_i_PA)/ (Te_P/1000))              car le Te est donne en ms.
+
+# Le decalage en colonne (en pixel PAN) entre les 2 images est:
+
+# dec_col_MS_P_pixPAN = int((col_i_MS*4 - col_i_P))
+
+# Il faut donc supprimer ces lignes et colonnes en trop en haut et a gauche de l'image puis en extraire ncol / nlig (sur l'image XS) et 4 x ncol / 4 x nlig (sur l'image PA)
+
+# Les extraits ainsi elabores sont alors superposables au zoom 4 pres.
+
+# => en revanche, les RPC ne sont plus bons si tu as decoupe les images.
+#########################################################################################################
+
+
+def Execute(directory,ram,dem,geoid):
+
+    path=os.path.join(os.path.join(directory,"IMG*PHR*_*"),"DIM_PHR*.XML")
 
     dimap_files = glob.glob(path)
 
@@ -29,7 +87,7 @@ def Execute(directory):
         root = tree.getroot()
         
         print "dir",os.path.dirname(dimap_files[count])
-        jp2_path=os.path.dirname(dimap_files[count]) +  "/" + "*.JP2"
+        jp2_path=os.path.join(os.path.dirname(dimap_files[count]),"*.JP2")
         
         jp2_file=glob.glob(jp2_path)
         print jp2_file[0]
@@ -53,26 +111,9 @@ def Execute(directory):
         dict['path'] = jp2_file[0]
         dict['T_i'] = float(T_i)
         dict['T_e']= float(T_e)
-        dict['nb_lig'] = float(NB_lig)
-        dict['nb_col'] = float(NB_col)
+        dict['nb_lig'] = int(NB_lig)
+        dict['nb_col'] = int(NB_col)
         dict['col_i'] = float(col_i)
-
-        #Now get image size for in
-        #path
-        #print "directory", os.path.dirname(dimap_files[count])
-        
-        # img_path = os.path.dirname(dimap_files[count]) + "/" + "*.JP2"
-        # img = glob.glob(img_path)
-        # readinfo = otbApplication.Registry.CreateApplication("ReadImageInfo")
-        # readinfo.SetParameterString("in",img[0])
-
-        # readinfo.ExecuteAndWriteOutput()
-        # #print readinfo.GetParametersKeys()
-        # sizex = readinfo.GetParameterInt("sizex")
-        # sizey = readinfo.GetParameterInt("sizey")
-
-        # tab[count][5] = sizex
-        # tab[count][6] = sizey
 
         tabs.append(dict)
         print "values for input dimap file ", dict
@@ -83,19 +124,19 @@ def Execute(directory):
     print "Le decalage en ligne (en pixels PAN) entre les 2 images est:", dec_lig_MS_P_pixPAN
     print "Le decalage en colonne (en pixels PAN) entre les 2 images est:", dec_col_MS_P_pixPAN
 
-    # print "Ligne de commande pour re-echantilloner le XS: "
-    # print 'otbcli_RigidTransformResample -in ... -transform.type translation -transform.type.translation.tx '+str(-1+dec_col_MS_P_pixPAN/4.+0.375)+' -transform.type.translation.ty '+str(dec_lig_MS_P_pixPAN/4.+0.375)+' -transform.type.translation.scalex 4 -transform.type.translation.scaley 4 -out xs_zoomed.tif?&gdal:co:TILED=yes&gdal:co:NBITS=12&box=0:0:'+str(tabs[0]['nb_col'])+':'+str(tabs[0]['nb_lig'])+' uint16'
-    # print "\n"
-    # print "Ligne de commande pour fusionner les images: "
-    # print "otbcli_Pansharpening -inp ... -inxs xs.tif -out ... uint16"
-
     print "pan path", tabs[0]['path']
     print "xs path", tabs[1]['path']
+
+    xs_without_extension=os.path.splitext(os.path.basename(tabs[1]['path']))[0]
+    xs_zoom_path = os.path.join(directory,xs_without_extension) + "_zoom.tif"
     
+    print "xs_zoom_path" , xs_zoom_path 
+    
+    #FIXME local path to dev version of this otb app
     zoom_app="/home/grizonnetm/projets/otb/bin/release/OTB/bin/otbApplicationLauncherCommandLine RigidTransformResample /home/grizonnetm/projets/otb/bin/release/OTB/bin/"
     #zoom_app="otbcli_RigidTransformResample"
 
-    zoom_cmd = zoom_app + " -in " + str(tabs[1]['path']) + " -transform.type translation -transform.type.translation.tx "+str(-1+dec_col_MS_P_pixPAN/4.+0.375)+" -transform.type.translation.ty "+str(dec_lig_MS_P_pixPAN/4.+0.375)+" -transform.type.translation.scalex 4 -transform.type.translation.scaley 4 -out \"xs_zoomed.tif?&gdal:co:TILED=yes&gdal:co:NBITS=12&box=0:0:"+str(tabs[0]['nb_col'])+":"+str(tabs[0]['nb_lig'])+"\"" + " uint16 -ram 512"
+    zoom_cmd = zoom_app + " -in " + str(tabs[1]['path']) + " -transform.type translation -transform.type.translation.tx "+str(-1+dec_col_MS_P_pixPAN/4.+0.375)+" -transform.type.translation.ty "+str(dec_lig_MS_P_pixPAN/4.+0.375)+" -transform.type.translation.scalex 4 -transform.type.translation.scaley 4 -out \"" + xs_zoom_path + "?&gdal:co:TILED=yes&gdal:co:NBITS=12&box=0:0:"+str(tabs[0]['nb_col'])+":"+str(tabs[0]['nb_lig'])+"\"" + " uint16 -ram " + ram
 
     print zoom_cmd
     status,output = commands.getstatusoutput(zoom_cmd)
@@ -106,7 +147,11 @@ def Execute(directory):
         
         print output
 
-    fusion_cmd = "otbcli_Pansharpening " + "-inp " + tabs[0]['path'] + " -inxs " + " xs_zoomed.tif -out \"fusion.tif?&gdal:co:TILED=yes&gdal:co:NBITS=12\" uint16 -ram 512"
+    
+    fusion_path = os.path.join(directory,os.path.basename(directory)) + "_pxs.tif" 
+    print "fusion_path" , fusion_path 
+
+    fusion_cmd = "otbcli_Pansharpening " + "-inp " + tabs[0]['path'] + " -inxs " + xs_zoom_path + " -out \"" + fusion_path + "?&gdal:co:TILED=yes&gdal:co:NBITS=12\" uint16 -ram " + ram
     
     print fusion_cmd
     status,output = commands.getstatusoutput(fusion_cmd)
@@ -117,22 +162,41 @@ def Execute(directory):
         
         print output
 
+
+    ortho_path = os.path.join(directory,os.path.basename(directory)) + "_ortho_pxs.tif"
+    
+    #orthorectification
+    dem_option= " -elev.dem " + dem 
+    geoid_option= " -elev.geoid " + geoid
+    ortho_cmd = "otbcli_OrthoRectification -io.in " + fusion_path + " -io.out \"" + ortho_path + "?&gdal:co:TILED=yes&gdal:co:NBITS=12\" uint16 -outputs.mode auto -outputs.spacingx 0.5 -outputs.spacingy -0.5" + dem_path + geoid_path + " -opt.ram " + ram
+
+    print "ortho_cmd", ortho_cmd
+    status,output = commands.getstatusoutput(fusion_cmd)
+
+    if not status == 0:
+        print "Error"
+        print "Command: "+ortho_cmd
+        
+        print output
+
 def main():
     parser = OptionParser(usage="usage: %prog [options] filename",
                           version="%prog 1.0")
 
-    parser.add_option("-d","--directory", help="absolute path to bundle product directory", dest="path", type="string")
-    #parser.add_option("-r","--ram", help="ram value", dest="ram", type="int")
+    parser.add_option("-p","--path_bundle", help="absolute path to bundle product directory", dest="img_path", type="string")
+    parser.add_option("-r","--ram", help="ram value", dest="ram", type="string", default=512)
+    parser.add_option("-d","--dem", help="dem directory", dest="dem", type="string")
+    parser.add_option("-g","--geoid", help="geoid file", dest="geoid", type="string")
 
     (opts, args) = parser.parse_args()
 
-    if opts.path is None:
+    if opts.img_path is None or opts.dem is None or opts.geoid is None: 
         print "A mandatory option is missing\n"
         parser.print_help()
         exit(-1)
     else:
         #print opts.path
-        Execute(opts.path)
+        Execute(os.path.dirname(opts.img_path),opts.ram,os.path.dirname(opts.dem),opts.geoid)
 
 if __name__ == '__main__':
     main()
