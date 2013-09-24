2d0b4808a9345dc21a0a6b58a332607ec3aa1cae
Stephane Albert <stephane.albert@c-s.fr>
2013-09-24 10:28:27 +0200

    ENH: Updated OTB stable and Monteverdi2 testing .cmake nighlty scripts.

---
+++ Config/raoul/raoul-Win7-Monteverdi2-Release-VC2010-testing.cmake
@@ -0,0 +1,53 @@
+SET (CTEST_SOURCE_DIRECTORY "C:/Users/jmalik/Dashboard/src/Monteverdi2")
+SET (CTEST_BINARY_DIRECTORY "C:/Users/jmalik/Dashboard/build/Monteverdi2-Release-VC2010-testing")
+
+SET (CTEST_CMAKE_GENERATOR  "Visual Studio 10" )
+SET (CTEST_CMAKE_COMMAND "C:/Program Files (x86)/CMake 2.8/bin/cmake.exe")
+SET (CTEST_SITE "raoul.c-s.fr" )
+SET (CTEST_BUILD_NAME "Win7-Visual2010-Release-Static-testing")
+SET (CTEST_BUILD_CONFIGURATION "Release")
+SET (CTEST_HG_COMMAND "C:/Program Files (x86)/Mercurial/hg.exe")
+#SET (CTEST_HG_UPDATE_OPTIONS "-C")
+
+SET (OTB_INITIAL_CACHE "
+BUILDNAME:STRING=${CTEST_BUILD_NAME}
+SITE:STRING=${CTEST_SITE}
+CMAKE_INSTALL_PREFIX:PATH=C:/Users/jmalik/Dashboard/install/Monteverdi2-Release-VC2010-testing
+
+CMAKE_BUILD_TYPE:STRING=${CTEST_BUILD_CONFIGURATION}
+
+CMAKE_INCLUDE_PATH:PATH=$ENV{OSGEO4W_ROOT}/include
+CMAKE_LIBRARY_PATH:PATH=$ENV{OSGEO4W_ROOT}/lib
+
+BUILD_TESTING:BOOL=ON
+Monteverdi2_USE_CPACK:BOOL=ON
+
+OTB_DIR:PATH=C:/Users/jmalik/Dashboard/install/OTB-Release-VC2010-stable
+
+ICUUC_INCLUDE_DIR:PATH=C:/Program Files (x86)/icu4c-4_2_1-Win32-msvc9/icu/include
+ICUUC_LIBRARY:FILEPATH=C:/Program Files (x86)/icu4c-4_2_1-Win32-msvc9/icu/lib/icuuc.lib
+LTDL_INCLUDE_DIR:PATH=C:/Program Files (x86)/GnuWin32/include
+LTDL_LIBRARY:FILEPATH=C:/Program Files (x86)/GnuWin32/lib/ltdl.lib
+
+OpenCV_DIR:PATH=C:/OSGeo4W/share/OpenCV
+")
+
+#Remove install dir
+execute_process(COMMAND ${CTEST_CMAKE_COMMAND} -E remove_directory C:/Users/jmalik/Dashboard/install/Monteverdi2-Release-VC2010-testing)
+execute_process(COMMAND ${CTEST_CMAKE_COMMAND} -E make_directory C:/Users/jmalik/Dashboard/install/Monteverdi2-Release-VC2010-testing)
+
+ctest_empty_binary_directory (${CTEST_BINARY_DIRECTORY})
+
+SET (CTEST_NOTES_FILES
+${CTEST_SCRIPT_DIRECTORY}/${CTEST_SCRIPT_NAME}
+${CTEST_BINARY_DIRECTORY}/CMakeCache.txt
+)
+
+ctest_start(Nightly)
+ctest_update(SOURCE "${CTEST_SOURCE_DIRECTORY}")
+file(WRITE "${CTEST_BINARY_DIRECTORY}/CMakeCache.txt" ${OTB_INITIAL_CACHE})
+ctest_configure (BUILD "${CTEST_BINARY_DIRECTORY}")
+ctest_read_custom_files(${CTEST_BINARY_DIRECTORY})
+ctest_build (BUILD "${CTEST_BINARY_DIRECTORY}" TARGET PACKAGE)
+ctest_test (BUILD "${CTEST_BINARY_DIRECTORY}")
+ctest_submit ()
