7c6dc5a88a7bc2af1a57c0fcb07b0993bf339077
Mickael Savinaud <mickael.savinaud@c-s.fr>
2013-10-30 16:36:33 +0100

    ENH: update raoul config

--- Config/raoul/raoul-Win7-OTB-RelWithDebInfo-VC2010.cmake
+++ Config/raoul/raoul-Win7-OTB-RelWithDebInfo-VC2010.cmake
@@ -1,18 +1,19 @@
+SET (CTEST_BUILD_CONFIGURATION "RelWithDebInfo")
+
 SET (CTEST_SOURCE_DIRECTORY "C:/Users/jmalik/Dashboard/src/OTB")
-SET (CTEST_BINARY_DIRECTORY "C:/Users/jmalik/Dashboard/build/OTB-RelWithDebInfo-VC2010")
+SET (CTEST_BINARY_DIRECTORY "C:/Users/jmalik/Dashboard/build/OTB-${CTEST_BUILD_CONFIGURATION}-VC2010")
 
 SET (CTEST_CMAKE_GENERATOR  "Visual Studio 10" )
 SET (CTEST_CMAKE_COMMAND "C:/Program Files (x86)/CMake 2.8/bin/cmake.exe")
 SET (CTEST_SITE "raoul.c-s.fr" )
-SET (CTEST_BUILD_NAME "Win7-Visual2010-RelWithDebInfo-Static")
-SET (CTEST_BUILD_CONFIGURATION "RelWithDebInfo")
+SET (CTEST_BUILD_NAME "Win7-Visual2010-${CTEST_BUILD_CONFIGURATION}-Static")
 SET (CTEST_HG_COMMAND "C:/Program Files (x86)/Mercurial/hg.exe")
 SET (CTEST_HG_UPDATE_OPTIONS "-C")
 
 SET (OTB_INITIAL_CACHE "
 BUILDNAME:STRING=${CTEST_BUILD_NAME}
 SITE:STRING=${CTEST_SITE}
-CMAKE_INSTALL_PREFIX:PATH=C:/Users/jmalik/Dashboard/install/OTB-RelWithDebInfo-VC2010
+CMAKE_INSTALL_PREFIX:PATH=C:/Users/jmalik/Dashboard/install/OTB-${CTEST_BUILD_CONFIGURATION}-VC2010
 
 BUILD_TESTING:BOOL=ON
 BUILD_EXAMPLES:BOOL=ON
@@ -75,6 +76,11 @@ PYTHON_INCLUDE_DIR:PATH=C:/OSGeo4W/apps/Python27/include
 PYTHON_LIBRARY:FILEPATH=C:/OSGeo4W/apps/Python27/libs/python27.lib
 
 ")
+
+
+#remove install dir
+execute_process(COMMAND ${CTEST_CMAKE_COMMAND} -E remove_directory C:/Users/jmalik/Dashboard/install/OTB-${CTEST_BUILD_CONFIGURATION}-VC2010)
+execute_process(COMMAND ${CTEST_CMAKE_COMMAND} -E make_directory C:/Users/jmalik/Dashboard/install/OTB-${CTEST_BUILD_CONFIGURATION}-VC2010)
 
 #remove build dir
 ctest_empty_binary_directory (${CTEST_BINARY_DIRECTORY})
