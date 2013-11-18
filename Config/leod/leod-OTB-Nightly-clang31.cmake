b7f7a5592ef85a466bef83440be7de76ee45a30b
Charles Peyrega <charles.peyrega@c-s.fr>
2013-11-18 20:05:41 +0100

    ENH: Update OTB scripts for leod

--- Config/leod/leod-OTB-Nightly-clang31.cmake
+++ Config/leod/leod-OTB-Nightly-clang31.cmake
@@ -1,80 +1,71 @@
-SET (ENV{DISPLAY} ":0")
+# Client maintainer: julien.malik@c-s.fr
+set(dashboard_model Nightly)
+set(CTEST_DASHBOARD_ROOT "$ENV{HOME}/Dashboard")
+set(CTEST_SITE "leod.c-s.fr")
+set(CTEST_BUILD_CONFIGURATION Release)
+set(CTEST_BUILD_NAME "MacOSX10.8-${CTEST_BUILD_CONFIGURATION}-clang31")
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set(CTEST_BUILD_COMMAND "/usr/bin/make -j8 -i -k" )
+set(CTEST_TEST_ARGS PARALLEL_LEVEL 4)
+set(CTEST_TEST_TIMEOUT 500)
 
-SET (ENV{CC}  "/usr/bin/clang")
-SET (ENV{CXX} "/usr/bin/clang++")
+set(CTEST_HG_COMMAND "/opt/local/bin/hg")
+set(CTEST_HG_UPDATE_OPTIONS "-C")
 
-SET (CTEST_SOURCE_DIRECTORY "/Users/otbval/Dashboard/nightly/OTB-Release-clang31/src")
-SET (CTEST_BINARY_DIRECTORY "/Users/otbval/Dashboard/nightly/OTB-Release-clang31/build")
+string(TOLOWER ${dashboard_model} lcdashboard_model)
 
-SET( CTEST_CMAKE_GENERATOR  "Unix Makefiles" )
-SET (CTEST_CMAKE_COMMAND "cmake" )
-SET (CTEST_BUILD_COMMAND "/usr/bin/make -j8 -i -k" )
-SET (CTEST_SITE "leod.c-s.fr")
-SET (CTEST_BUILD_NAME "MacOSX10.8-Release-clang31")
-SET (CTEST_BUILD_CONFIGURATION "Release")
-SET (CTEST_HG_COMMAND "/opt/local/bin/hg")
-SET (CTEST_HG_UPDATE_OPTIONS "-C")
-SET (CTEST_USE_LAUNCHERS ON)
+set(dashboard_root_name "tests")
+set(dashboard_source_name "${lcdashboard_model}/OTB-${CTEST_BUILD_CONFIGURATION}-clang31/src")
+set(dashboard_binary_name "${lcdashboard_model}/OTB-${CTEST_BUILD_CONFIGURATION}-clang31/build")
+
+#set(dashboard_fresh_source_checkout OFF)
+set(dashboard_hg_url "http://hg.orfeo-toolbox.org/OTB-Nightly")
+set(dashboard_hg_branch "default")
+
+set(ENV{DISPLAY} ":0.0")
+
+set(ENV{CC}  "/usr/bin/clang")
+set(ENV{CXX} "/usr/bin/clang++")
 
 
-SET (CTEST_INITIAL_CACHE "
-BUILDNAME:STRING=${CTEST_BUILD_NAME}
-SITE:STRING=${CTEST_SITE}
-CTEST_USE_LAUNCHERS:BOOL=ON
-
+macro(dashboard_hook_init)
+  set(dashboard_cache "${dashboard_cache}
+  
 CMAKE_LIBRARY_PATH:PATH=/opt/local/lib
 CMAKE_INCLUDE_PATH:PATH=/opt/local/include
-
-OTB_DATA_USE_LARGEINPUT:BOOL=OFF
-OTB_DATA_LARGEINPUT_ROOT:STRING=/Users/otbval/Data/OTB-LargeInput
-OTB_DATA_ROOT:STRING=$ENV{HOME}/Data/OTB-Data
-
-CMAKE_C_FLAGS:STRING= -Wall -Wno-uninitialized -Wno-unused-variable
-CMAKE_CXX_FLAGS:STRING= -Wall -Wno-deprecated -Wno-uninitialized -Wno-unused-variable -Wno-gnu -Wno-overloaded-virtual
+  
+CMAKE_C_FLAGS:STRING= -fPIC -Wall -Wno-deprecated -Wno-uninitialized -Wno-unused-variable
+CMAKE_CXX_FLAGS:STRING= -fPIC -Wall -Wno-deprecated -Wno-uninitialized -Wno-unused-variable -Wno-gnu -Wno-overloaded-virtual
 
 #CMAKE_OSX_ARCHITECTURES:STRING=i386
 OPENTHREADS_CONFIG_HAS_BEEN_RUN_BEFORE:BOOL=ON
 
-CMAKE_BUILD_TYPE:STRING=Release
 BUILD_TESTING:BOOL=ON
 BUILD_EXAMPLES:BOOL=OFF
 BUILD_APPLICATIONS:BOOL=ON
+BUILD_BUG_TRACKER_TESTS:BOOL=OFF
+
 OTB_WRAP_PYTHON:BOOL=ON
-OTB_WRAP_QT:BOOL=ON
-#OTB_WRAP_PYQT:BOOL=ON
 OTB_WRAP_JAVA:BOOL=ON
+OTB_WRAP_QT:BOOL=ON
+
+OTB_DATA_USE_LARGEINPUT:BOOL=OFF
+#OTB_DATA_LARGEINPUT_ROOT:STRING=$ENV{HOME}/Data/OTB-LargeInput
+OTB_DATA_ROOT:STRING=$ENV{HOME}/Data/OTB-Data
+
+OTB_USE_EXTERNAL_ITK:BOOL=ON
+ITK_DIR:PATH=${CTEST_DASHBOARD_ROOT}/itkv4/build
 
 OTB_USE_CURL:BOOL=ON
 OTB_USE_PQXX:BOOL=OFF
-ITK_USE_PATENTED:BOOL=ON
-OTB_USE_PATENTED:BOOL=ON
+OTB_USE_PATENTED:BOOL=OFF
 OTB_USE_EXTERNAL_BOOST:BOOL=ON
 OTB_USE_EXTERNAL_EXPAT:BOOL=ON
 OTB_USE_EXTERNAL_FLTK:BOOL=ON
-OTB_USE_GETTEXT:BOOL=OFF
-USE_FFTWD:BOOL=OFF
-USE_FFTWF:BOOL=OFF
-OTB_GL_USE_ACCEL:BOOL=OFF
-ITK_USE_REVIEW:BOOL=ON 
-ITK_USE_OPTIMIZED_REGISTRATION_METHODS:BOOL=ON 
 OTB_USE_MAPNIK:BOOL=OFF
-CMAKE_INSTALL_PREFIX:STRING=/Users/otbval/Dashboard/nightly/OTB-Release-clang31/install
+OTB_USE_OPENCV:BOOL=ON
 
-")
+    ")
+endmacro()
 
-
-SET (CTEST_NOTES_FILES
-${CTEST_SCRIPT_DIRECTORY}/${CTEST_SCRIPT_NAME}
-${CTEST_BINARY_DIRECTORY}/CMakeCache.txt
-)
-
-ctest_empty_binary_directory (${CTEST_BINARY_DIRECTORY})
-ctest_start(Nightly)
-ctest_update(SOURCE "${CTEST_SOURCE_DIRECTORY}")
-file(WRITE "${CTEST_BINARY_DIRECTORY}/CMakeCache.txt" ${CTEST_INITIAL_CACHE})
-ctest_configure (BUILD "${CTEST_BINARY_DIRECTORY}")
-ctest_read_custom_files(${CTEST_BINARY_DIRECTORY})
-ctest_build (BUILD "${CTEST_BINARY_DIRECTORY}")
-ctest_test (BUILD "${CTEST_BINARY_DIRECTORY}" PARALLEL_LEVEL 4 INCLUDE Tu)
-ctest_submit ()
-
+include(${CTEST_SCRIPT_DIRECTORY}/../otb_common.cmake)
--- Config/leod/leod-OTB-Nightly-gcc46.cmake
+++ Config/leod/leod-OTB-Nightly-gcc46.cmake
@@ -1,80 +1,71 @@
-SET (ENV{DISPLAY} ":0")
-SET (ENV{CC} "/opt/local/bin/gcc-mp-4.6")
-SET (ENV{CXX} "/opt/local/bin/g++-mp-4.6")
+# Client maintainer: julien.malik@c-s.fr
+set(dashboard_model Nightly)
+set(CTEST_DASHBOARD_ROOT "$ENV{HOME}/Dashboard")
+set(CTEST_SITE "leod.c-s.fr")
+set(CTEST_BUILD_CONFIGURATION Release)
+set(CTEST_BUILD_NAME "MacOSX10.8-${CTEST_BUILD_CONFIGURATION}-gcc46")
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set(CTEST_BUILD_COMMAND "/usr/bin/make -j8 -i -k" )
+set(CTEST_TEST_ARGS PARALLEL_LEVEL 4)
+set(CTEST_TEST_TIMEOUT 500)
 
-SET (CTEST_SOURCE_DIRECTORY "/Users/otbval/Dashboard/nightly/OTB-Release-gcc46/src")
-SET (CTEST_BINARY_DIRECTORY "/Users/otbval/Dashboard/nightly/OTB-Release-gcc46/build")
+set(CTEST_HG_COMMAND "/opt/local/bin/hg")
+set(CTEST_HG_UPDATE_OPTIONS "-C")
 
-SET( CTEST_CMAKE_GENERATOR  "Unix Makefiles" )
-SET (CTEST_CMAKE_COMMAND "cmake" )
-SET (CTEST_BUILD_COMMAND "/usr/bin/make -j8 -i -k" )
-SET (CTEST_SITE "leod.c-s.fr")
-SET (CTEST_BUILD_NAME "MacOSX10.8-Release-gcc46")
-SET (CTEST_BUILD_CONFIGURATION "Release")
-SET (CTEST_HG_COMMAND "/opt/local/bin/hg")
-SET (CTEST_HG_UPDATE_OPTIONS "-C")
-SET (CTEST_USE_LAUNCHERS ON)
+string(TOLOWER ${dashboard_model} lcdashboard_model)
+
+set(dashboard_root_name "tests")
+set(dashboard_source_name "${lcdashboard_model}/OTB-${CTEST_BUILD_CONFIGURATION}-gcc46/src")
+set(dashboard_binary_name "${lcdashboard_model}/OTB-${CTEST_BUILD_CONFIGURATION}-gcc46/build")
+
+#set(dashboard_fresh_source_checkout OFF)
+set(dashboard_hg_url "http://hg.orfeo-toolbox.org/OTB-Nightly")
+set(dashboard_hg_branch "default")
+
+set(ENV{DISPLAY} ":0.0")
+
+set(ENV{CC} "/opt/local/bin/gcc-mp-4.6")
+set(ENV{CXX} "/opt/local/bin/g++-mp-4.6")
 
 
-SET (CTEST_INITIAL_CACHE "
-BUILDNAME:STRING=${CTEST_BUILD_NAME}
-SITE:STRING=${CTEST_SITE}
-CTEST_USE_LAUNCHERS:BOOL=ON
-
+macro(dashboard_hook_init)
+  set(dashboard_cache "${dashboard_cache}
+  
 CMAKE_LIBRARY_PATH:PATH=/opt/local/lib
 CMAKE_INCLUDE_PATH:PATH=/opt/local/include
-
-OTB_DATA_USE_LARGEINPUT:BOOL=OFF
-OTB_DATA_LARGEINPUT_ROOT:STRING=/Users/otbval/Data/OTB-LargeInput
-OTB_DATA_ROOT:STRING=$ENV{HOME}/Data/OTB-Data
-
-CMAKE_C_FLAGS:STRING= -Wall -Wno-uninitialized -Wno-unused-variable -fPIC
-CMAKE_CXX_FLAGS:STRING= -Wall -Wno-deprecated -Wno-uninitialized -Wno-unused-variable -fPIC
+  
+CMAKE_C_FLAGS:STRING=-fPIC -Wall -Wshadow -Wno-uninitialized -Wno-unused-variable
+CMAKE_CXX_FLAGS:STRING=-fPIC -Wall -Wno-deprecated -Wno-uninitialized -Wno-unused-variable
 
 #CMAKE_OSX_ARCHITECTURES:STRING=i386
 OPENTHREADS_CONFIG_HAS_BEEN_RUN_BEFORE:BOOL=ON
 
-CMAKE_BUILD_TYPE:STRING=Release
 BUILD_TESTING:BOOL=ON
 BUILD_EXAMPLES:BOOL=ON
 BUILD_APPLICATIONS:BOOL=ON
+BUILD_BUG_TRACKER_TESTS:BOOL=OFF
+
 OTB_WRAP_PYTHON:BOOL=ON
-OTB_WRAP_QT:BOOL=ON
-#OTB_WRAP_PYQT:BOOL=ON
 OTB_WRAP_JAVA:BOOL=ON
+OTB_WRAP_QT:BOOL=ON
+
+OTB_DATA_USE_LARGEINPUT:BOOL=OFF
+#OTB_DATA_LARGEINPUT_ROOT:STRING=$ENV{HOME}/Data/OTB-LargeInput
+OTB_DATA_ROOT:STRING=$ENV{HOME}/Data/OTB-Data
+
+OTB_USE_EXTERNAL_ITK:BOOL=ON
+ITK_DIR:PATH=${CTEST_DASHBOARD_ROOT}/itkv4/build
 
 OTB_USE_CURL:BOOL=ON
 OTB_USE_PQXX:BOOL=OFF
-ITK_USE_PATENTED:BOOL=ON
-OTB_USE_PATENTED:BOOL=ON
+OTB_USE_PATENTED:BOOL=OFF
 OTB_USE_EXTERNAL_BOOST:BOOL=ON
 OTB_USE_EXTERNAL_EXPAT:BOOL=ON
 OTB_USE_EXTERNAL_FLTK:BOOL=ON
-OTB_USE_GETTEXT:BOOL=OFF
-USE_FFTWD:BOOL=OFF
-USE_FFTWF:BOOL=OFF
-OTB_GL_USE_ACCEL:BOOL=OFF
-ITK_USE_REVIEW:BOOL=ON 
-ITK_USE_OPTIMIZED_REGISTRATION_METHODS:BOOL=ON 
 OTB_USE_MAPNIK:BOOL=OFF
+OTB_USE_OPENCV:BOOL=ON
 
-CMAKE_INSTALL_PREFIX:STRING=/Users/otbval/Dashboard/nightly/OTB-Release-gcc46/install
+    ")
+endmacro()
 
-")
-
-
-SET (CTEST_NOTES_FILES
-${CTEST_SCRIPT_DIRECTORY}/${CTEST_SCRIPT_NAME}
-${CTEST_BINARY_DIRECTORY}/CMakeCache.txt
-)
-
-ctest_empty_binary_directory (${CTEST_BINARY_DIRECTORY})
-ctest_start(Nightly)
-ctest_update(SOURCE "${CTEST_SOURCE_DIRECTORY}")
-file(WRITE "${CTEST_BINARY_DIRECTORY}/CMakeCache.txt" ${CTEST_INITIAL_CACHE})
-ctest_configure (BUILD "${CTEST_BINARY_DIRECTORY}")
-ctest_read_custom_files(${CTEST_BINARY_DIRECTORY})
-ctest_build (BUILD "${CTEST_BINARY_DIRECTORY}")
-ctest_test (BUILD "${CTEST_BINARY_DIRECTORY}" PARALLEL_LEVEL 4 INCLUDE Tu)
-ctest_submit ()
-
+include(${CTEST_SCRIPT_DIRECTORY}/../otb_common.cmake)
