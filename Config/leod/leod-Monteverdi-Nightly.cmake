3b098cd09d358f6cb3385844f0bab64d1f8f8806
Charles Peyrega <charles.peyrega@c-s.fr>
2013-11-19 18:48:42 +0100

    ENH: Update MVD and MVD2 Nightly and Continuous scripts for leod

--- Config/leod/leod-Monteverdi-Nightly.cmake
+++ Config/leod/leod-Monteverdi-Nightly.cmake
@@ -1,14 +1,18 @@
-SET (ENV{DISPLAY} ":0")
+SET (ENV{DISPLAY} ":0.0")
 
-SET (CTEST_SOURCE_DIRECTORY "$ENV{HOME}/Dashboard/nightly/Monteverdi-Release/src")
-SET (CTEST_BINARY_DIRECTORY "$ENV{HOME}/Dashboard/nightly/Monteverdi-Release/build")
+SET (dashboard_model Nightly)
+string(TOLOWER ${dashboard_model} lcdashboard_model)
+
+SET (CTEST_BUILD_CONFIGURATION Release)
+
+SET (CTEST_SOURCE_DIRECTORY "$ENV{HOME}/Dashboard/${lcdashboard_model}/Monteverdi-${CTEST_BUILD_CONFIGURATION}/src")
+SET (CTEST_BINARY_DIRECTORY "$ENV{HOME}/Dashboard/${lcdashboard_model}/Monteverdi-${CTEST_BUILD_CONFIGURATION}/build")
 
 SET( CTEST_CMAKE_GENERATOR  "Unix Makefiles" )
 SET (CTEST_CMAKE_COMMAND "cmake" )
 SET (CTEST_BUILD_COMMAND "/usr/bin/make -j8 -i -k" )
 SET (CTEST_SITE "leod.c-s.fr")
-SET (CTEST_BUILD_NAME "MacOSX10.8-Release")
-SET (CTEST_BUILD_CONFIGURATION "Release")
+SET (CTEST_BUILD_NAME "MacOSX10.8-${CTEST_BUILD_CONFIGURATION}")
 SET (CTEST_HG_COMMAND "/opt/local/bin/hg")
 SET (CTEST_HG_UPDATE_OPTIONS "-C")
 SET (CTEST_USE_LAUNCHERS ON)
@@ -23,19 +27,20 @@ CMAKE_LIBRARY_PATH:PATH=/opt/local/lib
 CMAKE_INCLUDE_PATH:PATH=/opt/local/include
 
 BUILD_SHARED_LIBS:BOOL=OFF
-CMAKE_BUILD_TYPE:STRING=Release
+CMAKE_BUILD_TYPE:STRING=${CTEST_BUILD_CONFIGURATION}
 BUILD_TESTING:BOOL=ON
-CMAKE_INSTALL_PREFIX:STRING=$ENV{HOME}/Dashboard/nightly/Monteverdi-Release/install
+CMAKE_INSTALL_PREFIX:STRING=$ENV{HOME}/Dashboard/${lcdashboard_model}/Monteverdi-${CTEST_BUILD_CONFIGURATION}/install
 
 OTB_DATA_USE_LARGEINPUT:BOOL=ON
-OTB_DATA_LARGEINPUT_ROOT:STRING=/Users/otbval/Data/OTB-LargeInput
+OTB_DATA_LARGEINPUT_ROOT:STRING=$ENV{HOME}/Data/OTB-LargeInput
 OTB_DATA_ROOT:STRING=$ENV{HOME}/Data/OTB-Data
 
 CMAKE_C_FLAGS:STRING= -Wall -Wno-uninitialized -Wno-unused-variable
 CMAKE_CXX_FLAGS:STRING= -Wall -Wno-deprecated -Wno-uninitialized -Wno-unused-variable -Wno-gnu -Wno-overloaded-virtual
 #CMAKE_OSX_ARCHITECTURES:STRING=i386
 
-OTB_DIR:STRING=$ENV{HOME}/Dashboard/nightly/OTB-Release/build
+OTB_DIR:STRING=$ENV{HOME}/Dashboard/${lcdashboard_model}/OTB-${CTEST_BUILD_CONFIGURATION}/build
+ITK_DIR:PATH=$ENV{HOME}/Dashboard/itkv4/build
 
 OTB_USE_CPACK:BOOL=ON
 
@@ -48,7 +53,7 @@ ${CTEST_BINARY_DIRECTORY}/CMakeCache.txt
 )
 
 ctest_empty_binary_directory (${CTEST_BINARY_DIRECTORY})
-ctest_start(Nightly)
+ctest_start(${dashboard_model})
 ctest_update(SOURCE "${CTEST_SOURCE_DIRECTORY}")
 file(WRITE "${CTEST_BINARY_DIRECTORY}/CMakeCache.txt" ${CTEST_INITIAL_CACHE})
 ctest_configure (BUILD "${CTEST_BINARY_DIRECTORY}")
--- Config/leod/leod-Monteverdi2-Continuous.cmake
+++ Config/leod/leod-Monteverdi2-Continuous.cmake
@@ -1,18 +1,24 @@
-SET (CTEST_SOURCE_DIRECTORY "/Users/otbval/Dashboard/continuous/Monteverdi2/src")
-SET (CTEST_BINARY_DIRECTORY "/Users/otbval/Dashboard/continuous/Monteverdi2/build")
+SET (ENV{DISPLAY} ":0.0")
+
+SET (dashboard_model Continuous)
+string(TOLOWER ${dashboard_model} lcdashboard_model)
+
+SET (CTEST_BUILD_CONFIGURATION Release)
+
+SET (CTEST_SOURCE_DIRECTORY "$ENV{HOME}/Dashboard/${lcdashboard_model}/Monteverdi2/src")
+SET (CTEST_BINARY_DIRECTORY "$ENV{HOME}/Dashboard/${lcdashboard_model}/Monteverdi2/build")
 
 SET( CTEST_CMAKE_GENERATOR  "Unix Makefiles" )
 SET (CTEST_CMAKE_COMMAND "cmake" )
 SET (CTEST_BUILD_COMMAND "/usr/bin/make -j8 -i -k install" )
 SET (CTEST_SITE "leod.c-s.fr")
-SET (CTEST_BUILD_NAME "MacOSX10.8-Release")
-SET (CTEST_BUILD_CONFIGURATION "Release")
+SET (CTEST_BUILD_NAME "MacOSX10.8-${CTEST_BUILD_CONFIGURATION}")
 SET (CTEST_HG_COMMAND "/opt/local/bin/hg")
 SET (CTEST_HG_UPDATE_OPTIONS "-C")
 SET (CTEST_USE_LAUNCHERS ON)
-SET (ENV{DISPLAY} ":0")
 
 SET (CTEST_INITIAL_CACHE "
+
 BUILDNAME:STRING=${CTEST_BUILD_NAME}
 SITE:STRING=${CTEST_SITE}
 CTEST_USE_LAUNCHERS:BOOL=ON
@@ -20,22 +26,24 @@ CTEST_USE_LAUNCHERS:BOOL=ON
 CMAKE_LIBRARY_PATH:PATH=/opt/local/lib
 CMAKE_INCLUDE_PATH:PATH=/opt/local/include
 
+BUILD_SHARED_LIBS:BOOL=OFF
+CMAKE_BUILD_TYPE:STRING=${CTEST_BUILD_CONFIGURATION}
+BUILD_TESTING:BOOL=ON
+CMAKE_INSTALL_PREFIX:STRING=$ENV{HOME}/Dashboard/${lcdashboard_model}/Monteverdi2/install
+
 OTB_DATA_USE_LARGEINPUT:BOOL=ON
-OTB_DATA_LARGEINPUT_ROOT:STRING=/Users/otbval/Data/OTB-LargeInput
-OTB_DATA_ROOT:STRING=/Users/otbval/Data/OTB-Data
+OTB_DATA_LARGEINPUT_ROOT:STRING=$ENV{HOME}/Data/OTB-LargeInput
+OTB_DATA_ROOT:STRING=$ENV{HOME}/Data/OTB-Data
 
 CMAKE_C_FLAGS:STRING= -Wall -Wno-uninitialized -Wno-unused-variable
 CMAKE_CXX_FLAGS:STRING= -Wall -Wno-deprecated -Wno-uninitialized -Wno-unused-variable -Wno-gnu -Wno-overloaded-virtual
 #CMAKE_OSX_ARCHITECTURES:STRING=i386
 
-CMAKE_BUILD_TYPE:STRING=Release
-
-OTB_DIR:STRING=$ENV{HOME}/Dashboard/continuous/OTB/build
-
-BUILD_TESTING:BOOL=ON
-CMAKE_INSTALL_PREFIX:STRING=$ENV{HOME}/Dashboard/continuous/Monteverdi2/install
+OTB_DIR:STRING=$ENV{HOME}/Dashboard/${lcdashboard_model}/OTB/build
+ITK_DIR:PATH=$ENV{HOME}/Dashboard/itkv4/build
 
 ")
+
 
 SET (CTEST_NOTES_FILES
 ${CTEST_SCRIPT_DIRECTORY}/${CTEST_SCRIPT_NAME}
@@ -43,7 +51,7 @@ ${CTEST_BINARY_DIRECTORY}/CMakeCache.txt
 )
 
 ctest_empty_binary_directory (${CTEST_BINARY_DIRECTORY})
-ctest_start(Continuous)
+ctest_start(${dashboard_model})
 ctest_update(SOURCE "${CTEST_SOURCE_DIRECTORY}")
 file(WRITE "${CTEST_BINARY_DIRECTORY}/CMakeCache.txt" ${CTEST_INITIAL_CACHE})
 ctest_configure (BUILD "${CTEST_BINARY_DIRECTORY}")
