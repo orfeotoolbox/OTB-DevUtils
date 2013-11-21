a5ac92f66f903f6e9f5864d77fece96541076276
Manuel Grizonnet <manuel.grizonnet@orfeo-toolbox.org>
2013-11-21 08:51:35 +0100

    ENH: update monteverdi submission on pc-christophe

--- Config/pc-christophe/pc-christophe-Monteverdi-Nightly.cmake
+++ Config/pc-christophe/pc-christophe-Monteverdi-Nightly.cmake
@@ -1,20 +1,25 @@
+#SET (ENV{DISPLAY} ":0.0")
+
+SET (dashboard_model Nightly)
+
 SET (CTEST_SOURCE_DIRECTORY "/home/otbtesting/OTB/trunk/Monteverdi/")
 SET (CTEST_BINARY_DIRECTORY "/home/otbtesting/OTB/bin/Monteverdi-Nightly/")
 
+SET (CTEST_BUILD_CONFIGURATION Release)
 
-# which ctest command to use for running the dashboard
-SET (CTEST_COMMAND 
-  "ctest -j3 -D Nightly -A /home/otbtesting/OTB/trunk/OTB-DevUtils/Config/pc-christophe-Monteverdi-Nightly.cmake -V"
-  )
+SET( CTEST_CMAKE_GENERATOR  "Unix Makefiles" )
+SET (CTEST_CMAKE_COMMAND "cmake" )
+SET (CTEST_BUILD_COMMAND "/usr/bin/make -j4 -i -k" )
+SET (CTEST_SITE "pc-christophe.cst.cnes.fr" )
+SET (CTEST_BUILD_NAME "Fedora17-64bits--${CTEST_BUILD_CONFIGURATION}")
+SET (CTEST_HG_COMMAND "/usr/bin/hg")
+SET (CTEST_HG_UPDATE_OPTIONS "-C")
+SET (CTEST_USE_LAUNCHERS ON)
 
-# what cmake command to use for configuring this dashboard
-SET (CTEST_CMAKE_COMMAND 
-  "cmake"
-  )
-SET (CTEST_BUILD_COMMAND "/usr/bin/make -j4 -i -k")
+
 # should ctest wipe the binary tree before running
 SET (CTEST_START_WITH_EMPTY_BINARY_DIRECTORY TRUE)
-SET (CTEST_USE_LAUNCHERS ON)
+
 
 # this is the initial cache to use for the binary tree, be careful to escape
 # any quotes inside of this string if you use it
@@ -22,25 +27,48 @@ SET (CTEST_INITIAL_CACHE "
 // Use Launchers for CDash reporting
 CTEST_USE_LAUNCHERS:BOOL=ON
 //Name of the build
-BUILDNAME:STRING=Monteverdi-Fedora17-64bits-Release
+BUILDNAME:STRING=${CTEST_BUILD_NAME}
 //Name of the computer/site where compile is being run
-SITE:STRING=pc-christophe
+SITE:STRING=${CTEST_SITE}
 //LargeInput
-OTB_DATA_USE_LARGEINPUT:BOOL=OFF
-//OTB_DATA_LARGEINPUT_ROOT:STRING=/media/ssh/pc-inglada/media/TeraDisk2/LargeInput
+OTB_DATA_USE_LARGEINPUT:BOOL=ON
+OTB_DATA_LARGEINPUT_ROOT:STRING=/media/ssh/pc-inglada/media/TeraDisk2/LargeInput
 //Data root
 OTB_DATA_ROOT:STRING=/home/otbtesting/OTB/trunk/OTB-Data
 //Compilation options
 CMAKE_C_FLAGS:STRING= -Wall -Wno-uninitialized -Wno-unused-variable
 CMAKE_CXX_FLAGS:STRING= -Wall -Wno-deprecated -Wno-uninitialized -Wno-unused-variable
 //Set up the build options
-CMAKE_BUILD_TYPE:STRING=Release
+CMAKE_BUILD_TYPE:STRING=${CTEST_BUILD_CONFIGURATION}
 BUILD_TESTING:BOOL=ON
-OTB_DIR:STRING=/home/otbtesting/OTB/bin/OTB-Nightly/
+OTB_DIR:STRING=/home/otbtesting/OTB/bin/OTB-Nightly
+
 ")
 
-# set any extra envionment varibles here
-SET (CTEST_ENVIRONMENT
- "DISPLAY=:0"
+SET( OTB_PULL_RESULT_FILE "${CTEST_BINARY_DIRECTORY}/pull_result.txt" )
+
+SET (CTEST_NOTES_FILES
+${CTEST_SCRIPT_DIRECTORY}/${CTEST_SCRIPT_NAME}
+${OTB_PULL_RESULT_FILE}
+${CTEST_BINARY_DIRECTORY}/CMakeCache.txt
 )
+
+execute_process(COMMAND ${CTEST_CMAKE_COMMAND} -E remove_directory ${OTB_INSTALL_PREFIX})
+execute_process(COMMAND ${CTEST_CMAKE_COMMAND} -E make_directory ${OTB_INSTALL_PREFIX})
+ctest_empty_binary_directory (${CTEST_BINARY_DIRECTORY})
+
+execute_process( COMMAND ${CTEST_HG_COMMAND} pull http://hg.orfeo-toolbox.org/Monteverdi-Nightly
+                 WORKING_DIRECTORY "${CTEST_SOURCE_DIRECTORY}"
+                 OUTPUT_VARIABLE   OTB_PULL_RESULT
+                 ERROR_VARIABLE    OTB_PULL_RESULT )
+file(WRITE ${OTB_PULL_RESULT_FILE} ${OTB_PULL_RESULT} )
+
+ctest_start(${dashboard_model})
+ctest_update(SOURCE "${CTEST_SOURCE_DIRECTORY}")
+file(WRITE "${CTEST_BINARY_DIRECTORY}/CMakeCache.txt" ${OTB_INITIAL_CACHE})
+ctest_configure (BUILD "${CTEST_BINARY_DIRECTORY}")
+ctest_read_custom_files(${CTEST_BINARY_DIRECTORY})
+ctest_build (BUILD "${CTEST_BINARY_DIRECTORY}")
+ctest_test (BUILD "${CTEST_BINARY_DIRECTORY}" PARALLEL_LEVEL 4)
+ctest_submit ()
 
