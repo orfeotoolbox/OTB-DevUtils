3309fc90b175026f7a340c602fa496d39c0471be
Rashad Kanavath <rashad.kanavath@c-s.fr>
2014-10-07 15:36:44 +0530

    TEST: test mxe changes from upstream. clean mxe

--- Scripts/mxe_build.sh
+++ Scripts/mxe_build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+#07-10-2014: merge commits from parent
+## clean mxe
+## disable pull
 MXE_DIR='/home/otbtesting/win-sources/mxe'
 if [ $# -eq 3 ]; then
 MXE_DIR=$1
@@ -12,12 +15,13 @@ exit 1
 fi
 
 cd $MXE_DIR
-
+#clean mxe due to merge(parent)-push
+make clean
 if [ "$DO_PULL" == "yes" ]; then
    GIT_PULL_MASTER='git pull origin master'
-  $GIT_PULL_MASTER
+#  $GIT_PULL_MASTER
 fi;
 echo 'MXE_TARGET='$MXE_TARGET
 echo 'Sarting build of mxe and dependencies right now.'
 #now start building
-make MXE_TARGETS="$MXE_TARGET" gdal openthreads geos ossim itk qt opencv glfw3 glew freeglut boost expat qwt_qt4 fltk
+make MXE_TARGETS="$MXE_TARGET" expat zlib libpng jpeg xz tiff openjpeg gdal openthreads geos ossim itk qt opencv glfw3 glew freeglut boost qwt_qt4 fltk
