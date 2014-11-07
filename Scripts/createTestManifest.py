4debfb117097d3eda79ccbdc5069e3cfe9839b84
Guillaume Pasero <guillaume.pasero@c-s.fr>
2014-11-07 18:58:19 +0100

    ENH: produce a first testManifest, 42 files still TBD

--- Scripts/createTestManifest.py
+++ Scripts/createTestManifest.py
@@ -62,7 +62,9 @@ def parseTestCxx(path):
   registeredTests = []
   includes = []
   
-  systemInc = ["string.h","stdio.h","stdint.h","ctype.h","dirent.h","assert.h","sys/types.h"]
+  systemInc = ["string.h","stdio.h","stdint.h","ctype.h","dirent.h",\
+               "assert.h","sys/types.h","stdlib.h","profile.h","windows.h",\
+               "math.h"]
   
   search_string=r'^#include *([<"])([^<"]*\.h.*)([>"])'
   includeRegexp=re.compile(search_string)
@@ -168,25 +170,27 @@ def parseTestCxx(path):
   return res
 
 def getTestDependencies(includes,sourceList):
-  depList = []
+  depList = {}
   thirdPartyDep = []
   for inc in includes:
     if inc[0:3] in ["itk","vcl","vnl"]:
       # no extra dependency 
       continue
-    elif inc[0:3] == "otb":
+    elif inc[0:3] == "otb" and inc[3] != '_':
       if inc in sourceList.keys():
         if not sourceList[inc] in depList:
-          depList.append(sourceList[inc])
+          depList[sourceList[inc]] = {"to":inc}
       else:
         print "Warning ! OTB source not found : "+inc
     else:
       # try an external dependency
       depName = manifestParser.findExternalDep(inc)
-      if not depName in thirdPartyDep:
+      if depName == "Other":
+        print "Warning ! Unkown include : "+inc
+      elif not depName in thirdPartyDep:
         thirdPartyDep.append(depName)
   
-  return (depList,thirdPartyDep)
+  return [depList,thirdPartyDep]
 
 
 def buildOldFolderPartition(moduleList):
@@ -201,6 +205,70 @@ def buildOldFolderPartition(moduleList):
       partition[baseDir][mod] = partition[baseDir][mod] + 1
   
   return partition
+
+
+def findClosestSourceName(testFile,sourceList):
+  matchFile = ""
+  matchPercent = -1.0
+  
+  testFileLower = testFile.lower()
+  
+  for src in sourceList:
+    srcLower = src.lower()
+    maxLength = min(len(testFileLower),len(srcLower))
+    currentPos = 0
+    while (currentPos<maxLength):
+      if srcLower[currentPos] == testFileLower[currentPos]:
+        currentPos = currentPos + 1
+      else:
+        break
+    score = 100.0 * float(currentPos) / float(len(testFile))
+    if score > matchPercent:
+      matchFile = src
+      matchPercent = score
+    
+  return [matchFile,matchPercent]
+
+
+def getGroup(module,groups):
+  myGroup = ""
+  for grp in groups:
+    if module in groups[grp]:
+      myGroup = grp
+      break
+  return myGroup
+
+
+def gatherTestDepends(testMains,testFunctions,fullDepList):
+  gatherTestDependencies = {}
+  for tfile in testMains:
+    tmod = testMains[tfile]["module"]
+    if tmod == "TBD":
+      continue
+    if not tmod in gatherTestDependencies:
+      gatherTestDependencies[tmod] = {}
+    for tdep in testMains[tfile]["depList"]:
+      # skip module itself and its dependencies
+      if (tdep == tmod) or (tdep in fullDepList[tmod]):
+        continue
+      if not gatherTestDependencies[tmod].has_key(tdep):
+        gatherTestDependencies[tmod][tdep] = []
+      gatherTestDependencies[tmod][tdep].append(testMains[tfile]["depList"][tdep])
+  for tfile in testFunctions:
+    tmod = testFunctions[tfile]["module"]
+    if tmod == "TBD":
+      continue
+    if not tmod in gatherTestDependencies:
+      gatherTestDependencies[tmod] = {}
+    for tdep in testFunctions[tfile]["depList"]:
+      # skip module itself and its dependencies
+      if (tdep == tmod) or (tdep in fullDepList[tmod]):
+        continue
+      if not gatherTestDependencies[tmod].has_key(tdep):
+        gatherTestDependencies[tmod][tdep] = []
+      gatherTestDependencies[tmod][tdep].append(testFunctions[tfile]["depList"][tdep])
+  
+  return gatherTestDependencies
 
 
 def main(argv):
@@ -221,24 +289,38 @@ def main(argv):
   testMains = {}
   testFunctions = {}
   
+  outFD = open(outManifest,'wb')
+  
   # parse all cxx test files : analyse them and extract their dependencies
   for (d,f) in codeParser.FindBinaries(testing_dir):
     fullPath = op.join(d,f)
     shortPath = fullPath.replace(otbDir,'.')
     
+    moduleDestination = "TBD"
+    groupDestination = "TBD"
+    
     res = parseTestCxx(fullPath)
+    
+    if res["isTestDriver"]:
+      # no need to dispatch test drivers, they can be generated again
+      testDrivers[shortPath] = res["testFunctions"]
+      continue
     
     [testDepList,thirdPartyDep] = getTestDependencies(res["includes"],sourceList)
     
     # try to clean the dependency list (remove inherited modules)
+    ignoreModules = ["ImageIO","VectorDataIO","TestKernel"]
     cleanTestDepList = []
     depListToRemove = []
     for dep1 in testDepList:
+      # register the "from" field
+      testDepList[dep1]["from"] = shortPath
+      
       for dep2 in testDepList:
         if dep2 == dep1:
           continue
-        # avoid IOBase to 'eat' usefull dependencies
-        if dep1 == "IOBase":
+        # avoid IO modules to 'eat' usefull dependencies
+        if dep1 in ignoreModules:
           continue
         if (dep2 in fullDepList[dep1]) and \
            (not dep2 in depListToRemove):
@@ -247,35 +329,128 @@ def main(argv):
       if not dep in depListToRemove:
         cleanTestDepList.append(dep)
     
+    # build all dependencies of the test
+    testFullDepList = []
+    for dep in testDepList:
+      for subDep in fullDepList[dep]:
+        if not subDep in testFullDepList:
+          testFullDepList.append(subDep)
+    
     # try to get the list of module used to partition the corresponding source directory
     guessModules = []
     guessSourceDir = op.split(shortPath.replace("./Testing","."))[0]
     if OldFolderPartition.has_key(guessSourceDir):
       guessModules = OldFolderPartition[guessSourceDir].keys()
     
+    # special case for Testing/Application  -> ApplicationEngine
+    if guessSourceDir == "./Applications":
+      guessModules.append("ApplicationEngine")
     
-    # first filter : find modules that appear in cleanTestDepList and 
-    firstLuckyGuess = []
+    # first filter : find modules that appear in cleanTestDepList and in guessModules
+    overlappingModules = []
+    firstLuckyGuess = None
     for dep in cleanTestDepList:
       if dep in guessModules:
-        firstLuckyGuess.append(dep)
+        overlappingModules.append(dep)
+    if len(overlappingModules) == 1:
+      firstLuckyGuess = overlappingModules[0]
     
-    if len(firstLuckyGuess) == 1:
-      print f + " -> FIRST_LUCKY_GUESS "+firstLuckyGuess[0]
+    # second filter : find the source file with the closest name
+    secondLuckyGuess = None
+    [matchFile, matchPercent] = findClosestSourceName(f,sourceList)
+    if (sourceList[matchFile] in testDepList) and (matchPercent > 50.0):
+      secondLuckyGuess = sourceList[matchFile]
+    elif (sourceList[matchFile] in testFullDepList) and (matchPercent > 70.0):
+      secondLuckyGuess = sourceList[matchFile]
+    
+    # third filter : Utilities
+    thirdLuckyGuess = None
+    if guessSourceDir == "./Utilities":
+      groupDestination = "ThirdParty"
+      if len(thirdPartyDep) == 1:
+        thirdLuckyGuess = thirdPartyDep[0]
+    
+    # fourth filter : if there is only one dependency in cleanTestDepList : take it
+    fourthLuckyGuess = None
+    if len(cleanTestDepList) == 1:
+      fourthLuckyGuess = cleanTestDepList[0]
+    
+    # fifth filter : handle test containing "Reader" "Writer" ...
+    fifthLuckyGuess = None
+    if (f.find("Reader") >= 0) or (f.find("Reading") >= 0) or \
+       (f.find("Writer") >= 0) or (f.find("Writing") >= 0):
+      # remove non-IO deps from cleanTestDepList and look what's left
+      ioCleanDep = []
+      for dep in cleanTestDepList:
+        if getGroup(dep,groups) == "IO":
+          ioCleanDep.append(dep)
+      if len(ioCleanDep) == 1:
+        fifthLuckyGuess = ioCleanDep[0]
+    
+    
+    if firstLuckyGuess:
+      moduleDestination = firstLuckyGuess
+    elif secondLuckyGuess:
+      moduleDestination = secondLuckyGuess
+    elif thirdLuckyGuess:
+      moduleDestination = thirdLuckyGuess
+    elif fourthLuckyGuess:
+      moduleDestination = fourthLuckyGuess
+    elif fifthLuckyGuess:
+      moduleDestination = fifthLuckyGuess
     else:
-      print f + " -> Failed"
+      pass
+      #print f + " -> " + str(testDepList)
+      #print f + " -> "+ matchFile + " ( " + str(matchPercent) + "% )"
+    
+    # if module is found and not group, deduce group
+    if groupDestination == "TBD" and moduleDestination != "TBD":
+      groupDestination = getGroup(moduleDestination,groups)
     
     
-    if res["isTestDriver"]:
-      testDrivers[shortPath] = res["testFunctions"]
-    elif res["hasMain"]:
-      testMains[shortPath] = {"depList":testDepList , "thirdPartyDep":thirdPartyDep }
+    outputDic = {"depList":testDepList , "thirdPartyDep":thirdPartyDep, "group":groupDestination, "module":moduleDestination}
+    
+    if res["hasMain"]:
+      testMains[shortPath] = outputDic
     else:
-      testFunctions[shortPath] = {"depList":testDepList , "thirdPartyDep":thirdPartyDep, "testFunctions":res["testFunctions"] }
+      outputDic["testFunctions"] = res["testFunctions"]
+      testFunctions[shortPath] = outputDic
     
-    #print shortPath+" -> "+str(depList)
-    #print f + " -> "+str(res["isTestDriver"])+" / "+str(res["hasMain"])+" / "+str(len(res["testFunctions"]))+" / " +str(len(res["includes"]))
-    
+    outFD.write(shortPath+","+op.basename(op.dirname(shortPath))+","+groupDestination+","+moduleDestination+",test,\n")
+  
+  outFD.close()
+  
+  # sum all test dependencies in every module
+  allTestDepends = gatherTestDepends(testMains,testFunctions,fullDepList)
+  
+  # clean inherited test depends
+  cleanedTestDepends = {}
+  for mod in allTestDepends:
+    cleanList = []
+    depListToRemove = []
+    for dep1 in allTestDepends[mod]:
+      for dep2 in allTestDepends[mod]:
+        if dep2 == dep1:
+          continue
+        if (dep2 in fullDepList[dep1]) and \
+           (not dep2 in depListToRemove):
+          depListToRemove.append(dep2)
+    for dep in allTestDepends[mod]:
+      if not dep in depListToRemove:
+        cleanList.append(dep)
+    cleanedTestDepends[mod] = cleanList
+  
+  
+  manifestParser.printDepList(allTestDepends)
+  """
+  for mod in allTestDepends:
+    print "---------------------------------------"
+    print "Module "+mod
+    for dep in allTestDepends[mod]:
+      print "  -> "+dep
+      for tfile in allTestDepends[mod][dep]:
+        print "    * "+tfile
+  """  
   # DEBUG  
   return
   
