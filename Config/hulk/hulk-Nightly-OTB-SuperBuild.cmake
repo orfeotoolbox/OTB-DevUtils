110dbbcc67d5810022d8d76dfdfa3c8c5f7afde6
Guillaume Pasero <guillaume.pasero@c-s.fr>
2014-12-11 18:03:57 +0100

    ADD: SuperBuild experimental on hulk

---
+++ Config/hulk/hulk-Nightly-OTB-SuperBuild.cmake
@@ -0,0 +1,47 @@
+set(CTEST_DASHBOARD_ROOT "/home/otbval/Dashboard")
+set(CTEST_SITE "hulk.c-s.fr")
+set(CTEST_BUILD_CONFIGURATION Release)
+set(CTEST_BUILD_NAME "Ubuntu14.04-64bits-SuperBuild")
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set(CTEST_BUILD_COMMAND "/usr/bin/make -j9 -i -k" )
+set(CTEST_TEST_ARGS PARALLEL_LEVEL 4)
+set(CTEST_TEST_TIMEOUT 500)
+
+set(CTEST_SOURCE_DIRECTORY  "${CTEST_DASHBOARD_ROOT}/src/OTB-SuperBuild")
+set(CTEST_BINARY_DIRECTORY  "${CTEST_DASHBOARD_ROOT}/build/OTB-SuperBuild")
+set(CTEST_INSTALL_DIRECTORY "${CTEST_DASHBOARD_ROOT}/install/OTB-SuperBuild")
+
+set(CTEST_HG_COMMAND          "/usr/bin/hg")
+set(CTEST_HG_UPDATE_OPTIONS   "-C")
+
+set(OTB_INITIAL_CACHE "
+CMAKE_INSTALL_PREFIX:PATH=${CTEST_DASHBOARD_ROOT}/install/OTB-SuperBuild
+CMAKE_BUILD_TYPE:STRING=${CTEST_BUILD_CONFIGURATION}
+")
+
+
+set(CTEST_NOTES_FILES
+${CTEST_SCRIPT_DIRECTORY}/${CTEST_SCRIPT_NAME}
+${CTEST_BINARY_DIRECTORY}/CMakeCache.txt
+)
+
+execute_process(COMMAND ${CTEST_CMAKE_COMMAND} -E remove_directory ${CTEST_INSTALL_DIRECTORY})
+execute_process(COMMAND ${CTEST_CMAKE_COMMAND} -E make_directory ${CTEST_INSTALL_DIRECTORY})
+ctest_empty_binary_directory (${CTEST_BINARY_DIRECTORY})
+
+ctest_update()
+
+ctest_start(Experimental)
+ctest_update(SOURCE "${CTEST_SOURCE_DIRECTORY}")
+file(WRITE "${CTEST_BINARY_DIRECTORY}/CMakeCache.txt" ${OTB_INITIAL_CACHE})
+ctest_configure (BUILD "${CTEST_BINARY_DIRECTORY}")
+ctest_read_custom_files(${CTEST_BINARY_DIRECTORY})
+ctest_build (BUILD "${CTEST_BINARY_DIRECTORY}")
+
+# before testing, set the LD_LIBRARY_PATH
+set(ENV{LD_LIBRARY_PATH} ${CTEST_INSTALL_DIRECTORY}/lib)
+
+ctest_test (BUILD "${CTEST_BINARY_DIRECTORY}/OTB/build" PARALLEL_LEVEL 6)
+ctest_submit ()
+
+
