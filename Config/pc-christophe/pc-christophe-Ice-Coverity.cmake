64b4f9f1913f298da027df13ba192e0881c888e9
Manuel Grizonnet <manuel.grizonnet@orfeo-toolbox.org>
2015-01-26 15:32:35 +0100

    ENH: coverity configuration for monteverdi2 and ice

---
+++ Config/pc-christophe/pc-christophe-Ice-Coverity.cmake
@@ -0,0 +1,16 @@
+# Client maintainer: manuel.grizonnet@cnes.fr
+# Make command for parallel builds
+SET(MAKECOMMAND "/usr/bin/make -i -k -j 4" CACHE STRING "" FORCE)
+# Name of the build
+SET(BUILDNAME "Fedora20-64bits-Coverity-Debug" CACHE STRING "" FORCE)
+# Name of the computer/site where compile is being run
+SET(SITE "pc-christophe.cst.cnes.fr" CACHE STRING "" FORCE)
+SET(CMAKE_BUILD_TYPE "Debug" CACHE STRING "" FORCE)
+
+##external ITK
+SET(ITK_DIR "/home/otbtesting/install/itk/stable/Release/lib/cmake/ITK-4.6" CACHE STRING "" FORCE)
+SET(OTB_DIR "/home/otbtesting/install/orfeo/trunk/OTB-Nightly/Release/lib/otb/" CACHE STRING "" FORCE)
+SET(GLFW_INCLUDE_DIR "/usr/include/GLFW" CACHE STRING "" FORCE)
+
+SET(BUILD_ICE_APPLICATION ON CACHE BOOL "" FORCE)
+SET(BUILD_TESTING ON CACHE BOOL "" FORCE)
---
+++ Config/pc-christophe/pc-christophe-Monteverdi2-Coverity.cmake
@@ -0,0 +1,26 @@
+# Client maintainer: manuel.grizonnet@cnes.fr
+# Make command for parallel builds
+SET(MAKECOMMAND "/usr/bin/make -i -k -j 4" CACHE STRING "" FORCE)
+# Name of the build
+SET(BUILDNAME "Fedora20-64bits-Coverity-Debug" CACHE STRING "" FORCE)
+# Name of the computer/site where compile is being run
+SET(SITE "pc-christophe.cst.cnes.fr" CACHE STRING "" FORCE)
+SET(CMAKE_BUILD_TYPE "Debug" CACHE STRING "" FORCE)
+
+##external ITK
+SET(ITK_DIR "/home/otbtesting/install/itk/stable/Release/lib/cmake/ITK-4.6" CACHE STRING "" FORCE)
+SET(OTB_DIR "/home/otbtesting/install/orfeo/trunk/OTB-Nightly/Release/lib/otb/" CACHE STRING "" FORCE)
+
+SET(BUILD_TESTING ON CACHE BOOL "" FORCE)
+
+#otbIce
+SET(ICE_INCLUDE_DIR "/home/otbtesting/install}/orfeo/trunk/Ice/Release/include/otb/" CACHE STRING "" FORCE)
+SET(ICE_LIBRARY "/home/otbtesting/install/orfeo/trunk/Ice/Release/lib/otb/libOTBIce.so" CACHE STRING "" FORCE)
+
+SET(OTB_DATA_USE_LARGEINPUT ON CACHE BOOL "" FORCE)
+SET(OTB_DATA_ROOT "/home/otbtesting/sources/orfeo/OTB-Data" CACHE STRING "" FORCE)
+SET(OTB_DATA_LARGEINPUT_ROOT "/media/ssh/pc-inglada/media/TeraDisk2/LargeInput" CACHE STRING "" FORCE)
+
+#Qwt
+SET(QWT_INCLUDE_DIR "/usr/include/qwt5-qt4" CACHE STRING "" FORCE)
+SET(QWT_LIBRARY "/usr/lib64/libqwt.so.5" CACHE STRING "" FORCE)
