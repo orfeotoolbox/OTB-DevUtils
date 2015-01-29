590d76e4110f45e00cb86ee3f9477ad52e615ce0
Manuel Grizonnet <manuel.grizonnet@orfeo-toolbox.org>
2015-01-29 14:37:46 +0100

    BUG: wrong path for ice includes for coverity

--- Config/pc-christophe/pc-christophe-Monteverdi2-Coverity.cmake
+++ Config/pc-christophe/pc-christophe-Monteverdi2-Coverity.cmake
@@ -14,7 +14,7 @@ SET(OTB_DIR "/home/otbtesting/install/orfeo/trunk/OTB-Nightly/Release/lib/otb/" 
 SET(BUILD_TESTING ON CACHE BOOL "" FORCE)
 
 #otbIce
-SET(ICE_INCLUDE_DIR "/home/otbtesting/install}/orfeo/trunk/Ice/Release/include/otb/" CACHE STRING "" FORCE)
+SET(ICE_INCLUDE_DIR "/home/otbtesting/install/orfeo/trunk/Ice/Release/include/otb/" CACHE STRING "" FORCE)
 SET(ICE_LIBRARY "/home/otbtesting/install/orfeo/trunk/Ice/Release/lib/otb/libOTBIce.so" CACHE STRING "" FORCE)
 
 SET(OTB_DATA_USE_LARGEINPUT ON CACHE BOOL "" FORCE)
