4f6ccf08df1729b48eff6e8c77a112e7606e34cc
Rashad Kanavath <rashad.kanavath@c-s.fr>
2015-01-30 18:32:03 +0100

    clean binary and install before build

--- Config/raoul/raoul-Win7-OTB-Release-VC2010-SuperBuild-Win32.cmake
+++ Config/raoul/raoul-Win7-OTB-Release-VC2010-SuperBuild-Win32.cmake
@@ -4,7 +4,7 @@ set(CTEST_BUILD_NAME "Win7-${BUILD_DIR_NAME}-Static")
 set(CTEST_SITE "raoul.c-s.fr" )
 set(CTEST_DASHBOARD_ROOT "C:/SuperBuild")
 set(CTEST_CMAKE_GENERATOR "NMake Makefiles")
-set(CTEST_TEST_TIMEOUT 500)
+set(CTEST_TEST_TIMEOUT 1500)
 
 set(CTEST_SOURCE_DIRECTORY  "C:/Users/jmalik/Dashboard/src/OTB-SuperBuild")
 set(CTEST_BINARY_DIRECTORY  "${CTEST_DASHBOARD_ROOT}/build/")
@@ -17,23 +17,25 @@ set(CTEST_DROP_LOCATION "/submit.php?project=OTB")
 set(CTEST_DROP_SITE_CDASH TRUE)
 set(CTEST_USE_LAUNCHERS TRUE)
 
-set(OTB_INITIAL_CACHE 
+set(SUPERBUILD_INITIAL_CACHE 
 "CMAKE_INSTALL_PREFIX:PATH=${CTEST_INSTALL_DIRECTORY}
 CMAKE_BUILD_TYPE:STRING=${CTEST_BUILD_CONFIGURATION}
 OTB_DATA_ROOT:PATH=${CTEST_DASHBOARD_ROOT}/src/OTB-Data
+ENABLE_OTB_LARGE_INPUTS:BOOL=ON
+OTB_DATA_LARGEINPUT_ROOT:PATH=C:/Users/jmalik/Dashboard/src/OTB-LargeInput
 CTEST_USE_LAUNCHERS:BOOL=${CTEST_USE_LAUNCHERS}")
 
-# execute_process(COMMAND ${CMAKE_COMMAND} -E remove_directory ${CTEST_INSTALL_DIRECTORY})
-# execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory ${CTEST_INSTALL_DIRECTORY})
-# execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory ${CTEST_INSTALL_DIRECTORY}/lib)
-# execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory ${CTEST_INSTALL_DIRECTORY}/bin)
-# execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory ${CTEST_INSTALL_DIRECTORY}/include)
+execute_process(COMMAND ${CMAKE_COMMAND} -E remove_directory ${CTEST_INSTALL_DIRECTORY})
+execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory ${CTEST_INSTALL_DIRECTORY})
+execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory ${CTEST_INSTALL_DIRECTORY}/lib)
+execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory ${CTEST_INSTALL_DIRECTORY}/bin)
+execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory ${CTEST_INSTALL_DIRECTORY}/include)
 
-# ctest_empty_binary_directory (${CTEST_BINARY_DIRECTORY})
+ctest_empty_binary_directory (${CTEST_BINARY_DIRECTORY})
 
 ctest_start(Experimental)
 ctest_update(SOURCE "${CTEST_SOURCE_DIRECTORY}")
-# file(WRITE "${CTEST_BINARY_DIRECTORY}/CMakeCache.txt" ${OTB_INITIAL_CACHE})
+file(WRITE "${CTEST_BINARY_DIRECTORY}/CMakeCache.txt" ${SUPERBUILD_INITIAL_CACHE})
 ctest_configure (BUILD "${CTEST_BINARY_DIRECTORY}")
 
 ctest_read_custom_files(${CTEST_BINARY_DIRECTORY})
@@ -41,4 +43,5 @@ ctest_read_custom_files(${CTEST_BINARY_DIRECTORY})
 ctest_build (BUILD "${CTEST_BINARY_DIRECTORY}")
 
 ctest_test (BUILD "${CTEST_BINARY_DIRECTORY}/OTB/build" ${CTEST_TEST_ARGS})
+
 ctest_submit ()
