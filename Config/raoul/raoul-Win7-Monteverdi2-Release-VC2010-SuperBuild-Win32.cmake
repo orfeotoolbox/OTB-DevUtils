b1bd9446e33ff672c2f570f06b773f9d1197ba66
Rashad Kanavath <rashad.kanavath@c-s.fr>
2015-02-10 15:43:48 +0100

    Monteverdi and OTB Superbuild scripts

--- Config/raoul/raoul-Win7-Monteverdi2-Release-VC2010-SuperBuild-Win32.cmake
+++ Config/raoul/raoul-Win7-Monteverdi2-Release-VC2010-SuperBuild-Win32.cmake
@@ -1,5 +1,5 @@
 set(CTEST_BUILD_CONFIGURATION Release)
-set(BUILD_DIR_NAME "OTB-SuperBuild-vc10-x86-Release")
+set(BUILD_DIR_NAME "Monteverdi2-SuperBuild-vc10-x86-Release")
 set(CTEST_BUILD_NAME "Win7-${BUILD_DIR_NAME}")
 set(CTEST_SITE "raoul.c-s.fr" )
 set(CTEST_DASHBOARD_ROOT "C:/SuperBuild")
@@ -7,8 +7,10 @@ set(CTEST_CMAKE_GENERATOR "NMake Makefiles")
 set(CTEST_TEST_TIMEOUT 1500)
 
 set(CTEST_SOURCE_DIRECTORY  "C:/Users/jmalik/Dashboard/src/OTB-SuperBuild")
-set(CTEST_BINARY_DIRECTORY  "${CTEST_DASHBOARD_ROOT}/build/")
+set(CTEST_BINARY_DIRECTORY  "${CTEST_DASHBOARD_ROOT}/build")
 set(CTEST_INSTALL_DIRECTORY "${CTEST_DASHBOARD_ROOT}/install/")
+
+set(CTEST_USE_LAUNCHERS TRUE)
 
 set(DATA_ROOT C:/Users/jmalik/Dashboard/src)
 set(SUPERBUILD_INITIAL_CACHE 
@@ -25,39 +27,32 @@ USE_SYSTEM_PNG:BOOL=OFF
 USE_SYSTEM_TIFF:BOOL=OFF
 USE_SYSTEM_GLFW:BOOL=OFF
 USE_SYSTEM_GLEW:BOOL=OFF
+USE_SYSTEM_GLUT:BOOL=OFF
 USE_SYSTEM_EXPAT:BOOL=OFF
-OTB_WRAP_JAVA:BOOL=OFF
 ENABLE_QT4:BOOL=ON
 BUILD_ICE:BOOL=ON
 BUILD_MONTEVERDI2:BOOL=ON
-BUILD_ICE_APPLICATION:BOOL=OFF
+BUILD_EXAMPLES=OFF
+BUILD_TESTING=OFF
+BUILD_ICE_APPLICATION:BOOL=ON
 OTB_DATA_LARGEINPUT_ROOT:PATH=${DATA_ROOT}/OTB-LargeInput
 CTEST_USE_LAUNCHERS:BOOL=${CTEST_USE_LAUNCHERS}")
 
-# ctest_empty_binary_directory (${CTEST_BINARY_DIRECTORY})
+##pull from OTB, Ice, Monteverdi2
+#execute_process(COMMAND ${CMAKE_COMMAND} -E remove_directory ${CTEST_BINARY_DIRECTORY}/OTB/build)
+execute_process(COMMAND ${CMAKE_COMMAND} -E remove_directory ${CTEST_BINARY_DIRECTORY}/ICE/build)
+execute_process(COMMAND ${CMAKE_COMMAND} -E remove_directory ${CTEST_BINARY_DIRECTORY}/MONTEVERDI2/build)
 
-#clear stamps file
-set(OTB_STAMP_DIR ${CTEST_BINARY_DIRECTORY}/OTB/src/OTB-stamp)
-execute_process(COMMAND ${CMAKE_COMMAND} -E remove ${OTB_STAMP_DIR}/OTB-patch)
-execute_process(COMMAND ${CMAKE_COMMAND} -E remove ${OTB_STAMP_DIR}/OTB-configure)
-execute_process(COMMAND ${CMAKE_COMMAND} -E remove ${OTB_STAMP_DIR}/OTB-build)
-execute_process(COMMAND ${CMAKE_COMMAND} -E remove ${OTB_STAMP_DIR}/OTB-install)
-execute_process(COMMAND ${CMAKE_COMMAND} -E remove ${OTB_STAMP_DIR}/OTB-done)
-
-#clear otb install
-execute_process(COMMAND ${CMAKE_COMMAND} -E remove_directory ${CTEST_INSTALL_DIRECTORY}/include/otb)
-execute_process(COMMAND ${CMAKE_COMMAND} -E remove_directory ${CTEST_INSTALL_DIRECTORY}/lib/otb)
-
-#empty otb build directory
-ctest_empty_binary_directory ( ${CTEST_BINARY_DIRECTORY}/OTB/build/ )
-
-ctest_start(Experimental)
+ctest_start(Nightly)
 ctest_update(SOURCE "${CTEST_SOURCE_DIRECTORY}")
 file(WRITE "${CTEST_BINARY_DIRECTORY}/CMakeCache.txt" ${SUPERBUILD_INITIAL_CACHE})
 ctest_configure (BUILD "${CTEST_BINARY_DIRECTORY}")
 
-ctest_read_custom_files(${CTEST_BINARY_DIRECTORY})
+ctest_read_custom_files(${CTEST_BINARY_DIRECTORY}/MONTEVERDI2)
 
-ctest_build (BUILD "${CTEST_BINARY_DIRECTORY}" TARGET OTB)
-ctest_test (BUILD "${CTEST_BINARY_DIRECTORY}/OTB/build" ${CTEST_TEST_ARGS})
+ctest_build (BUILD "${CTEST_BINARY_DIRECTORY}" TARGET MONTEVERDI2)
+ctest_test (BUILD "${CTEST_BINARY_DIRECTORY}/MONTEVERDI2/build" ${CTEST_TEST_ARGS})
+
+include(${CTEST_BINARY_DIRECTORY}/MONTEVERDI2/src/MONTEVERDI2/CTestConfig.cmake)
+
 ctest_submit ()
