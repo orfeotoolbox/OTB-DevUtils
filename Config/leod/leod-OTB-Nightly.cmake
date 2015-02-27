af1744387cf4bcbfa208c1a0dc1d762206f06cc6
Christophe Palmann <christophe.palmann@c-s.fr>
2015-02-27 12:37:43 +0100

    Updated nighlty scripts for leod

--- Config/leod/leod-OTB-Nightly.cmake
+++ Config/leod/leod-OTB-Nightly.cmake
@@ -3,9 +3,10 @@ set(dashboard_model Nightly)
 set(CTEST_DASHBOARD_ROOT "$ENV{HOME}/Dashboard")
 set(CTEST_SITE "leod.c-s.fr")
 set(CTEST_BUILD_CONFIGURATION Release)
-set(CTEST_BUILD_NAME "MacOSX10.8-${CTEST_BUILD_CONFIGURATION}")
+set(CTEST_BUILD_NAME "MacOSX10.10-${CTEST_BUILD_CONFIGURATION}")
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
-set(CTEST_BUILD_COMMAND "/usr/bin/make -j8 -i -k" )
+set(CTEST_CMAKE_COMMAND "cmake" )
+set(CTEST_BUILD_COMMAND "/usr/bin/make -j8 -i -k install" )
 set(CTEST_TEST_ARGS PARALLEL_LEVEL 4)
 set(CTEST_TEST_TIMEOUT 500)
 
@@ -24,12 +25,17 @@ set(dashboard_hg_branch "default")
 
 set(ENV{DISPLAY} ":0.0")
 
+#set(CTEST_INITIAL_CACHE "
+#BUILDNAME:STRING=${CTEST_BUILD_NAME}
+#SITE:STRING=${CTEST_SITE}
+#CTEST_USE_LAUNCHERS:BOOL=ON
 
 macro(dashboard_hook_init)
   set(dashboard_cache "${dashboard_cache}
   
-CMAKE_LIBRARY_PATH:PATH=/opt/local/lib
-CMAKE_INCLUDE_PATH:PATH=/opt/local/include
+#CMAKE_LIBRARY_PATH:PATH=/opt/local/lib
+#CMAKE_INCLUDE_PATH:PATH=/opt/local/include
+CMAKE_PREFIX_PATH:PATH=/opt/local
   
 CMAKE_C_FLAGS:STRING= -fPIC -Wall -Wno-deprecated -Wno-uninitialized -Wno-unused-variable
 CMAKE_CXX_FLAGS:STRING= -fPIC -Wall -Wno-deprecated -Wno-uninitialized -Wno-unused-variable -Wno-gnu -Wno-overloaded-virtual
@@ -39,8 +45,6 @@ OPENTHREADS_CONFIG_HAS_BEEN_RUN_BEFORE:BOOL=ON
 
 BUILD_TESTING:BOOL=ON
 BUILD_EXAMPLES:BOOL=ON
-BUILD_APPLICATIONS:BOOL=ON
-BUILD_BUG_TRACKER_TESTS:BOOL=OFF
 
 OTB_WRAP_PYTHON:BOOL=ON
 OTB_WRAP_JAVA:BOOL=ON
@@ -50,19 +54,25 @@ OTB_DATA_USE_LARGEINPUT:BOOL=ON
 OTB_DATA_LARGEINPUT_ROOT:STRING=$ENV{HOME}/Data/OTB-LargeInput
 OTB_DATA_ROOT:STRING=$ENV{HOME}/Data/OTB-Data
 
-OTB_USE_EXTERNAL_ITK:BOOL=ON
-ITK_DIR:PATH=${CTEST_DASHBOARD_ROOT}/itkv4/build
-
 OTB_USE_CURL:BOOL=ON
 OTB_USE_PATENTED:BOOL=OFF
 OTB_USE_EXTERNAL_BOOST:BOOL=ON
 OTB_USE_EXTERNAL_EXPAT:BOOL=ON
 OTB_USE_MAPNIK:BOOL=OFF
 OTB_USE_OPENCV:BOOL=ON
+OTB_USE_SIFTFAST:BOOL=OFF
 
 PYTHON_EXECUTABLE:FILEPATH=/opt/local/bin/python2.7
 PYTHON_INCLUDE_DIR:PATH=/opt/local/Library/Frameworks/Python.framework/Versions/2.7/Headers
 PYTHON_LIBRARY:FILEPATH=/opt/local/Library/Frameworks/Python.framework/Versions/2.7/Python
+
+ITK_DIR:PATH=${CTEST_DASHBOARD_ROOT}/itkv4/build
+
+OSSIM_INCLUDE_DIR:PATH=${CTEST_DASHBOARD_ROOT}/ossim/install/include
+OSSIM_LIBRARY:FILEPATH=${CTEST_DASHBOARD_ROOT}/ossim/install/lib64/libossim.dylib
+
+MUPARSERX_INCLUDE_DIR:PATH=${CTEST_DASHBOARD_ROOT}/muparserx/install/include
+MUPARSERX_LIBRARY:FILEPATH=${CTEST_DASHBOARD_ROOT}/muparserx/install/lib/libmuparserx.dylib
 
     ")
 endmacro()
