cee36892c7523c93c3a125270cddb52b297f6a8f
Manuel Grizonnet <manuel.grizonnet@orfeo-toolbox.org>
2015-03-02 10:38:54 +0100

    minimal otb

--- Config/pc-christophe/pc-christophe-OTB-Minimal.cmake
+++ Config/pc-christophe/pc-christophe-OTB-Minimal.cmake
@@ -3,7 +3,7 @@ set(dashboard_model Nightly)
 set(CTEST_DASHBOARD_ROOT "/home/otbtesting/")
 SET (CTEST_SITE "pc-christophe.cst.cnes.fr")
 set(CTEST_BUILD_CONFIGURATION Debug)
-set(CTEST_BUILD_NAME "Fedora20-64bits-${CTEST_BUILD_CONFIGURATION}")
+set(CTEST_BUILD_NAME "Fedora20-64bits-Minimal-${CTEST_BUILD_CONFIGURATION}")
 set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
 set(CTEST_BUILD_COMMAND "/usr/bin/make -j4 -i -k install" )
 set(CTEST_TEST_ARGS PARALLEL_LEVEL 4)
@@ -26,11 +26,11 @@ set (OTB_INSTALL_PREFIX "${INSTALLROOT}/orfeo/trunk/OTB-Minimal/${CTEST_BUILD_CO
 macro(dashboard_hook_init)
 set(dashboard_cache "${dashboard_cache}
 
-CMAKE_C_FLAGS:STRING=-Wall -Wno-uninitialized -Wno-unused-variable -Wno-unused-but-set-variable
-CMAKE_CXX_FLAGS:STRING=-Wno-cpp -Wextra
+#CMAKE_C_FLAGS:STRING=-Wall -Wno-uninitialized -Wno-unused-variable -Wno-unused-but-set-variable
+#CMAKE_CXX_FLAGS:STRING=-Wno-cpp -Wextra
 CMAKE_INSTALL_PREFIX:PATH=${OTB_INSTALL_PREFIX}
 
-BUILD_APPLICATIONS:BOOL=OFF
+#BUILD_APPLICATIONS:BOOL=OFF
 BUILD_TESTING:BOOL=ON
 BUILD_EXAMPLES:BOOL=OFF
 
@@ -46,8 +46,8 @@ OSSIM_INCLUDE_DIR:PATH=${INSTALLROOT}/ossim/stable/include
 OSSIM_LIBRARY:FILEPATH=${INSTALLROOT}/ossim/stable/lib64/libossim.so
 
 ##external muparserx
-MUPARSERX_LIBRARY:PATH=${INSTALLROOT}/muparserx/lib/libmuparserx.so
-MUPARSERX_INCLUDE_DIR:PATH=${INSTALLROOT}/muparserx/include
+#MUPARSERX_LIBRARY:PATH=${INSTALLROOT}/muparserx/lib/libmuparserx.so
+#MUPARSERX_INCLUDE_DIR:PATH=${INSTALLROOT}/muparserx/include
 
 #PYTHON_EXECUTABLE:FILEPATH=/usr/bin/python
 OTB_WRAP_PYTHON:BOOL=OFF
