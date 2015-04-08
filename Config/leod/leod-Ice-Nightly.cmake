ed68be6444ed41d44c1b25ac549d8445092f0010
Rashad Kanavath <rashad.kanavath@c-s.fr>
2015-04-08 16:55:36 +0200

    leod: use glut from XQuartz to build iceviewer

--- Config/leod/leod-Ice-Nightly.cmake
+++ Config/leod/leod-Ice-Nightly.cmake
@@ -28,10 +28,14 @@ CMAKE_BUILD_TYPE:STRING=${CTEST_BUILD_CONFIGURATION}
 ITK_DIR:PATH=/Users/otbval/Dashboard/itkv4/build
 OTB_DIR:PATH=/Users/otbval/Dashboard/nightly/OTB-Release/build
 
+#use glut from XQuartz - http://hg.orfeo-toolbox.org/Ice/rev/2686f7776582
+
+GLUT_glut_LIBRARY=/usr/X11R6/lib/libglut.3.dylib
+GLUT_INCLUDE_DIR=/usr/X11R6/include
+
 BUILD_TESTING:BOOL=ON
 CMAKE_INSTALL_PREFIX:STRING=${ICE_INSTALL_PREFIX}
-
-BUILD_ICE_APPLICATION:BOOL=OFF
+BUILD_ICE_APPLICATION:BOOL=ON
 ")
 
 set (CTEST_NOTES_FILES
