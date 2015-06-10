5851277bc1e1b2e46c7b383cf580513091ecd4bf
Rashad Kanavath <rashad.kanavath@c-s.fr>
2015-06-10 16:27:00 +0200

    use cmake build instead of vcxproj to fix OTB link error

--- Packaging/osgeo4w/tinyxml/package.cmd
+++ Packaging/osgeo4w/tinyxml/package.cmd
@@ -21,43 +21,48 @@ set B=1
 set W=%OSGEO4W_ROOT%\usr\src\osgeo4w\%P%
 set R=../../release/%P%
 mkdir "%W%"
+
 rmdir "%W%\%P%-%V%" /s /q
 rmdir "%W%\%P%-%V%-install" /s /q
+rmdir "%W%\%P%-%V%-build" /s /q
+mkdir %W%\%P%-%V%-build
+
 mkdir "%OSGEO4W_ROOT%\usr\src\release\%P%"
 
 copy %CURRENT_SCRIPT_DIR%package.cmd "%W%"
 copy %CURRENT_SCRIPT_DIR%setup.hint "%W%"
-copy %CURRENT_SCRIPT_DIR%tinyxml.sln "%W%"
-copy %CURRENT_SCRIPT_DIR%tinyxml_dll.vcxproj "%W%"
+copy %CURRENT_SCRIPT_DIR%CMakeLists.txt "%W%"
 
 cd %W%
+
 wget http://sourceforge.net/projects/%P%/files/%P%/%V%/%P%_2_6_2.tar.gz/download
 if errorlevel 1 (echo Download error & goto exit)
 
 tar xvzf %P%_2_6_2.tar.gz
 if errorlevel 1 (echo Untar error & goto exit)
 rename %P% "%P%-%V%"
-copy tinyxml.sln "%P%-%V%"
-copy tinyxml_dll.vcxproj "%P%-%V%"
+
+::patch
+copy CMakeLists.txt "%P%-%V%"
 
 :: build
-devenv %P%-%V%\%P%.sln /Build "Release|Win32" /Project tinyxml_lib.vcxproj
-devenv %P%-%V%\%P%.sln /Build "Release|Win32" /Project tinyxml_dll.vcxproj
-
-:: install
-mkdir %P%-%V%-install\bin
-mkdir %P%-%V%-install\include
-mkdir %P%-%V%-install\lib
-
-copy %P%-%V%\*.h "%P%-%V%-install\include"
-copy %P%-%V%\Releasetinyxml\%P%.lib "%P%-%V%-install\lib"
-copy %P%-%V%\Releasetinyxml_dll\%P%.dll "%P%-%V%-install\bin"
+cd %P%-%V%-build
+cmake "../%P%-%V%" -G "NMake Makefiles" ^
+    -DCMAKE_INSTALL_PREFIX:STRING="../%P%-%V%-install" ^
+    -DCMAKE_BUILD_TYPE:STRING=Release ^
+    -DBUILD_SHARED_LIBS:BOOL=ON 
+    
+cmake --build . --config Release --target INSTALL
+cmake . -DBUILD_SHARED_LIBS:BOOL=OFF
+cmake --build . --config Release --target INSTALL
+cd ..
 
 :: package
 tar -C %P%-%V%-install -cjf "%R%/%P%-%V%-%B%.tar.bz2" bin include lib
-tar -C ../.. -cjf "%R%/%P%-%V%-%B%-src.tar.bz2"  osgeo4w/%P%/package.cmd osgeo4w/%P%/setup.hint osgeo4w/%P%/tinyxml.sln osgeo4w/%P%/tinyxml_dll.vcxproj
+tar -C ../.. -cjf "%R%/%P%-%V%-%B%-src.tar.bz2"  osgeo4w/%P%/package.cmd osgeo4w/%P%/setup.hint osgeo4w/%P%/CMakeLists.txt
 tar -jtf "%R%/%P%-%V%-%B%.tar.bz2" | sed "s@\\\@/@g" | sed "s@//@/@g" > "%R%\%P%-%V%-%B%.manifest"
 copy setup.hint "%R%"
-:: copy %P%-%V%\COPYING "%R%\%P%-%V%-%B%.txt"
+copy %P%-%V%\readme.txt "%R%\%P%-%V%-%B%.txt"
 
+cd %CURRENT_SCRIPT_DIR%
 :exit
