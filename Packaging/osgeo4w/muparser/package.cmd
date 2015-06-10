647827b9b7a14dd010d4562533c5ea2d7aeb6a4d
Rashad Kanavath <rashad.kanavath@c-s.fr>
2015-06-10 17:07:22 +0200

    use cmake build system for muparser

--- Packaging/osgeo4w/muparser/package.cmd
+++ Packaging/osgeo4w/muparser/package.cmd
@@ -17,13 +17,19 @@ set P=muparser
 set V=2.2.3
 set B=1
 
+
 set W=%OSGEO4W_ROOT%\usr\src\osgeo4w\%P%
 set R=../../release/%P%
 mkdir "%W%"
+rmdir "%W%\%P%-%V%" /s /q
+rmdir "%W%\%P%-%V%-build" /s /q
+rmdir "%W%\%P%-%V%-install" /s /q
+mkdir "%W%\%P%-%V%-build"
 mkdir "%OSGEO4W_ROOT%\usr\src\release\%P%"
 
 copy %CURRENT_SCRIPT_DIR%package.cmd "%W%"
 copy %CURRENT_SCRIPT_DIR%setup.hint "%W%"
+copy %CURRENT_SCRIPT_DIR%CMakeLists.txt "%W%"
 
 cd %W%
 wget --no-check-certificate "https://docs.google.com/uc?export=download&id=0BzuB-ydOOoduLUNRanpDNV9iVk0" -O %P%-%V%.zip
@@ -34,23 +40,24 @@ if errorlevel 1 (echo Unzip error & goto exit)
 
 rename %P%_v2_2_3 "%P%-%V%"
 
+::patch
+copy CMakeLists.txt %P%-%V%
+
 :: build
-cd %P%-%V%/build
-nmake -fmakefile.vc SHARED=1
-cd ../..
+cd %P%-%V%-build
+cmake "../%P%-%V%" -G "NMake Makefiles" ^
+    -DCMAKE_INSTALL_PREFIX:STRING="../%P%-%V%-install" ^
+    -DCMAKE_BUILD_TYPE:STRING=Release ^
+    -DBUILD_SHARED_LIBS:BOOL=ON
 
-:: install
-mkdir %P%-%V%-install\bin
-mkdir %P%-%V%-install\include
-mkdir %P%-%V%-install\lib
-
-copy %P%-%V%\include\*.h "%P%-%V%-install\include"
-copy %P%-%V%\lib\%P%.lib "%P%-%V%-install\lib"
-copy %P%-%V%\lib\%P%.dll "%P%-%V%-install\bin"
+cmake --build . --config Release --target INSTALL
+cmake . -DBUILD_SHARED_LIBS:BOOL=OFF
+cmake --build . --config Release --target INSTALL
+cd ..
 
 :: package
 tar -C %P%-%V%-install -cjf "%R%/%P%-%V%-%B%.tar.bz2" bin include lib
-tar -C ../.. -cjf "%R%/%P%-%V%-%B%-src.tar.bz2"  osgeo4w/%P%/package.cmd osgeo4w/%P%/setup.hint
+tar -C ../.. -cjf "%R%/%P%-%V%-%B%-src.tar.bz2"  osgeo4w/%P%/package.cmd osgeo4w/%P%/setup.hint osgeo4w/%P%/CMakeLists.txt
 tar -jtf "%R%/%P%-%V%-%B%.tar.bz2" | sed "s@\\\@/@g" | sed "s@//@/@g" > "%R%\%P%-%V%-%B%.manifest"
 copy setup.hint "%R%"
 copy %P%-%V%\License.txt "%R%\%P%-%V%-%B%.txt"
