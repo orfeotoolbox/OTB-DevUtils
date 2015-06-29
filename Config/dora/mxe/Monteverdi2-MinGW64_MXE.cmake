093392415f5958563edfb817bc49d36663bd2503
Rashad Kanavath <rashad.kanavath@c-s.fr>
2015-06-29 17:54:27 +0200

    MXE: set mxe target root directory to build mxe package

--- Config/dora/mxe/Monteverdi2-MinGW64_MXE.cmake
+++ Config/dora/mxe/Monteverdi2-MinGW64_MXE.cmake
@@ -13,16 +13,15 @@ macro(dashboard_hook_init)
 set(dashboard_cache "${dashboard_cache}
 
 CMAKE_C_FLAGS:STRING=-Wall -Wno-uninitialized -Wno-unused-variable
-
 CMAKE_CXX_FLAGS:STRING=-Wall -Wno-deprecated -Wno-uninitialized -Wno-unused-variable
-
 BUILD_TESTING:BOOL=ON
+
+MXE_TARGET_DIR:PATH=${MXE_ROOT}/usr/${MXE_TARGET_ARCH}-w64-mingw32.shared
 
 #install otb, ice, monteverdi in the same directory for ease of searching dll and exes
 CMAKE_INSTALL_PREFIX:PATH=${CTEST_DASHBOARD_ROOT}/nightly/install-MinGW-${MXE_TARGET_ARCH}
 
 OTB_DIR:PATH=${CTEST_DASHBOARD_ROOT}/nightly/install-MinGW-${MXE_TARGET_ARCH}/lib/cmake/OTB-5.0
-
 ICE_INCLUDE_DIR:PATH=${CTEST_DASHBOARD_ROOT}/nightly/install-MinGW-${MXE_TARGET_ARCH}/include
 ICE_LIBRARY:FILEPATH=${CTEST_DASHBOARD_ROOT}/nightly/install-MinGW-${MXE_TARGET_ARCH}/lib/libOTBIce.dll.a
 
