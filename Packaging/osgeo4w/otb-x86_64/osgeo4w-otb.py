1d5cf7fbeaa331e52eb52bbe506fb7746bf5e9d8
Rashad Kanavath <rashad.kanavath@c-s.fr>
2015-07-01 13:28:25 +0200

    BUG: incorrect OTB install directory used

--- Packaging/osgeo4w/otb-x86_64/osgeo4w-otb.py
+++ Packaging/osgeo4w/otb-x86_64/osgeo4w-otb.py
@@ -6,7 +6,7 @@ MONTEVERDI2_SRC="C:\\Users\\jmalik\\Dashboard\\src\\Monteverdi2-0.8.1"
 ICE_SRC="C:\\Users\\jmalik\\Dashboard\\src\\Ice-0.3.0"
 #OTB_WRAPPING_SRC="C:\\Users\\jmalik\\Dashboard\\src\\OTB-Wrapping"
 
-OTB_INSTALL="C:\\Users\\jmalik\\Dashboard\\install\\OTB-5.0.0-vc10-x86-Release"
+OTB_INSTALL="C:\\Users\\jmalik\\Dashboard\\install\\OTB-5.0.0-vc10-amd64-Release"
 MONTEVERDI_INSTALL="C:\\Users\\jmalik\\Dashboard\\install\\Monteverdi-1.24.0-vc10-amd64-Release"
 MONTEVERDI2_INSTALL="C:\\Users\\jmalik\\Dashboard\\install\\Monteverdi2-0.8.1-vc10-amd64-Release"
 ICE_INSTALL="C:\\Users\\jmalik\\Dashboard\\install\\Ice-0.3.0-vc10-amd64-Release"
