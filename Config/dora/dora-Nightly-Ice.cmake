eb407e650094d14381e985ef81e0ace2be77fcb1
Mickaël Savinaud <mickael.savinaud@c-s.fr>
2015-07-08 10:01:06 +0200

    ENH: move to git the dora nightly script for Ice

--- Config/dora/dora-Nightly-Ice.cmake
+++ Config/dora/dora-Nightly-Ice.cmake
@@ -1,53 +1,40 @@
-set (ENV{DISPLAY} ":0.0")
-set (ENV{LANG} "C")
+####################################
+# Client maintainer: julien.malik@c-s.fr
+set(dashboard_model Experimental)
+set(CTEST_DASHBOARD_ROOT "/home/otbval/Dashboard")
+set(CTEST_SITE "dora.c-s.fr")
+set(CTEST_BUILD_CONFIGURATION Release)
+set(CTEST_BUILD_NAME "Ubuntu12.04-64bits-${CTEST_BUILD_CONFIGURATION}")
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set(CTEST_BUILD_COMMAND "/usr/bin/make -j6 -i -k install" )
+set(CTEST_TEST_ARGS PARALLEL_LEVEL 4)
+set(CTEST_TEST_TIMEOUT 500)
 
-set (CTEST_BUILD_CONFIGURATION "Release")
+set(CTEST_GIT_COMMAND "/usr/bin/git")
 
-set (CTEST_SOURCE_DIRECTORY "/home/otbval/Dashboard/nightly/Ice/src")
-set (CTEST_BINARY_DIRECTORY "/home/otbval/Dashboard/nightly/Ice/build")
-set (CTEST_CMAKE_GENERATOR  "Unix Makefiles")
-set (CTEST_CMAKE_COMMAND "cmake" )
-set (CTEST_BUILD_COMMAND "/usr/bin/make -j6 -i -k install" )
-set (CTEST_SITE "dora.c-s.fr" )
-set (CTEST_BUILD_NAME "Ubuntu12.04-64bits-${CTEST_BUILD_CONFIGURATION}")
-set (CTEST_HG_COMMAND "/usr/bin/hg")
-set (CTEST_HG_UPDATE_OPTIONS "")
-set (CTEST_USE_LAUNCHERS ON)
+set(dashboard_fresh_source_checkout OFF)
+set(dashboard_source_name "nightly/Ice/src")
+set(dashboard_binary_name "nightly/Ice/build")
+
+set(dashboard_git_url "https://git@git.orfeo-toolbox.org/git/ice.git") 
 
 set (ICE_INSTALL_PREFIX "/home/otbval/Dashboard/nightly/Ice/install")
 
-set (ICE_INITIAL_CACHE "
-BUILDNAME:STRING=${CTEST_BUILD_NAME}
-SITE:STRING=${CTEST_SITE}
-
-CMAKE_C_FLAGS:STRING=-Wall
-CMAKE_CXX_FLAGS:STRING=-Wall
-
-CMAKE_BUILD_TYPE:STRING=${CTEST_BUILD_CONFIGURATION}
-
-# OTB_DIR:PATH=/home/otbval/Dashboard/nightly/OTB-Release/install/lib/otb
-OTB_DIR:PATH=$ENV{HOME}/Dashboard/nightly/OTB-Release/install/lib/cmake/OTB-4.5
+macro(dashboard_hook_init)
+  set(dashboard_cache "${dashboard_cache}
+  
+CMAKE_C_FLAGS:STRING= -Wall
+CMAKE_CXX_FLAGS:STRING= -Wall
 
 BUILD_TESTING:BOOL=ON
+
+OTB_DIR:PATH=$ENV{HOME}/Dashboard/nightly/OTB-Release/install/lib/cmake/OTB-5.0
+
 CMAKE_INSTALL_PREFIX:STRING=${ICE_INSTALL_PREFIX}
 
 BUILD_ICE_APPLICATION:BOOL=OFF
-")
 
-set (CTEST_NOTES_FILES
-${CTEST_SCRIPT_DIRECTORY}/${CTEST_SCRIPT_NAME}
-${CTEST_BINARY_DIRECTORY}/CMakeCache.txt
-)
+    ")
+endmacro()
 
-execute_process (COMMAND ${CTEST_CMAKE_COMMAND} -E remove_directory ${ICE_INSTALL_PREFIX})
-execute_process (COMMAND ${CTEST_CMAKE_COMMAND} -E make_directory ${ICE_INSTALL_PREFIX})
-ctest_empty_binary_directory (${CTEST_BINARY_DIRECTORY})
-
-ctest_start (Nightly)
-ctest_update (SOURCE "${CTEST_SOURCE_DIRECTORY}")
-file (WRITE "${CTEST_BINARY_DIRECTORY}/CMakeCache.txt" ${ICE_INITIAL_CACHE})
-ctest_configure (BUILD "${CTEST_BINARY_DIRECTORY}")
-ctest_read_custom_files (${CTEST_BINARY_DIRECTORY})
-ctest_build (BUILD "${CTEST_BINARY_DIRECTORY}")
-ctest_test (BUILD "${CTEST_BINARY_DIRECTORY}" PARALLEL_LEVEL 6)
-ctest_submit ()
+include(${CTEST_SCRIPT_DIRECTORY}/../otb_common-git.cmake)
