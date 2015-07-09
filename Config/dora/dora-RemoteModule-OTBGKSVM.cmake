4e27bf8bb48e2f9cdb33063088b29f94f7b0c428
Rashad Kanavath <rashad.kanavath@c-s.fr>
2015-07-09 16:17:46 +0200

    added OTBGKSVM remote module

---
+++ Config/dora/dora-RemoteModule-OTBGKSVM.cmake
@@ -0,0 +1,29 @@
+set(dashboard_module "OTBGKSVM")
+set(dashboard_module_url "https://github.com/jmichel-otb/GKSVM")
+
+set(CTEST_DASHBOARD_ROOT "/home/otbval/Dashboard")
+set(CTEST_SITE "dora.c-s.fr")
+set(CTEST_BUILD_CONFIGURATION Release)
+set(CTEST_BUILD_NAME "Ubuntu12.04-64bits-${CTEST_BUILD_CONFIGURATION}-${dashboard_module}")
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set(CTEST_GIT_COMMAND "/usr/bin/git")
+set(CTEST_GIT_UPDATE_OPTIONS "reset --hard origin/nightly")
+set(CTEST_BUILD_COMMAND "/usr/bin/make -j8 -i -k" )
+set(CTEST_TEST_ARGS PARALLEL_LEVEL 4)
+set(CTEST_TEST_TIMEOUT 1500)
+set(CTEST_TEST_ARGS INCLUDE_LABEL ${dashboard_module})
+
+set(dashboard_model "Nightly")
+set(dashboard_source_name "nightly/OTB-${CTEST_BUILD_CONFIGURATION}/src")
+set(dashboard_binary_name "nightly/OTB-${CTEST_BUILD_CONFIGURATION}/build-${dashboard_module}")
+
+set(dashboard_cache "
+BUILD_EXAMPLES:BOOL=OFF
+OTB_DATA_ROOT:STRING=$ENV{HOME}/Data/OTB-Data
+BUILD_SHARED_LIBS:BOOL=ON
+BUILD_TESTING:BOOL=ON
+OTB_BUILD_DEFAULT_MODULES:BOOL=OFF
+Module_${dashboard_module}:BOOL=ON
+")
+
+include(${CTEST_SCRIPT_DIRECTORY}/../otb_common.cmake)
