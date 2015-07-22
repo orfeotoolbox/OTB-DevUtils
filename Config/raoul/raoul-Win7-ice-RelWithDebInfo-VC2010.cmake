581d04d59e4727eacaadc54ff88cfa8b781acbd7
Rashad Kanavath <rashad.kanavath@c-s.fr>
2015-07-22 10:15:55 +0200

    raoul: fix typo in ice cmake script

--- Config/raoul/raoul-Win7-ice-RelWithDebInfo-VC2010.cmake
+++ Config/raoul/raoul-Win7-ice-RelWithDebInfo-VC2010.cmake
@@ -6,22 +6,20 @@ set(CTEST_BUILD_TARGET INSTALL)
 include(${CTEST_SCRIPT_DIRECTORY}/raoul_common.cmake)
 
 macro(dashboard_hook_init)
-  set(dashboard_cache "${dashboard_cache}
+set(dashboard_cache
+"
 BUILDNAME:STRING=${CTEST_BUILD_NAME}
 SITE:STRING=${CTEST_SITE}
-
 CMAKE_INSTALL_PREFIX:PATH=${CTEST_INSTALL_PREFIX}
-
 CMAKE_INCLUDE_PATH:PATH=$ENV{OSGEO4W_ROOT}/include
 CMAKE_LIBRARY_PATH:PATH=$ENV{OSGEO4W_ROOT}/lib
 CMAKE_BUILD_TYPE:STRING=${CTEST_BUILD_CONFIGURATION}
 OTB_DIR:PATH=${CTEST_DASHBOARD_ROOT}/install/OTB-vc10-${OTB_ARCH}-${CTEST_BUILD_CONFIGURATION}/lib/cmake/OTB-5.0
-
 ")
 endmacro()
 
 #remove install dir
-execute_process(COMMAND ${CTEST_CMAKE_COMMAND} -E remove_directory ${CTEST_INSTALL_PREFIX}
+execute_process(COMMAND ${CTEST_CMAKE_COMMAND} -E remove_directory ${CTEST_INSTALL_PREFIX})
 execute_process(COMMAND ${CTEST_CMAKE_COMMAND} -E make_directory ${CTEST_INSTALL_PREFIX})
 
 include(${CTEST_SCRIPT_DIRECTORY}/../otb_common.cmake)
