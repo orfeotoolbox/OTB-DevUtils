74c0d1d877a846c6e371c747df3e1bc4ab6f6483
Rashad Kanavath <rashad.kanavath@c-s.fr>
2015-08-18 17:41:52 +0200

    leod: clear no_submit for examples

---
+++ Config/leod/leod-OTB-Nightly-Examples.cmake
@@ -0,0 +1,45 @@
+# Client maintainer: julien.malik@c-s.fr
+set(dashboard_model Nightly)
+set(CTEST_DASHBOARD_ROOT "$ENV{HOME}/Dashboard")
+set(CTEST_SITE "leod.c-s.fr")
+set(CTEST_BUILD_CONFIGURATION Release)
+set(CTEST_BUILD_NAME "MacOSX10.10-${CTEST_BUILD_CONFIGURATION}")
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set(CTEST_CMAKE_COMMAND "cmake" )
+set(CTEST_BUILD_COMMAND "/usr/bin/make -j8" )
+set(CTEST_TEST_ARGS PARALLEL_LEVEL 4)
+set(CTEST_TEST_TIMEOUT 500)
+set(CTEST_DASHBOARD_TRACK Examples)
+set(CTEST_GIT_COMMAND "/usr/bin/git")
+
+set(CTEST_NIGHTLY_START_TIME "20:00:00 CEST")
+set(CTEST_DROP_METHOD "http")
+set(CTEST_DROP_SITE "dash.orfeo-toolbox.org")
+set(CTEST_DROP_LOCATION "/submit.php?project=OTB")
+set(CTEST_DROP_SITE_CDASH TRUE)
+
+string(TOLOWER ${dashboard_model} lcdashboard_model)
+
+set(dashboard_update_dir "${CTEST_DASHBOARD_ROOT}/${lcdashboard_model}/OTB-${CTEST_BUILD_CONFIGURATION}/src")
+set(dashboard_source_name "${lcdashboard_model}/OTB-${CTEST_BUILD_CONFIGURATION}/src/Examples")
+set(dashboard_binary_name "${lcdashboard_model}/OTB-${CTEST_BUILD_CONFIGURATION}/build-examples")
+
+#set(dashboard_fresh_source_checkout OFF)
+set(dashboard_git_url "https://git@git.orfeo-toolbox.org/git/otb.git")
+#set(dashboard_hg_branch "default")
+
+set(ENV{DISPLAY} ":0.0")
+
+macro(dashboard_hook_init)
+set(dashboard_cache "
+BUILD_TESTING:BOOL=ON
+BUILD_EXAMPLES:BOOL=ON
+OTB_DATA_USE_LARGEINPUT:BOOL=ON
+OTB_DATA_LARGEINPUT_ROOT:STRING=$ENV{HOME}/Data/OTB-LargeInput
+OTB_DATA_ROOT:STRING=$ENV{HOME}/Data/OTB-Data
+OTB_DIR:PATH=${CTEST_DASHBOARD_ROOT}/${lcdashboard_model}/OTB-${CTEST_BUILD_CONFIGURATION}/build
+OTB_BUILD_DEFAULT_MODULES:BOOL=ON
+")
+endmacro()
+
+include(${CTEST_SCRIPT_DIRECTORY}/../otb_common.cmake)
