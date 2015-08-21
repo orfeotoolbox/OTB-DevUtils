824f01c86e28a971e6e6e2089e458c7d15a085e5
Grizonnet Manuel <manuel.grizonnet@cnes.fr>
2015-08-21 16:43:47 +0200

    ENH: update coverity platform

--- Config/pc-christophe/pc-christophe-OTB-Nightly-Coverity.cmake
+++ Config/pc-christophe/pc-christophe-OTB-Nightly-Coverity.cmake
@@ -18,7 +18,7 @@ SET(BUILD_EXAMPLES ON CACHE BOOL "" FORCE)
 SET(BUILD_APPLICATIONS ON CACHE BOOL "" FORCE)
 
 ##external ITK
-SET(ITK_DIR "/home/otbtesting/install/itk/stable/Release/lib/cmake/ITK-4.6" CACHE STRING "" FORCE)
+SET(ITK_DIR "/home/otbtesting/install/itk/stable/Release/lib/cmake/ITK-4.7" CACHE STRING "" FORCE)
 
 ##external OSSIM
 SET(OSSIM_INCLUDE_DIR "/home/otbtesting/install/ossim/stable/include" CACHE STRING "" FORCE)
@@ -29,7 +29,7 @@ SET(MUPARSERX_LIBRARY "/home/otbtesting/install/muparserx/lib/libmuparserx.so" C
 SET(MUPARSERX_INCLUDE_DIR "/home/otbtesting/install/muparserx/include" CACHE STRING "" FORCE)
 
 #external openjpeg
-SET(OpenJPEG_DIR "/home/otbtesting/install/openjpeg/trunk/lib/openjpeg-2.1" CACHE STRING "" FORCE)
+SET(OpenJPEG_DIR "/home/otbtesting/install/openjpeg/stable/lib/openjpeg-2.0" CACHE STRING "" FORCE)
 
 # These options are not available anymore
 SET(OTB_USE_PATENTED ON CACHE BOOL "" FORCE)
