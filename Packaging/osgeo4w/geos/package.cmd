6e41258c5e1dcab6a9ef4edb6427cf4ec6818cc6
Rashad Kanavath <rashad.kanavath@c-s.fr>
2015-11-05 12:10:13 +0100

    PKG: update osgeo4w packaging for geos-3.5.0

--- Packaging/osgeo4w/geos/package.cmd
+++ Packaging/osgeo4w/geos/package.cmd
@@ -1,6 +1,6 @@
 :: GEOS
 :: 
-:: Source http://download.osgeo.org/geos/geos-3.4.2.tar.bz2
+:: Source http://download.osgeo.org/geos/geos-3.5.0.tar.bz2
 :: prerequisite: setup.hint
 
 set CURRENT_SCRIPT_DIR=%~dp0
@@ -12,32 +12,35 @@ pause
 goto exit
 )
 
-set P=geos_all
+set P=geos
 set P1=geos
-set V=3.4.2
-set B=2
+set V=3.5.0
+set B=1
 
 set W=%OSGEO4W_ROOT%\usr\src\osgeo4w\%P%
 set R=../../release/%P%
+set R1=%OSGEO4W_ROOT%\usr\src\release\%P%
 
 rmdir "%W%" /s /q
-rmdir "%R%" /s /q
+rmdir "%R1%" /s /q
 
 mkdir "%W%"
-mkdir "%OSGEO4W_ROOT%\usr\src\release\%P%"
+mkdir "%R1%"
 
 copy %CURRENT_SCRIPT_DIR%package.cmd "%W%"
 copy %CURRENT_SCRIPT_DIR%setup.hint "%W%"
+copy %CURRENT_SCRIPT_DIR%CMakeLists.txt "%W%"
+copy %CURRENT_SCRIPT_DIR%GenerateSourceGroups.cmake "%W%"
 
 cd %W%
-rmdir "%W%\%P%-%V%" /s /q
 wget http://download.osgeo.org/%P1%/%P1%-%V%.tar.bz2
 if errorlevel 1 (echo Download error & goto exit)
 
 tar xf %P1%-%V%.tar.bz2
 if errorlevel 1 (echo Untar error & goto exit)
 
-rename %P1%-%V% %P%-%V%
+copy "%W%\CMakeLists.txt " "%P%-%V%"
+copy %W%\GenerateSourceGroups.cmake "%P%-%V%\cmake\modules"
 
 :: build
 mkdir "%P%-%V%-build"
@@ -46,6 +49,7 @@ cd %P%-%V%-build
 cmake "../%P%-%V%" -G "NMake Makefiles" ^
  -DCMAKE_BUILD_TYPE:STRING=Release ^
  -DBUILD_SHARED_LIBS:BOOL=ON ^
+ -DCMAKE_CXX_FLAGS:STRING="-DNOMINMAX  /EHsc" ^
  -DCMAKE_INSTALL_PREFIX:STRING="../%P%-%V%-install" ^
  -DGEOS_ENABLE_TESTS:BOOL=OFF
 
@@ -54,12 +58,12 @@ cmake --build . --config Release --target INSTALL
 cd ..
 
 :: package
- tar -C %P%-%V%-install -cjf "%R%/%P%-%V%-%B%.tar.bz2" include lib bin
-tar -C ../.. -cjf "%R%/%P%-%V%-%B%-src.tar.bz2" osgeo4w/%P%/package.cmd osgeo4w/%P%/setup.hint
+tar -C %P%-%V%-install -cjf "%R%/%P%-%V%-%B%.tar.bz2" include lib bin
+tar -C ../.. -cjf "%R%/%P%-%V%-%B%-src.tar.bz2" osgeo4w/%P%/package.cmd osgeo4w/%P%/setup.hint osgeo4w/%P%/GenerateSourceGroups.cmake osgeo4w/%P%/CMakeLists.txt
 
-tar -jtf "%R%/%P%-%V%-%B%.tar.bz2" | sed "s@\\\@/@g" | sed "s@//@/@g" > "%R%\%P%-%V%-%B%.manifest"
-copy setup.hint "%R%"
-copy %P%-%V%\COPYING "%R%\%P%-%V%-%B%.txt"
+tar -jtf "%R%/%P%-%V%-%B%.tar.bz2" | sed "s@\\\@/@g" | sed "s@//@/@g" > "%R%/%P%-%V%-%B%.manifest"
+copy setup.hint "%R1%"
+copy %P%-%V%\COPYING "%R1%\%P%-%V%-%B%.txt"
 
 cd %CURRENT_SCRIPT_DIR%
 
