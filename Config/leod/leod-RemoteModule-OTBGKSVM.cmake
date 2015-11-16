3249b8e90d4d579a445660930032fa85ae7aa492
Guillaume Pasero <guillaume.pasero@c-s.fr>
2015-11-16 11:26:30 +0100

    TEST: leod: make sure the right GDAL lib is used

--- Config/leod/leod-RemoteModule-OTBGKSVM.cmake
+++ Config/leod/leod-RemoteModule-OTBGKSVM.cmake
@@ -17,13 +17,21 @@ set(dashboard_source_name "nightly/OTB-${CTEST_BUILD_CONFIGURATION}/src")
 set(dashboard_binary_name "nightly/OTB-${CTEST_BUILD_CONFIGURATION}/build-${dashboard_module}")
 
 set(dashboard_cache "
+CMAKE_PREFIX_PATH:PATH=/opt/local
+CMAKE_C_FLAGS:STRING= -fPIC -Wall -Wno-deprecated -Wno-uninitialized -Wno-unused-variable
+CMAKE_CXX_FLAGS:STRING= -fPIC -Wall -Wno-deprecated -Wno-uninitialized -Wno-unused-variable -Wno-gnu -Wno-overloaded-virtual -Wno-\\\\#warnings
+
 OTB_DATA_ROOT:STRING=$ENV{HOME}/Data/OTB-Data
 ITK_DIR:PATH=${CTEST_DASHBOARD_ROOT}/itkv4/install/lib/cmake/ITK-4.8
 OSSIM_INCLUDE_DIR:PATH=${CTEST_DASHBOARD_ROOT}/ossim/install/include
 OSSIM_LIBRARY:FILEPATH=${CTEST_DASHBOARD_ROOT}/ossim/install/lib/libossim.dylib
 
+GDAL_CONFIG:PATH=/opt/local/bin/gdal-config
+GDAL_CONFIG_CHECKING:BOOL=ON
+GDAL_INCLUDE_DIR:PATH=/opt/local/include
+GDAL_LIBRARY:PATH=/opt/local/lib/libgdal.dylib
+
 BUILD_EXAMPLES:BOOL=OFF
-OTB_DATA_ROOT:STRING=$ENV{HOME}/Data/OTB-Data
 BUILD_SHARED_LIBS:BOOL=ON
 BUILD_TESTING:BOOL=ON
 OTB_BUILD_DEFAULT_MODULES:BOOL=OFF
--- Config/leod/leod-RemoteModule-SertitObject.cmake
+++ Config/leod/leod-RemoteModule-SertitObject.cmake
@@ -12,16 +12,24 @@ set(CTEST_BUILD_COMMAND "/usr/bin/make -j8 -i -k" )
 set(CTEST_TEST_ARGS PARALLEL_LEVEL 4)
 set(CTEST_TEST_TIMEOUT 1500)
 
-
 set(dashboard_model "Nightly")
 set(dashboard_source_name "nightly/OTB-${CTEST_BUILD_CONFIGURATION}/src")
 set(dashboard_binary_name "nightly/OTB-${CTEST_BUILD_CONFIGURATION}/build-${dashboard_module}")
 
 set(dashboard_cache "
+CMAKE_PREFIX_PATH:PATH=/opt/local
+CMAKE_C_FLAGS:STRING= -fPIC -Wall -Wno-deprecated -Wno-uninitialized -Wno-unused-variable
+CMAKE_CXX_FLAGS:STRING= -fPIC -Wall -Wno-deprecated -Wno-uninitialized -Wno-unused-variable -Wno-gnu -Wno-overloaded-virtual -Wno-\\\\#warnings
+
 OTB_DATA_ROOT:STRING=$ENV{HOME}/Data/OTB-Data
 ITK_DIR:PATH=${CTEST_DASHBOARD_ROOT}/itkv4/install/lib/cmake/ITK-4.8
 OSSIM_INCLUDE_DIR:PATH=${CTEST_DASHBOARD_ROOT}/ossim/install/include
 OSSIM_LIBRARY:FILEPATH=${CTEST_DASHBOARD_ROOT}/ossim/install/lib/libossim.dylib
+
+GDAL_CONFIG:PATH=/opt/local/bin/gdal-config
+GDAL_CONFIG_CHECKING:BOOL=ON
+GDAL_INCLUDE_DIR:PATH=/opt/local/include
+GDAL_LIBRARY:PATH=/opt/local/lib/libgdal.dylib
 
 BUILD_EXAMPLES:BOOL=OFF
 BUILD_SHARED_LIBS:BOOL=ON
@@ -29,6 +37,5 @@ BUILD_TESTING:BOOL=ON
 OTB_BUILD_DEFAULT_MODULES:BOOL=OFF
 Module_${dashboard_module}:BOOL=ON
 ")
-
 
 include(${CTEST_SCRIPT_DIRECTORY}/../otb_common.cmake)
