1a84e152787328882907a491f6d76f2ab37af411
Guillaume Pasero <guillaume.pasero@c-s.fr>
2015-11-16 10:52:32 +0100

    TEST: otbGRM: use the same cmake settings as in nightly

--- Config/leod/leod-RemoteModule-otbGRM.cmake
+++ Config/leod/leod-RemoteModule-otbGRM.cmake
@@ -25,17 +25,47 @@ set(dashboard_source_name "nightly/OTB-${CTEST_BUILD_CONFIGURATION}/src")
 set(dashboard_binary_name "nightly/OTB-${CTEST_BUILD_CONFIGURATION}/build-${dashboard_module}")
 
 set(dashboard_cache "
-OTB_DATA_ROOT:STRING=$ENV{HOME}/Data/OTB-Data
-ITK_DIR:PATH=${CTEST_DASHBOARD_ROOT}/itkv4/install/lib/cmake/ITK-4.8
-OSSIM_INCLUDE_DIR:PATH=${CTEST_DASHBOARD_ROOT}/ossim/install/include
-OSSIM_LIBRARY:FILEPATH=${CTEST_DASHBOARD_ROOT}/ossim/install/lib/libossim.dylib
+CMAKE_PREFIX_PATH:PATH=/opt/local
+CMAKE_C_FLAGS:STRING= -fPIC -Wall -Wno-deprecated -Wno-uninitialized -Wno-unused-variable
+CMAKE_CXX_FLAGS:STRING= -fPIC -Wall -Wno-deprecated -Wno-uninitialized -Wno-unused-variable -Wno-gnu -Wno-overloaded-virtual -Wno-\\\\#warnings
 
-BUILD_EXAMPLES:BOOL=OFF
-OTB_DATA_ROOT:STRING=$ENV{HOME}/Data/OTB-Data
+OPENTHREADS_CONFIG_HAS_BEEN_RUN_BEFORE:BOOL=ON
+
 BUILD_SHARED_LIBS:BOOL=ON
 BUILD_TESTING:BOOL=ON
+BUILD_EXAMPLES:BOOL=OFF
+
+OTB_WRAP_PYTHON:BOOL=ON
+OTB_WRAP_JAVA:BOOL=OFF
+OTB_WRAP_QT:BOOL=OFF
+
+OTB_DATA_USE_LARGEINPUT:BOOL=OFF
+OTB_DATA_ROOT:STRING=$ENV{HOME}/Data/OTB-Data
 OTB_BUILD_DEFAULT_MODULES:BOOL=OFF
 Module_${dashboard_module}:BOOL=ON
+
+OTB_USE_CURL:BOOL=ON
+OTB_USE_LIBKML:BOOL=ON
+OTB_USE_LIBSVM:BOOL=OFF
+OTB_USE_MUPARSER:BOOL=OFF
+OTB_USE_MUPARSERX:BOOL=OFF
+OTB_USE_OPENCV:BOOL=OFF
+OTB_USE_OPENJPEG:BOOL=ON
+OTB_USE_QT4:BOOL=OFF
+
+PYTHON_EXECUTABLE:FILEPATH=/opt/local/bin/python2.7
+PYTHON_INCLUDE_DIR:PATH=/opt/local/Library/Frameworks/Python.framework/Versions/2.7/Headers
+PYTHON_LIBRARY:FILEPATH=/opt/local/Library/Frameworks/Python.framework/Versions/2.7/Python
+
+ITK_DIR:PATH=${CTEST_DASHBOARD_ROOT}/itkv4/install/lib/cmake/ITK-4.8
+
+GDAL_CONFIG:PATH=/opt/local/bin/gdal-config
+GDAL_CONFIG_CHECKING:BOOL=ON
+GDAL_INCLUDE_DIR:PATH=/opt/local/include
+GDAL_LIBRARY:PATH=/opt/local/lib/libgdal.dylib
+
+OSSIM_INCLUDE_DIR:PATH=${CTEST_DASHBOARD_ROOT}/ossim/install/include
+OSSIM_LIBRARY:FILEPATH=${CTEST_DASHBOARD_ROOT}/ossim/install/lib/libossim.dylib
 ")
 
 include(${CTEST_SCRIPT_DIRECTORY}/../otb_common.cmake)
