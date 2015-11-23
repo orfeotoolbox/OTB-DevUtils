f3114e362021cc9b43fa3aff7c8505ad49b36432
Rashad Kanavath <rashad.kanavath@c-s.fr>
2015-11-23 18:21:30 +0100

    TEST: pc-christophe: try no supress of warnings in dashboard script

--- Config/pc-christophe/pc-christophe-OTB-Nightly-clang.cmake
+++ Config/pc-christophe/pc-christophe-OTB-Nightly-clang.cmake
@@ -26,8 +26,8 @@ set(dashboard_cache "${dashboard_cache}
 
 CMAKE_C_COMPILER=/usr/bin/clang
 CMAKE_CXX_COMPILER=/usr/bin/clang++
-CMAKE_C_FLAGS:STRING=-Wall -Wno-uninitialized  -Wno-unused-variable -Wno-gnu
-CMAKE_CXX_FLAGS:STRING=-Wall -Wno-deprecated -Wno-uninitialized -Wno-gnu -Wno-overloaded-virtual -Wshorten-64-to-32
+CMAKE_C_FLAGS:STRING=-Wall
+CMAKE_CXX_FLAGS:STRING=-Wall -Wno-gnu-static-float-init
 CMAKE_INSTALL_PREFIX=${OTB_INSTALL_PREFIX}
 
 BUILD_APPLICATIONS:BOOL=ON
