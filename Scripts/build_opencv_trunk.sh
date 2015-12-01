901f76e714bae47ac1831a29d1a12f0693bfb872
Victor Poughon <victor.poughon@cnes.fr>
2015-12-01 15:00:36 +0100

    Improve opencv build script
    Previously Script/build_opencv_trunk.sh only built 2.4 branch and called
    it trunk. This new build script takes 3 arguments:
    build_opencv_trunk.sh <sourcedir> <branch> <buildtype>
    
    This allows building both 2.4 and master with the same script. Until OTB
    is compatible with opencv 3.0 we'll agree on the following build type
    names:
    stable =    2.4 branch
    trunk  = master branch
    
    To build opencv trunk (master branch):
    build_opencv_trunk.sh ~/sources master trunk
    
    To build opencv stable (2.4 branch):
    build_opencv_trunk.sh ~/sources 2.4 stable

--- Scripts/build_opencv_trunk.sh
+++ Scripts/build_opencv_trunk.sh
@@ -1,51 +1,52 @@
 #!/bin/bash
-##07-oct-2014: checkout 2.4 branch instead of trunk 
-###ref mantis bug #956
-### http://bugs.orfeo-toolbox.org/view.php?id=956
 
-if [ $# -ne 2 ]; then
- echo 'Usage: '$0' <path/to/source>  <type>'
- echo 'Usage: '$0' /home/user/sources/ trunk'
+set -o nounset
+
+# Right now we consider that
+#    stable = 2.4
+#    trunk  = master
+# because OTB is not yet compatible with opencv 3.0
+# See mantis bug #956 at
+# http://bugs.orfeo-toolbox.org/view.php?id=956
+
+if [ $# -ne 3 ]; then
+ echo 'Usage: '$0' <path/to/source>  <branch> <buildtype>'
+ echo 'Usage: '$0' /home/user/sources/ 2.4 stable'
  exit;
 fi
 
-SRCROOT=$1 #default is /home/otbtesting/sources
-TYPE=$2 #default: trunk
+SRCROOT=$1 # Location of source dir, e.g. /home/otbtesting/sources
+BRANCH=$2 # Branch, e.g master or 2.4
+BUILDTYPE=$3 # trunk or stable
 
-SOURCEDIR=$SRCROOT/opencv/$TYPE
-BUILDDIR=$HOME/build/opencv/$TYPE
+SOURCEDIR=$SRCROOT/opencv/$BUILDTYPE
+BUILDDIR=$HOME/build/opencv/$BUILDTYPE
 INSTALLROOT=$HOME/install
 
-cd $SOURCEDIR
-git pull origin 2.4
-git checkout 2.4
-rm -fr $INSTALLROOT/opencv/$TYPE
-
-if [ -d "$BUILDDIR" ]; then
-    # clean up build dir
-    /bin/rm -fr $BUILDDIR
-else
-    mkdir $BUILDDIR
-fi
-
+echo "Building OpenCV."
 echo 'Source Dir='$SOURCEDIR
 echo 'Build Dir='$BUILDDIR
 echo 'Install Dir='$INSTALLROOT
 
 cd $SOURCEDIR
-git show > $BUILDDIR/opencv_git_show.txt
+git pull origin $BRANCH
+rm -fr $INSTALLROOT/opencv/$BUILDTYPE
 
+# Clean up build dir
+if [ -d "$BUILDDIR" ]; then
+    /bin/rm -fr $BUILDDIR
+fi
 
-#configure opencv
+mkdir -p $BUILDDIR
+mkdir -p $INSTALLROOT
+
+# Configure, build and install
 cd $BUILDDIR
 cmake $SOURCEDIR \
-    -DCMAKE_INSTALL_PREFIX:STRING=$INSTALLROOT/opencv/$TYPE \
+    -DCMAKE_INSTALL_PREFIX:STRING=$INSTALLROOT/opencv/$BUILDTYPE \
     -DCMAKE_BUILD_TYPE:STRING=Release \
     -DBUILD_PACKAGE:BOOL=OFF \
     -DBUILD_SHARED_LIBS:BOOL=ON
 
-#build
-make -j2
-
-#install to $INSTALLROOT
+make -j4
 make install
