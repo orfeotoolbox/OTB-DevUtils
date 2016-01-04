5b51d30909d395754cf9d878d228bc8666c1a95e
Victor Poughon <victor.poughon@cnes.fr>
2016-01-04 15:11:36 +0100

    ENH: add pc-christophe documentation submission to dashboard

---
+++ Config/pc-christophe/pc-christophe-OTB-CookBook.cmake
@@ -0,0 +1,36 @@
+set(dashboard_model Nightly)
+set(CTEST_DASHBOARD_ROOT "/home/otbtesting")
+set(CTEST_SITE "pc-christophe.cst.cnes.fr")
+set(CTEST_BUILD_CONFIGURATION Release)
+set(CTEST_BUILD_NAME "Fedora22-64bits-CookBook")
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set(CTEST_BUILD_COMMAND "/usr/bin/make -i -k" )
+set(CTEST_TEST_ARGS PARALLEL_LEVEL 1)
+set(CTEST_TEST_TIMEOUT 500)
+set(CTEST_USE_LAUNCHERS OFF)
+set(CTEST_GIT_COMMAND "/usr/bin/git")
+
+set(dashboard_root_name "tests")
+set(dashboard_source_name "sources/orfeo/otb-documents/CookBook")
+set(dashboard_binary_name "build/orfeo/otb-documents/Cookbook")
+
+set(dashboard_git_url "https://git@git.orfeo-toolbox.org/git/otb-documents.git")
+set(dashboard_update_dir ${CTEST_DASHBOARD_ROOT}/orfeo/otb-documents)
+set(dashboard_git_branch master)
+
+macro(dashboard_hook_init)
+  set(dashboard_cache "${dashboard_cache}
+
+BUILDNAME:STRING=${CTEST_BUILD_NAME}
+SITE:STRING=${CTEST_SITE}
+
+CMAKE_BUILD_TYPE:STRING=${CTEST_BUILD_CONFIGURATION}
+
+OTB_DATA_ROOT:STRING=${CTEST_DASHBOARD_ROOT}/sources/orfeo/OTB-Data
+OTB_DATA_PATHS:STRING=${CTEST_DASHBOARD_ROOT}/sources/orfeo/OTB-Data/Examples::${CTEST_DASHBOARD_ROOT}/sources/orfeo/OTB-Data/Input
+
+OTB_DIR:STRING=${CTEST_DASHBOARD_ROOT}/build/orfeo/trunk/OTB-Nightly/Release
+")
+endmacro()
+
+include(${CTEST_SCRIPT_DIRECTORY}/../otb_common.cmake)
---
+++ Config/pc-christophe/pc-christophe-OTB-SoftwareGuide.cmake
@@ -0,0 +1,40 @@
+set(dashboard_model Nightly)
+set(CTEST_DASHBOARD_ROOT "/home/otbtesting")
+set(CTEST_SITE "pc-christophe.cst.cnes.fr")
+set(CTEST_BUILD_CONFIGURATION Release)
+set(CTEST_BUILD_NAME "Fedora22-64bits-SoftwareGuide")
+set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+set(CTEST_BUILD_COMMAND "/usr/bin/make -i -k" )
+set(CTEST_TEST_ARGS PARALLEL_LEVEL 1)
+set(CTEST_TEST_TIMEOUT 500)
+set(CTEST_USE_LAUNCHERS OFF)
+set(CTEST_GIT_COMMAND "/usr/bin/git")
+
+set(dashboard_root_name "tests")
+set(dashboard_source_name "sources/orfeo/otb-documents/SoftwareGuide")
+set(dashboard_binary_name "build/orfeo/otb-documents/SoftwareGuide")
+
+set(dashboard_git_url "https://git@git.orfeo-toolbox.org/git/otb-documents.git")
+set(dashboard_update_dir ${CTEST_DASHBOARD_ROOT}/orfeo/otb-documents)
+set(dashboard_git_branch master)
+
+macro(dashboard_hook_init)
+  set(dashboard_cache "${dashboard_cache}
+
+BUILDNAME:STRING=${CTEST_BUILD_NAME}
+SITE:STRING=${CTEST_SITE}
+
+CMAKE_BUILD_TYPE:STRING=${CTEST_BUILD_CONFIGURATION}
+
+CMAKE_C_FLAGS:STRING= -Wall
+CMAKE_CXX_FLAGS:STRING= -Wall -Wno-cpp
+
+OTB_DATA_LARGEINPUT_ROOT:STRING=/media/TeraDisk2/LargeInput
+OTB_DATA_ROOT:STRING=${CTEST_DASHBOARD_ROOT}/sources/orfeo/OTB-Data
+
+OTB_DIR:STRING=${CTEST_DASHBOARD_ROOT}/build/orfeo/trunk/OTB-Nightly/Release
+OTB_SOURCE_DIR:PATH=${CTEST_DASHBOARD_ROOT}/sources/orfeo/trunk/OTB-Nightly
+")
+endmacro()
+
+include(${CTEST_SCRIPT_DIRECTORY}/../otb_common.cmake)
