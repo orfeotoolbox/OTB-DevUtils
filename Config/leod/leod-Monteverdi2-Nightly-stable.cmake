d14bd4dc06c17b60b76db630b9554eabd533f3a9
Guillaume Pasero <guillaume.pasero@c-s.fr>
2016-01-19 18:48:49 +0100

    BUG: add commands to create .dmg inside the ctest script

--- Config/leod/leod-Monteverdi2-Nightly-stable.cmake
+++ Config/leod/leod-Monteverdi2-Nightly-stable.cmake
@@ -57,6 +57,25 @@ macro(dashboard_hook_test)
 set(ENV{DYLD_LIBRARY_PATH} /Users/otbval/Dashboard/nightly/OTB-Release/install-stable/lib::/Users/otbval/Dashboard/nightly/Ice-Release/install-stable/lib)
 endmacro()
 
+macro(dashboard_hook_end)
+  find_program(HDIUTIL_EXECUTABLE hdiutil)
+  if(HDIUTIL_EXECUTABLE)
+    file(READ "${CTEST_DASHBOARD_ROOT}/${dashboard_source_name}/CMakeLists.txt" _CMAKEFILE_CONTENT)
+    string(REGEX REPLACE ".*set\\(Monteverdi_VERSION_MAJOR \"([0-9]+)\"\\).*" "\\1" VER_MAJOR "${_CMAKEFILE_CONTENT}")
+    string(REGEX REPLACE ".*set\\(Monteverdi_VERSION_MINOR \"([0-9]+)\"\\).*" "\\1" VER_MINOR "${_CMAKEFILE_CONTENT}")
+    string(REGEX REPLACE ".*set\\(Monteverdi_VERSION_PATCH \"([0-9]+)\"\\).*" "\\1" VER_PATCH "${_CMAKEFILE_CONTENT}")
+    string(REGEX REPLACE ".*set\\(Monteverdi_VERSION_SUFFIX \"([a-zA-Z0-9]*)\"\\).*" "\\1" VER_SUFFIX "${_CMAKEFILE_CONTENT}")
+    set(DMG_VERSION "${VER_MAJOR}.${VER_MINOR}.${VER_PATCH}")
+    if("${VER_SUFFIX}")
+      set(DMG_VERSION "${DMG_VERSION}.${VER_SUFFIX}")
+    endif()
+    execute_process(COMMAND hdiutil create -srcfolder ${MVD2_INSTALL_PREFIX}/Monteverdi.app Monteverdi-${DMG_VERSION}-Darwin.dmg  -megabytes 150
+                    WORKING_DIRECTORY ${CTEST_DASHBOARD_ROOT}/${dashboard_binary_name})
+    execute_process(COMMAND hdiutil create -srcfolder ${MVD2_INSTALL_PREFIX}/Mapla.app Mapla-${DMG_VERSION}-Darwin.dmg
+                    WORKING_DIRECTORY ${CTEST_DASHBOARD_ROOT}/${dashboard_binary_name})
+  endif()
+endmacro()
+
 # Remove install tree
 execute_process(COMMAND ${CTEST_CMAKE_COMMAND} -E remove_directory ${MVD2_INSTALL_PREFIX})
 execute_process(COMMAND ${CTEST_CMAKE_COMMAND} -E make_directory ${MVD2_INSTALL_PREFIX})
