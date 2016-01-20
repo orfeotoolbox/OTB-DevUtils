c7616692af087b75bd50c460eda8c30915b94185
Guillaume Pasero <guillaume.pasero@c-s.fr>
2016-01-20 13:46:58 +0100

    ADD: common variables for leod platform

---
+++ Config/leod/leod_common.cmake
@@ -0,0 +1,32 @@
+# Common variables for leod dashboard
+set(CTEST_DASHBOARD_ROOT "$ENV{HOME}/Dashboard")
+set(CTEST_SITE "leod.c-s.fr")
+set(ENV{DISPLAY} ":0.0")
+set (ENV{LANG} "C")
+
+if(NOT DEFINED CTEST_CMAKE_GENERATOR)
+  set(CTEST_CMAKE_GENERATOR "Unix Makefiles")
+endif(CTEST_CMAKE_GENERATOR)
+
+if(NOT DEFINED CTEST_CMAKE_COMMAND)
+  set(CTEST_CMAKE_COMMAND "cmake" )
+endif()
+
+if(NOT DEFINED CTEST_TEST_ARGS)
+  set(CTEST_TEST_ARGS PARALLEL_LEVEL 4)
+endif()
+
+if(NOT DEFINED CTEST_TEST_TIMEOUT)
+  set(CTEST_TEST_TIMEOUT 1500)
+endif()
+
+if(NOT DEFINED CTEST_GIT_COMMAND)
+  set(CTEST_GIT_COMMAND "/opt/local/bin/git")
+endif()
+
+if(NOT DEFINED CTEST_BUILD_COMMAND)
+  if(NOT DEFINED dashboard_build_command)
+    set(dashboard_build_command "/usr/bin/make -j8 -k" )
+  endif()
+endif()
+
