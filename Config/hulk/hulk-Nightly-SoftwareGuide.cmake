1980f7e4d77897d73ed0bc1c82a89ad1fff4cee4
Guillaume Pasero <guillaume.pasero@c-s.fr>
2016-03-30 18:17:18 +0200

    BUG: switch SG compilation on OTB repository

--- Config/hulk/hulk-Nightly-SoftwareGuide.cmake
+++ Config/hulk/hulk-Nightly-SoftwareGuide.cmake
@@ -6,40 +6,31 @@ set(CTEST_BUILD_COMMAND "/usr/bin/make -i -k" )
 include(${CTEST_SCRIPT_DIRECTORY}/hulk_common.cmake)
 
 set(dashboard_root_name "tests")
-set(dashboard_source_name "src/OTB-Documents/SoftwareGuide")
-set(dashboard_binary_name "build/OTB-Documents/SoftwareGuide")
+set(dashboard_source_name "src/OTB/Documents/SoftwareGuide")
+set(dashboard_binary_name "build/SoftwareGuide")
 
 #set(dashboard_fresh_source_checkout OFF)
-set(dashboard_git_url "https://git@git.orfeo-toolbox.org/git/otb-documents.git")
-set(dashboard_update_dir ${CTEST_DASHBOARD_ROOT}/src/OTB-Documents)
-set(dashboard_git_branch master)
+set(dashboard_git_url "https://git@git.orfeo-toolbox.org/git/otb.git")
+set(dashboard_update_dir ${CTEST_DASHBOARD_ROOT}/src/OTB)
 
 macro(dashboard_hook_init)
   set(dashboard_cache "${dashboard_cache}
 CTEST_USE_LAUNCHERS:BOOL=OFF
 
 CMAKE_BUILD_TYPE:STRING=${CTEST_BUILD_CONFIGURATION}
-GENERATE_WRAPPING_DOC:BOOL=ON
-
-OTB-Wrapping_SOURCE_DIR:PATH=$ENV{HOME}/Dashboard/src/OTB-Wrapping
 
 CMAKE_C_FLAGS:STRING= -Wall
 CMAKE_CXX_FLAGS:STRING= -Wall -Wno-cpp
 
 OTB_DATA_LARGEINPUT_ROOT:STRING=$ENV{HOME}/Data/OTB-LargeInput
 OTB_DATA_ROOT:STRING=$ENV{HOME}/Dashboard/src/OTB-Data
-#OTB_DATA_PATHS:STRING=$ENV{HOME}/Dashboard/src/OTB-Data/Examples::$ENV{HOME}/Dashboard/src/OTB-Data/Input
 
 OTB_DIR:STRING=$ENV{HOME}/Dashboard/build/OTB-Release
 OTB_SOURCE_DIR:PATH=$ENV{HOME}/Dashboard/src/OTB
-OpenCV_DIR:PATH=/usr/share/OpenCV
 ")
 endmacro()
 
 #set(dashboard_no_test 1)
 #set(dashboard_no_submit 1)
-
-execute_process(COMMAND ${CMAKE_COMMAND} -D GIT_COMMAND:PATH=${CTEST_GIT_COMMAND} -D TESTED_BRANCH:STRING=release-5.2 -P ${CTEST_SCRIPT_DIRECTORY}/../git_updater.cmake
-                        WORKING_DIRECTORY $ENV{HOME}/Dashboard/src/OTB)
 
 include(${CTEST_SCRIPT_DIRECTORY}/../otb_common.cmake)
