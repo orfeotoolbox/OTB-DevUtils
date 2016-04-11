6a4d2f327b4e9f4cf9323ab00e346a568f7aae0d
Guillaume Pasero <guillaume.pasero@c-s.fr>
2016-04-11 18:17:39 +0200

    ENH: CB generation switch between trunk and stable

--- Config/hulk/hulk-Nightly-CookBook.cmake
+++ Config/hulk/hulk-Nightly-CookBook.cmake
@@ -5,6 +5,19 @@ set(CTEST_BUILD_NAME "Ubuntu14.04-64bits-CookBook")
 set(CTEST_BUILD_COMMAND "/usr/bin/make -i -k" )
 include(${CTEST_SCRIPT_DIRECTORY}/hulk_common.cmake)
 
+# peek into ../config_stable.cmake to choose between trunk or stable
+file(STRINGS "${CTEST_SCRIPT_DIRECTORY}/../config_stable.cmake" _FULL_CONTENT_STABLE)
+string(TOUPPER "${_FULL_CONTENT_STABLE}" _FULL_CONTENT_STABLE_UP)
+if("${_FULL_CONTENT_STABLE_UP}" MATCHES ".*[^#] *SET *\\( *CONFIG_STABLE_SWITCH +(ON|1|YES|Y|TRUE) *\\).*")
+  set(OTB_PROJECT OTB)
+  include(${CTEST_SCRIPT_DIRECTORY}/../config_stable.cmake)
+  unset(OTB_PROJECT)
+  set(OTB_DIR $ENV{HOME}/Dashboard/build/OTB-stable)
+  set(CTEST_BUILD_NAME "${CTEST_BUILD_NAME}-${OTB_STABLE_VERSION}")
+else()
+  set(OTB_DIR $ENV{HOME}/Dashboard/build/OTB-GDAL_2.0)
+endif()
+
 set(dashboard_root_name "tests")
 set(dashboard_source_name "src/OTB-Documents/CookBook")
 set(dashboard_binary_name "build/OTB-Documents/CookBook")
@@ -23,8 +36,7 @@ CMAKE_BUILD_TYPE:STRING=${CTEST_BUILD_CONFIGURATION}
 OTB_DATA_ROOT:STRING=$ENV{HOME}/Dashboard/src/OTB-Data
 OTB_DATA_PATHS:STRING=$ENV{HOME}/Dashboard/src/OTB-Data/Examples::$ENV{HOME}/Dashboard/src/OTB-Data/Input
 
-OTB_DIR:STRING=$ENV{HOME}/Dashboard/build/OTB-Release
-OpenCV_DIR:PATH=/usr/share/OpenCV
+OTB_DIR:STRING=${OTB_DIR}
 ")
 endmacro()
 
