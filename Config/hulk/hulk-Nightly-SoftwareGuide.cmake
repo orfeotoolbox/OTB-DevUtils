40733b76f0ce21e17f9842b19c18dacd6b48a960
Guillaume Pasero <guillaume.pasero@c-s.fr>
2016-04-11 18:11:48 +0200

    ENH: SG generation switch between trunk and stable

--- Config/hulk/hulk-Nightly-SoftwareGuide.cmake
+++ Config/hulk/hulk-Nightly-SoftwareGuide.cmake
@@ -5,6 +5,19 @@ set(CTEST_BUILD_NAME "Ubuntu14.04-64bits-SoftwareGuide")
 set(CTEST_BUILD_COMMAND "/usr/bin/make -i -k" )
 include(${CTEST_SCRIPT_DIRECTORY}/hulk_common.cmake)
 
+# peek into ../config_stable.cmake to choose between trunk or stable
+file(STRINGS "${CTEST_SCRIPT_DIRECTORY}/../config_stable.cmake" _FULL_CONTENT_STABLE)
+string(TOUPPER "${_FULL_CONTENT_STABLE}" _FULL_CONTENT_STABLE_UP)
+if("${_FULL_CONTENT_STABLE_UP}" MATCHES ".*[^#] *SET *\\( *CONFIG_STABLE_SWITCH +(ON|1|YES|Y|TRUE) *\\).*")
+  set(OTB_PROJECT OTB)
+  include(${CTEST_SCRIPT_DIRECTORY}/../config_stable.cmake)
+  unset(OTB_PROJECT)
+  set(OTB_DIR $ENV{HOME}/Dashboard/build/OTB-stable)
+  set(CTEST_BUILD_NAME "${CTEST_BUILD_NAME}-${OTB_STABLE_VERSION}")
+else()
+  set(OTB_DIR $ENV{HOME}/Dashboard/build/OTB-GDAL_2.0)
+endif()
+
 set(dashboard_root_name "tests")
 set(dashboard_source_name "src/OTB/Documentation/SoftwareGuide")
 set(dashboard_binary_name "build/SoftwareGuide")
@@ -25,7 +38,7 @@ CMAKE_CXX_FLAGS:STRING= -Wall -Wno-cpp
 OTB_DATA_LARGEINPUT_ROOT:STRING=$ENV{HOME}/Data/OTB-LargeInput
 OTB_DATA_ROOT:STRING=$ENV{HOME}/Dashboard/src/OTB-Data
 
-OTB_DIR:STRING=$ENV{HOME}/Dashboard/build/OTB-Release
+OTB_DIR:STRING=${OTB_DIR}
 OTB_SOURCE_DIR:PATH=$ENV{HOME}/Dashboard/src/OTB
 ")
 endmacro()
