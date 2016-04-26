f0c69142abd3af25dfdaa2c74eaf2f28a4273ec3
Rashad Kanavath <rashad.kanavath@c-s.fr>
2016-04-26 17:37:18 +0200

    MXE: locate OTB source directory for Packaging

--- Config/dora/mxe/Monteverdi-MinGW64_MXE.cmake
+++ Config/dora/mxe/Monteverdi-MinGW64_MXE.cmake
@@ -10,4 +10,10 @@ set(dashboard_git_url "https://git@git.orfeo-toolbox.org/git/monteverdi2.git")
 set(dashboard_cc_flags "-Wno-uninitialized -Wno-unused-variable")
 set(dashboard_cxx_flags "-Wno-deprecated -Wno-uninitialized -Wno-unused-variable")
 
+macro(dashboard_hook_init)
+set(dashboard_cache "
+OTB_SOURCE_DIR:PATH=/data/dashboard/nightly/otb-Release/src
+")
+endmacro()
+
 include(${CTEST_SCRIPT_DIRECTORY}/../../mxe_common.cmake)
