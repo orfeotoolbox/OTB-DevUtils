0cda08bd1ea16e2b64193204ce2770bd88eea8ad
Manuel Grizonnet <manuel.grizonnet@cnes.fr>
2016-06-27 08:22:51 +0200

    ENH: activate c++11 on itk trunk

--- Config/pc-christophe/pc-christophe-ITK-stable.cmake
+++ Config/pc-christophe/pc-christophe-ITK-stable.cmake
@@ -26,7 +26,7 @@ execute_process(COMMAND ${CMAKE_COMMAND} -E remove_directory ${ITK_INSTALL_PREFI
 macro(dashboard_hook_init)
   set(dashboard_cache "${dashboard_cache}
 
-CMAKE_CXX_FLAGS:STRING=-fPIC -Wall -Wextra -Wunused-local-typedefs -std=c++11
+CMAKE_CXX_FLAGS:STRING=-fPIC -Wall -Wextra -Wunused-local-typedefs
 CMAKE_C_FLAGS:STRING=-fPIC -Wall -Wextra
 CMAKE_INSTALL_PREFIX=${ITK_INSTALL_PREFIX}
 
