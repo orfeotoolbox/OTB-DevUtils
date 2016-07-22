ee6b0b76c84ad47b5f5da06d51b66d9e470edc6f
Stephane ALBERT <stephane.albert@c-s.fr>
2016-07-22 11:31:00 +0200

    ENH: Updated po9573 custom configuration.

--- Config/po9573/po9573-salbert-Monteverdi2-stable.cmake
+++ Config/po9573/po9573-salbert-Monteverdi2-stable.cmake
@@ -37,7 +37,8 @@ CMAKE_BUILD_TYPE:STRING=${CTEST_BUILD_CONFIGURATION}
 # ITK_DIR:PATH=$ENV{HOME}/local/lib/cmake/ITK-4.8
 ITK_DIR:PATH=$ENV{HOME}/dev/install/ITK-4-debug/lib/cmake/ITK-4.8
 
-OTB_DIR:PATH=$ENV{HOME}/dev/install/OTB${USER_CONFIG}/lib/cmake/OTB-5.6
+# OTB_DIR:PATH=$ENV{HOME}/dev/install/OTB${USER_CONFIG}/lib/cmake/OTB-5.6
+OTB_DIR:PATH=$ENV{HOME}/dev/build/OTB${USER_CONFIG}
 
 # ICE_INCLUDE_DIR:STRING=$ENV{HOME}/dev/install/Ice/include
 # ICE_LIBRARY=$ENV{HOME}/dev/install/Ice/lib/libOTBIce.so
