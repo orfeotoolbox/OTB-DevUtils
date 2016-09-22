21e893a016b9232f494d9214de3c169ca3abeed2
Guillaume Pasero <guillaume.pasero@c-s.fr>
2016-09-22 11:42:22 +0200

    ENH: update CookBook generation with sphinx & rst

--- Config/dora/dora-Nightly-CookBook.cmake
+++ Config/dora/dora-Nightly-CookBook.cmake
@@ -6,16 +6,17 @@ include(${CTEST_SCRIPT_DIRECTORY}/dora_common.cmake)
 
 set(CTEST_DASHBOARD_TRACK Develop)
 
+set(dashboard_otb_source "nightly/OTB-Release/src")
 set(dashboard_otb_binary "nightly/OTB-Release/build")
+set(dashboard_otb_install "nightly/OTB-Release/install")
 
 set(dashboard_root_name "tests")
-set(dashboard_source_name "nightly/OTB-Documents/CookBook")
+set(dashboard_source_name "${dashboard_otb_source}/Documentation/CookBook")
 set(dashboard_binary_name "nightly/CookBook/develop")
 
 #set(dashboard_fresh_source_checkout OFF)
-set(dashboard_git_url "https://git@git.orfeo-toolbox.org/git/otb-documents.git")
-set(dashboard_update_dir ${CTEST_DASHBOARD_ROOT}/nightly/OTB-Documents)
-set(dashboard_git_branch master)
+set(dashboard_git_url "https://git@git.orfeo-toolbox.org/git/otb.git")
+set(dashboard_update_dir ${CTEST_DASHBOARD_ROOT}/${dashboard_otb_source})
 
 macro(dashboard_hook_init)
   set(dashboard_cache "${dashboard_cache}
@@ -26,7 +27,7 @@ CMAKE_BUILD_TYPE:STRING=${CTEST_BUILD_CONFIGURATION}
 OTB_DATA_ROOT:STRING=$ENV{HOME}/Data/OTB-Data
 OTB_DATA_PATHS:STRING=$ENV{HOME}/Data/OTB-Data/Examples::$ENV{HOME}/Data/OTB-Data/Input
 
-OTB_DIR:STRING=${CTEST_DASHBOARD_ROOT}/${dashboard_otb_binary}
+OTB_DIR:STRING=${CTEST_DASHBOARD_ROOT}/${dashboard_otb_install}
 ")
 endmacro()
 
