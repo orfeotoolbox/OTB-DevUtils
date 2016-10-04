22df7971cdf0bea0b379425097c874bbc5e956ed
Guillaume Pasero <guillaume.pasero@c-s.fr>
2016-10-04 08:58:26 +0200

    TEST: default drop method is https

--- Config/otb_common.cmake
+++ Config/otb_common.cmake
@@ -335,7 +335,7 @@ if(NOT EXISTS "${dashboard_update_dir}"
   # CTest delayed initialization is broken, so we put the
   # CTestConfig.cmake info here.
   set(CTEST_NIGHTLY_START_TIME "20:00:00 CEST")
-  set(CTEST_DROP_METHOD "http")
+  set(CTEST_DROP_METHOD "https")
   set(CTEST_DROP_SITE "dash.orfeo-toolbox.org")
   set(CTEST_DROP_LOCATION "/submit.php?project=OTB")
   set(CTEST_DROP_SITE_CDASH TRUE)
