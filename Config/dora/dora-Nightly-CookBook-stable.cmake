26fb1038692a9b37bf3cca4df175b2b6ead6dab6
Guillaume Pasero <guillaume.pasero@c-s.fr>
2016-10-24 11:50:59 +0200

    BUG: fix CookBook generation for 5.8

--- Config/dora/dora-Nightly-CookBook-stable.cmake
+++ Config/dora/dora-Nightly-CookBook-stable.cmake
@@ -10,16 +10,17 @@ set(CTEST_BUILD_NAME "Ubuntu12.04-64bits-CookBook-${dashboard_git_branch}")
 
 set(CTEST_DASHBOARD_TRACK LatestRelease)
 
+set(dashboard_otb_source "nightly/OTB-Release/src")
 set(dashboard_otb_binary "nightly/OTB-Release/build-stable")
+set(dashboard_otb_install "nightly/OTB-Release/install-stable")
 
 set(dashboard_root_name "tests")
-set(dashboard_source_name "nightly/OTB-Documents/CookBook")
+set(dashboard_source_name "${dashboard_otb_source}/Documentation/Cookbook")
 set(dashboard_binary_name "nightly/CookBook/stable")
 
 #set(dashboard_fresh_source_checkout OFF)
-set(dashboard_git_url "https://git@git.orfeo-toolbox.org/git/otb-documents.git")
-set(dashboard_update_dir ${CTEST_DASHBOARD_ROOT}/nightly/OTB-Documents)
-set(dashboard_git_branch master)
+set(dashboard_git_url "https://git@git.orfeo-toolbox.org/git/otb.git")
+set(dashboard_update_dir ${CTEST_DASHBOARD_ROOT}/${dashboard_otb_source})
 
 macro(dashboard_hook_init)
   set(dashboard_cache "${dashboard_cache}
@@ -30,7 +31,7 @@ CMAKE_BUILD_TYPE:STRING=${CTEST_BUILD_CONFIGURATION}
 OTB_DATA_ROOT:STRING=$ENV{HOME}/Data/OTB-Data
 OTB_DATA_PATHS:STRING=$ENV{HOME}/Data/OTB-Data/Examples::$ENV{HOME}/Data/OTB-Data/Input
 
-OTB_DIR:STRING=${CTEST_DASHBOARD_ROOT}/${dashboard_otb_binary}
+OTB_DIR:STRING=${CTEST_DASHBOARD_ROOT}/${dashboard_otb_install}/${OTB_STABLE_DIR_SUFFIX}
 ")
 endmacro()
 
