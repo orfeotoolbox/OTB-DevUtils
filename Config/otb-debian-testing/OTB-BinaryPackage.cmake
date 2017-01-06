bfe557411965041b4e06be23fe4242b1be8391b9
Rashad Kanavath <rashad.kanavath@c-s.fr>
2017-01-06 11:25:37 +0100

    debian-gcc6: add packaging scripts

---
+++ Config/otb-debian-testing/OTB-BinaryPackage.cmake
@@ -0,0 +1,49 @@
+set(dashboard_model Nightly)
+set(CTEST_BUILD_CONFIGURATION Release)
+set(CTEST_DASHBOARD_ROOT "/home/otbval/dashboard")
+set(CTEST_BUILD_NAME "Package-Linux-gcc6-x86_64")
+set(CTEST_BUILD_COMMAND "/usr/bin/make -k -j1 PACKAGE-OTB" )
+set(CTEST_TEST_ARGS PARALLEL_LEVEL 1)
+set(CTEST_SITE "otb-debian-testing.c-s.fr")
+
+set(dashboard_source_name "otb/src/SuperBuild/Packaging")
+set(dashboard_binary_name "otb/pkg-otb")
+set(dashboard_git_url "https://git@git.orfeo-toolbox.org/git/otb.git")
+set(dashboard_update_dir ${CTEST_DASHBOARD_ROOT}/otb/src)
+
+# special setting for ctest_submit(), issue with CA checking
+set(CTEST_CURL_OPTIONS "CURLOPT_SSL_VERIFYPEER_OFF")
+
+# cmake ~/dashboard/otb/src/SuperBuild/Packaging \
+# -DSUPERBUILD_BINARY_DIR=/home/mrashad/dashboard/otb/build \
+# -DDOWNLOAD_LOCATION=/media/otbnas/otb/DataForTests/SuperBuild-archives \
+# -DSB_INSTALL_PREFIX=/home/mrashad/dashboard/otb/install \
+# -DGENERATE_XDK=ON
+
+set(SUPERBUILD_BINARY_DIR ${CTEST_DASHBOARD_ROOT}/otb/build)
+set(SUPERBUILD_INSTALL_DIR ${CTEST_DASHBOARD_ROOT}/otb/install)
+
+macro(dashboard_hook_init)
+set(dashboard_cache "
+CMAKE_INSTALL_PREFIX:PATH=/tmp/install-pkg-otb
+CMAKE_BUILD_TYPE:STRING=${CTEST_BUILD_CONFIGURATION}
+CMAKE_VERBOSE_MAKEFILE:BOOL=OFF
+CTEST_USE_LAUNCHERS:BOOL=${CTEST_USE_LAUNCHERS}
+
+BUILD_TESTING:BOOL=ON
+
+OTB_DATA_ROOT:PATH=/media/otbnas/otb/DataForTests/OTB-Data
+DOWNLOAD_LOCATION:PATH=/media/otbnas/otb/DataForTests/SuperBuild-archives
+SUPERBUILD_BINARY_DIR:PATH=${SUPERBUILD_BINARY_DIR}
+SUPERBUILD_INSTALL_DIR:PATH=${SUPERBUILD_INSTALL_DIR}
+GENERATE_PACKAGE:BOOL=ON
+OTB_WRAP_PYTHON:BOOL=ON
+
+")
+endmacro()
+
+macro(dashboard_hook_submit)
+#  dashboard_copy_packages()
+endmacro() 
+
+include(${CTEST_SCRIPT_DIRECTORY}/../otb_common.cmake)
