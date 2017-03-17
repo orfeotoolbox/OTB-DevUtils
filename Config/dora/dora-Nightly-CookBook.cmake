6ad3826cf2ff6fc75142777ed788f59d6b6d3ac8
Guillaume Pasero <guillaume.pasero@c-s.fr>
2017-03-17 11:50:06 +0100

    ENH: start hook not needed anymore

--- Config/dora/dora-Nightly-CookBook.cmake
+++ Config/dora/dora-Nightly-CookBook.cmake
@@ -31,14 +31,6 @@ CMAKE_PREFIX_PATH:PATH=${CTEST_DASHBOARD_ROOT}/${dashboard_otb_install}
 ")
 endmacro()
 
-macro(dashboard_hook_start)
-  # make sure the source folder exists before calling ctest_start
-  if(NOT EXISTS ${CTEST_DASHBOARD_ROOT}/${dashboard_source_name})
-    execute_process(COMMAND ${CTEST_GIT_UPDATE_CUSTOM}
-                    WORKING_DIRECTORY ${dashboard_update_dir})
-  endif()
-endmacro()
-
 #set(dashboard_no_test 1)
 #set(dashboard_no_submit 1)
 
