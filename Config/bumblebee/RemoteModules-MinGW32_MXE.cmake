96531383292f0e064b3cc65dfbdeaeebe8cae1a7
Guillaume Pasero <guillaume.pasero@c-s.fr>
2017-03-21 10:43:44 +0100

    ADD: automatic remote modules testing for bumblebee

---
+++ Config/bumblebee/RemoteModules-MinGW32_MXE.cmake
@@ -0,0 +1,16 @@
+set(dashboard_model Nightly)
+set(PROJECT otb)
+set(CTEST_BUILD_CONFIGURATION Release)
+set(dashboard_remote_modules 1)
+set(dashboard_no_install 1)
+# filter the list
+set(dashboard_remote_blacklist OTBBioVars OTBPhenology OTBTemporalGapFilling)
+
+set(MXE_TARGET_ARCH "i686")
+
+include(${CTEST_SCRIPT_DIRECTORY}/bumblebee_common.cmake)
+
+include(${CTEST_SCRIPT_DIRECTORY}/../mxe_common.cmake)
+set(dashboard_cache "${mxe_common_cache}")
+
+include(${CTEST_SCRIPT_DIRECTORY}/../otb_common.cmake)
