363c95c81e6f0746d8fbcb758e0a54eccf2c856e
Guillaume Pasero <guillaume.pasero@c-s.fr>
2017-03-21 11:24:53 +0100

    BUG: fix ironhide scripts

--- Config/ironhide/ironhide_common.cmake
+++ Config/ironhide/ironhide_common.cmake
@@ -26,18 +26,7 @@ if (NOT DEFINED CTEST_SITE)
 set(CTEST_SITE "ironhide.c-s.fr" )
 endif()
 
-if (NOT DEFINED CTEST_CMAKE_GENERATOR)
-if (${OTB_ARCH} STREQUAL "x86")
-  #set(CTEST_CMAKE_GENERATOR  "Visual Studio 10" )
-  set(CTEST_CMAKE_GENERATOR "NMake Makefiles")
-  #set(CTEST_CMAKE_GENERATOR "Ninja")
-elseif(${OTB_ARCH} STREQUAL "amd64")
-  #set(CTEST_CMAKE_GENERATOR  "Visual Studio 10 Win64" )
-  set(CTEST_CMAKE_GENERATOR "NMake Makefiles")
-  #set(CTEST_CMAKE_GENERATOR "Ninja")
-endif()
-endif()
-# switch to Ninja builders instead of Visual Studio projects
+set(CTEST_CMAKE_GENERATOR "NMake Makefiles")
 set(CMAKE_MAKE_PROGRAM "nmake")
 
 if (NOT DEFINED CTEST_TEST_ARGS)
@@ -49,7 +38,6 @@ set(CTEST_TEST_TIMEOUT 1500)
 endif()
 
 set(CTEST_GIT_COMMAND $ENV{GIT})
-# set(CTEST_HG_COMMAND "C:/Program Files (x86)/Mercurial/hg.exe")
 
 # build a shorted binary path (OTB needs source and binary paths < 50)
 if (${OTB_ARCH} STREQUAL "x86")
@@ -64,6 +52,10 @@ elseif(${CTEST_BUILD_CONFIGURATION} STREQUAL "Debug")
   set(BUILD_CONF_SHORT "Dbg")
 elseif(${CTEST_BUILD_CONFIGURATION} STREQUAL "RelWithDebInfo")
   set(BUILD_CONF_SHORT "RDI")
+endif()
+
+if(NOT DEFINED CTEST_BUILD_FLAGS)
+  set(CTEST_BUILD_FLAGS "-j2 -k")
 endif()
 
 if (NOT DEFINED dashboard_fresh_source_checkout)
