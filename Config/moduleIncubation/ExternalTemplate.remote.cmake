087a20ec8685ff6a082930f63bd4b75225bbcb31
Manuel Grizonnet <manuel.grizonnet@cnes.fr>
2017-05-31 17:58:15 +0200

    TEST: test remote module template on otb dashboard

---
+++ Config/moduleIncubation/ExternalTemplate.remote.cmake
@@ -0,0 +1,5 @@
+otb_fetch_module(ExternalTemplate
+        "OTB module template"
+        GIT_REPOSITORY https://github.com/orfeotoolbox/otbExternalModuleTemplate.git
+        GIT_TAG master
+        )
