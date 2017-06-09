177e55d6eee8fa49bb8207dce4b2b8132faa6fc1
Manuel Grizonnet <manuel.grizonnet@cnes.fr>
2017-06-09 17:22:49 +0200

    TEST: add dtw in incubation

---
+++ Config/moduleIncubation/DTW.remote.cmake
@@ -0,0 +1,8 @@
+#Contact: Rémi Cresson <remi.cresson@teledetection.fr>
+otb_fetch_module(Mosaic
+  "This module provides Dynamic Time Warping algorithm for remote sensing time
+  series analysis 
+  "
+  GIT_REPOSITORY http://tully.ups-tlse.fr/grizonnet/dtw.git
+  GIT_TAG master
+)
