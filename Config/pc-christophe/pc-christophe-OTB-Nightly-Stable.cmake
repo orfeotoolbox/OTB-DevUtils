aff365e5662c5273365ee4104de8f23cd6f3ec4c
Julien Michel <julien.michel@cnes.fr>
2017-07-21 10:41:24 +0200

    COMP: 6.0 stable still needs the explicit -std=c++11

--- Config/pc-christophe/pc-christophe-OTB-Nightly-Stable.cmake
+++ Config/pc-christophe/pc-christophe-OTB-Nightly-Stable.cmake
@@ -27,7 +27,7 @@ macro(dashboard_hook_init)
 set(dashboard_cache "${dashboard_cache}
 
 CMAKE_C_FLAGS:STRING=-Wall -Wextra
-CMAKE_CXX_FLAGS:STRING=-Wall -Wno-cpp -Wextra -Wno-deprecated-declarations -fopenmp
+CMAKE_CXX_FLAGS:STRING=-Wall -Wno-cpp -Wextra -Wno-deprecated-declarations -fopenmp --std=c++11
 CMAKE_INSTALL_PREFIX:PATH=${CTEST_INSTALL_DIRECTORY}
 
 BUILD_TESTING:BOOL=ON
