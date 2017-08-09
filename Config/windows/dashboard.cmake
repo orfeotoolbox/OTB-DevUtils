5cba21d2c63ba94a8ef2ff5fe2757c7668afd183
Rashad Kanavath <rashad.kanavath@c-s.fr>
2017-08-09 16:12:23 +0200

    Windows: update docs for reconfigurable builds for others
    * Config/windows/dashboard.cmake:

--- Config/windows/dashboard.cmake
+++ Config/windows/dashboard.cmake
@@ -5,37 +5,35 @@
 # set(dashboard_no_test 1)
 # set(dashboard_no_submit 1)
 # set(dashboard_model Experimental)
-
-#packaging build will force dashboard_build_target to 'install'
 # set(dashboard_build_target OTBWavelet-all)
-
 # set(CTEST_BUILD_CONFIGURATION RelWithDebInfo)
 # set(CTEST_CMAKE_GENERATOR "Visual Studio 14 2015") 
 # set(CTEST_CMAKE_GENERATOR "Visual Studio 14 2015 Win64")
 
-# SuperBuild: DONT EVEN THINK OF ACTIVATING
-# ANY SYSTEM LIBRARY AND THEN MAKING OTB PACKAGE!
-# THIS WILL REQUIRE SPECIFIC PATCHING IN GENERATED CMAKE FILES
-# SEE otb.git/Packaging/install_cmake_files.cmake
+# This variable let you get a freaky minimal build of OTB
+# on windows by deactivating all thirdparty libraries 
+# and LARGEINPUT tests
+set(default_on_off ON)
 
-set(otb_data_use_largeinput ON)
+# SEE 'DOCUMENTATION' SECTION BELOW..
 
+set(otb_data_use_largeinput ${default_on_off})
 set(dashboard_cache 
 "
-OTB_USE_OPENGL:BOOL=ON
-OTB_USE_GLEW:BOOL=ON
-OTB_USE_GLFW:BOOL=ON
-OTB_USE_GLUT:BOOL=ON
-OTB_USE_QT4:BOOL=ON
-OTB_USE_QWT:BOOL=ON
-OTB_USE_6S:BOOL=ON
-OTB_USE_CURL:BOOL=ON
-OTB_USE_OPENCV:BOOL=ON
-OTB_USE_LIBSVM:BOOL=ON
-OTB_USE_MUPARSER:BOOL=ON
-OTB_USE_MUPARSERX:BOOL=ON
-OTB_WRAP_PYTHON:BOOL=ON
-OTB_USE_SHARK:BOOL=ON
+OTB_USE_OPENGL:BOOL=${default_on_off}
+OTB_USE_GLEW:BOOL=${default_on_off}
+OTB_USE_GLFW:BOOL=${default_on_off}
+OTB_USE_GLUT:BOOL=${default_on_off}
+OTB_USE_QT4:BOOL=${default_on_off}
+OTB_USE_QWT:BOOL=${default_on_off}
+OTB_USE_6S:BOOL=${default_on_off}
+OTB_USE_CURL:BOOL=${default_on_off}
+OTB_USE_OPENCV:BOOL=${default_on_off}
+OTB_USE_LIBSVM:BOOL=${default_on_off}
+OTB_USE_MUPARSER:BOOL=${default_on_off}
+OTB_USE_MUPARSERX:BOOL=${default_on_off}
+OTB_WRAP_PYTHON:BOOL=${default_on_off}
+OTB_USE_SHARK:BOOL=${default_on_off}
 ")
 
 if("$ENV{dashboard_otb_branch}" STREQUAL "rfc-98-qwt6")
@@ -45,31 +43,42 @@ endif()
 include(windows_common.cmake)
 
 
-############################# NOTES #############################
-# set( dashboard_no_clean 1 )
+######################## DOCUMENTATION ###########################
+
+# SuperBuild: DONT EVEN THINK OF ACTIVATING
+# ANY SYSTEM LIBRARY AND THEN MAKING OTB PACKAGE!
+# THIS WILL REQUIRE SPECIFIC PATCHING IN GENERATED CMAKE FILES
+# SEE otb.git/Packaging/install_cmake_files.cmake
+
 ## Do not clean build and install directory. Default is to clean
+# set( dashboard_no_clean 1 )
 
+## Do not reset otb and otb-data git. This will keep local changes.
 # set( dashboard_no_update 1 )
-## Do not reset otb and otb-data git. This will keep local changes. Default is to delete local changes
 
-# set( dashboard_no_configure 1 )
 ## Do not run ctest_configure step. Default is to run ctest_configure
+# set( dashboard_no_configure 1 )
 
-# set( dashboard_no_submit 1 )
+## Do not run ctest_build step. Default is to run ctest_build
+# set( dashboard_no_build 1 )
+
+## setting this variable will pass BUILD_TESTING:BOOL=OFF
+# set( dashboard_no_test 1 )
+
 ## Do not submit build to dashboard. Default is to submit build
+# set( dashboard_no_submit 1 )
 
+## Force dashboard_model to Experimental.
 # set(dashboard_model Experimental)
-## Force dashboard_model to Experimental. Default value depends on selected branch
 
-# set(dashboard_build_target OTBWavelet-all)
 ## Build target OTBWavelet-all and run only test with label OTBWavelet
+## packaging build will force dashboard_build_target to 'install'
+# set(dashboard_build_target OTBWavelet-all)
 
-# set(CTEST_BUILD_CONFIGURATION RelWithDebInfo)
 ## Change build type to RelWithDebInfo. Default is Release
+# set(CTEST_BUILD_CONFIGURATION RelWithDebInfo)
 
+## Use Visual Studio 14 2015 x86/x64 generator (default is ninja)
 # set(CTEST_CMAKE_GENERATOR "Visual Studio 14 2015") 
-## Use Visual Studio 14 2015 generator x86 (default is ninja)
-
 # set(CTEST_CMAKE_GENERATOR "Visual Studio 14 2015 Win64")
-## Use Visual Studio 14 2015 generator x64 (default is ninja)
 
