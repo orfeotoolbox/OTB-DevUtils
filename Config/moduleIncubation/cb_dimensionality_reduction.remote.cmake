efd6a56d0c735ea26eb39907482cfa2c7df92a08
Guillaume Pasero <guillaume.pasero@c-s.fr>
2017-10-02 17:03:41 +0200

    TEST: remove testing of dimensionality reduction module, not ready yet for dashboard testing.

--- Config/moduleIncubation/cb_dimensionality_reduction.remote.cmake
+++
@@ -1,7 +0,0 @@
-#Contact: Cédric Traizet
-otb_fetch_module(CbDimensionalityReduction
-  "This module contains a new dimensionality reduction framework for the Orfeo Toolbox 
-  "
-  GIT_REPOSITORY http://tully.ups-tlse.fr/traizetc/cb_dimensionality_reduction.git
-  GIT_TAG master
-)
