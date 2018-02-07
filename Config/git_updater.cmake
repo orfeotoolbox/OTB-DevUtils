ce168ef30a537054c5f6f9e161804cc98c5a4960
Guillaume Pasero <guillaume.pasero@c-s.fr>
2018-02-07 16:46:44 +0100

    TEST: support fork/branch as input branch for git_updater

--- Config/git_updater.cmake
+++ Config/git_updater.cmake
@@ -1,6 +1,41 @@
 # script to update a git repository and reset it to a given branch
 
-execute_process(COMMAND ${GIT_COMMAND} fetch)
+# analyse TESTED_BRANCH
+get_filename_component(_BRANCH ${TESTED_BRANCH} NAME)
+get_filename_component(_REMOTE ${TESTED_BRANCH} DIRECTORY)
+if(_REMOTE)
+  if(_REMOTE MATCHES "^[a-z0-9]+\$")
+    # check if the remote is already present
+    execute_process(COMMAND ${GIT_COMMAND} remote
+      OUTPUT_VARIABLE _remotes)
+    string(REPLACE "\n" ";" _remote_list "${_remotes}END")
+    # remove last item (because of trailing newline)
+    list(REMOVE_AT _remote_list -1)
+    message("Remote list : ${_remote_list}")
+    list(FIND _remote_list ${_REMOTE} _is_remote_here)
+    if(_is_remote_here EQUAL -1)
+      # add the remote
+      execute_process(
+        COMMAND ${GIT_COMMAND} remote add ${_REMOTE} https://gitlab.orfeo-toolbox.org/${_REMOTE}/otb.git)
+    endif()
+  else()
+    message("Wrong remote name found : ${_REMOTE}")
+    return()
+  endif()
+else()
+  set(_REMOTE origin)
+endif()
+
+# check if local branch already exists
+execute_process(COMMAND ${GIT_COMMAND} branch --list ${_BRANCH}
+  OUTPUT_VARIABLE _local_branch)
+
+# run the update
+execute_process(COMMAND ${GIT_COMMAND} fetch --all)
 execute_process(COMMAND ${GIT_COMMAND} clean -d -f -f)
-execute_process(COMMAND ${GIT_COMMAND} checkout ${TESTED_BRANCH})
-execute_process(COMMAND ${GIT_COMMAND} reset --hard origin/${TESTED_BRANCH})
+if(_local_branch)
+  execute_process(COMMAND ${GIT_COMMAND} checkout ${_BRANCH})
+  execute_process(COMMAND ${GIT_COMMAND} reset --hard ${_REMOTE}/${_BRANCH})
+else()
+  execute_process(COMMAND ${GIT_COMMAND} checkout -b ${_BRANCH} --track ${_REMOTE}/${_BRANCH})
+endif()
