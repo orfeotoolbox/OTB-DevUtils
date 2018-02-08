115a2f2c49fb01d9065ebfd580fb8edd95a895c4
Guillaume Pasero <guillaume.pasero@c-s.fr>
2018-02-08 12:15:47 +0100

    ENH: support fork/branch on windows

--- Config/windows/nightly.cmake
+++ Config/windows/nightly.cmake
@@ -22,6 +22,8 @@ execute_process(COMMAND ${CMAKE_COMMAND}
   WORKING_DIRECTORY ${DEVUTILS_CONFIG_DIR}/../)
 endif()
 
+# macro common
+include("${DEVUTILS_CONFIG_DIR}/macro_common.cmake")
 
 #unmount and mount otbnas on R drive
 execute_process(COMMAND "net" "use" "R:" "/delete" "/Y")
@@ -34,29 +36,22 @@ include("${DEVUTILS_CONFIG_DIR}/config_stable.cmake")
 if(NOT OTB_STABLE_VERSION)
   message(FATAL_ERROR "OTB_STABLE_VERSION is not set")
 endif()
+set(OTB_STABLE_BRANCH ${dashboard_git_branch})
 
 # Input file where SuperBuild branch name is set
-set(sb_file "${DEVUTILS_CONFIG_DIR}/superbuild_branch.txt")
-set(sb_file_contents)
-if(EXISTS ${sb_file})
-  message("reading superbuild branch name from : ${sb_file}")
-  file(STRINGS ${sb_file} sb_file_contents)
-endif()
-
 set(SUPERBUILD_BRANCH)
 set(SUPERBUILD_DATA_BRANCH)
 if(RELEASE_PREPARATION)
   set(SUPERBUILD_BRANCH "release-${OTB_STABLE_VERSION}")
   set(SUPERBUILD_DATA_BRANCH "release-${OTB_STABLE_VERSION}")
 else()
-  foreach(line_in_sb_file_content ${sb_file_contents})
-    STRING(REPLACE " " ";" branch_input "${line_in_sb_file_content}")
-    list(LENGTH branch_input branch_input_LEN)
-    list(GET branch_input 0 SUPERBUILD_BRANCH)
-    if( branch_input_LEN GREATER 1)
-      list(GET branch_input 1 SUPERBUILD_DATA_BRANCH)
-    endif()
-  endforeach()
+  set(sb_file "${DEVUTILS_CONFIG_DIR}/superbuild_branch.txt")
+  if(EXISTS ${sb_file})
+    message("reading superbuild branch name from : ${sb_file}")
+    parse_branch_list(${sb_file})
+    list(GET _branch_list 0 SUPERBUILD_BRANCH)
+    set(SUPERBUILD_DATA_BRANCH ${specific_data_branch_for_${SUPERBUILD_BRANCH}})
+  endif()
 endif()
 
 if(NOT SUPERBUILD_DATA_BRANCH)
@@ -67,17 +62,15 @@ if(NOT SUPERBUILD_BRANCH)
   set(SUPERBUILD_BRANCH nightly)
 endif()
 
+convert_branch_to_filename(${SUPERBUILD_BRANCH} SUPERBUILD_BRANCH_OK)
+
 message("SUPERBUILD_BRANCH=${SUPERBUILD_BRANCH}")
 message("SUPERBUILD_DATA_BRANCH=${SUPERBUILD_DATA_BRANCH}")
 
 set(LIST_OF_BRANCHES)
-list(APPEND LIST_OF_BRANCHES "nightly nightly")
-list(APPEND LIST_OF_BRANCHES "release-${OTB_STABLE_VERSION} release-${OTB_STABLE_VERSION}")
+list(APPEND LIST_OF_BRANCHES nightly ${OTB_STABLE_BRANCH})
 
 # LIST_OF_BRANCHES is updated with contents from feature_branches.txt
-
-
-
 
 #CAREFUL when you update code below.
 if(NOT DEFINED COMPILER_ARCH)
@@ -111,48 +104,41 @@ endforeach()
 # SuperBuild
 execute_process(COMMAND ${SCRIPTS_DIR}/dashboard.bat 
    ${COMPILER_ARCH} 0 SUPER_BUILD ${SUPERBUILD_BRANCH} ${SUPERBUILD_DATA_BRANCH}
-   OUTPUT_FILE ${LOGS_DIR}/superbuild_${SUPERBUILD_BRANCH}_${COMPILER_ARCH}.txt
-   ERROR_FILE ${LOGS_DIR}/superbuild_${SUPERBUILD_BRANCH}_${COMPILER_ARCH}.txt
+   OUTPUT_FILE ${LOGS_DIR}/superbuild_${SUPERBUILD_BRANCH_OK}_${COMPILER_ARCH}.txt
+   ERROR_FILE ${LOGS_DIR}/superbuild_${SUPERBUILD_BRANCH_OK}_${COMPILER_ARCH}.txt
   WORKING_DIRECTORY ${SCRIPTS_DIR})
 
 # Packaging  
 execute_process(COMMAND ${SCRIPTS_DIR}/dashboard.bat 
    ${COMPILER_ARCH} 0 PKG ${SUPERBUILD_BRANCH} ${SUPERBUILD_DATA_BRANCH}
-   OUTPUT_FILE ${LOGS_DIR}/package_otb_${SUPERBUILD_BRANCH}_${COMPILER_ARCH}.txt
-   ERROR_FILE ${LOGS_DIR}/package_otb_${SUPERBUILD_BRANCH}_${COMPILER_ARCH}.txt
+   OUTPUT_FILE ${LOGS_DIR}/package_otb_${SUPERBUILD_BRANCH_OK}_${COMPILER_ARCH}.txt
+   ERROR_FILE ${LOGS_DIR}/package_otb_${SUPERBUILD_BRANCH_OK}_${COMPILER_ARCH}.txt
   WORKING_DIRECTORY ${SCRIPTS_DIR})
 
 # nightly latest release + Feature Branches
 set(FEATURE_BRANCHES_FILE "${DEVUTILS_CONFIG_DIR}/feature_branches.txt")
 
 message("Checking feature branches file : ${FEATURE_BRANCHES_FILE}")
-file(STRINGS ${FEATURE_BRANCHES_FILE} FEATURE_BRANCHES_FILE_CONTENTS
-REGEX "^ *([a-zA-Z0-9]|-|_|\\.)+ *([a-zA-Z0-9]|-|_|\\.)* *\$")
+parse_branch_list(${FEATURE_BRANCHES_FILE})
+list(APPEND LIST_OF_BRANCHES ${_branch_list})
 
-list(APPEND LIST_OF_BRANCHES ${FEATURE_BRANCHES_FILE_CONTENTS})
-#list(REVERSE LIST_OF_BRANCHES)
-
-foreach(branch_input ${LIST_OF_BRANCHES})
-  set(otb_branch)
-  set(data_branch)
-  string(REGEX REPLACE 
-  "^ *(([a-zA-Z0-9]|-|_|\\.)+) *(([a-zA-Z0-9]|-|_|\\.)*) *\$" "\\1" otb_branch ${branch_input})
- string(REGEX REPLACE 
- "^ *(([a-zA-Z0-9]|-|_|\\.)+) *(([a-zA-Z0-9]|-|_|\\.)*) *\$" "\\3" data_branch ${branch_input})
- 
+foreach(otb_branch ${LIST_OF_BRANCHES})
+  # find the corresponding data branch
+  set(data_branch ${specific_data_branch_for_${otb_branch}})
   if(NOT data_branch)
     set(data_branch "nightly")
   endif()
-  
-  string(STRIP ${data_branch} data_branch)
-  string(STRIP ${otb_branch} otb_branch)
-  
+
+  # convert branch names for filenames
+  convert_branch_to_filename(${otb_branch} otb_branch_clean)
+  convert_branch_to_filename(${data_branch} data_branch_clean)
+
   message("${DATE_TIME}: Bulding otb branch '${otb_branch}' with data branch '${data_branch}'")
-  message("${DATE_TIME}: Output will be logged on ${LOGS_DIR}/${otb_branch}_${COMPILER_ARCH}_${data_branch}.txt")
+  message("${DATE_TIME}: Output will be logged on ${LOGS_DIR}/${otb_branch_clean}_${COMPILER_ARCH}_${data_branch_clean}.txt")
   execute_process(COMMAND ${SCRIPTS_DIR}/dashboard.bat 
   ${COMPILER_ARCH} 0 BUILD ${otb_branch} ${data_branch}
-  OUTPUT_FILE ${LOGS_DIR}/${otb_branch}_${COMPILER_ARCH}_${data_branch}.txt
-  ERROR_FILE ${LOGS_DIR}/${otb_branch}_${COMPILER_ARCH}_${data_branch}.txt
+  OUTPUT_FILE ${LOGS_DIR}/${otb_branch_clean}_${COMPILER_ARCH}_${data_branch_clean}.txt
+  ERROR_FILE ${LOGS_DIR}/${otb_branch_clean}_${COMPILER_ARCH}_${data_branch_clean}.txt
   WORKING_DIRECTORY ${SCRIPTS_DIR})
   
 endforeach()
