a75b4042c3ad306c8ffc6e0ad77fa159da6c5fb9
Manuel Grizonnet <manuel.grizonnet@cnes.fr>
2018-11-05 10:17:34 +0100

    TEST: remove dtw remote module from dash (build issue not fixed)

--- Config/moduleIncubation/DTW.remote.cmake
+++
@@ -1,8 +0,0 @@
-#Contact: Manuel Grizonnet
-otb_fetch_module(DTW
-  "This module provides Dynamic Time Warping algorithm for remote sensing time
-  series analysis 
-  "
-  GIT_REPOSITORY http://tully.ups-tlse.fr/grizonnet/dtw.git
-  GIT_TAG master
-)
