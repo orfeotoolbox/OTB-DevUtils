71ff3d9c1fe470933963b57d56f9c7e2c5ed3901
Guillaume Pasero <guillaume.pasero@c-s.fr>
2018-11-19 17:20:55 +0100

    RM: diapotb module integrated as official remote module

--- Config/moduleIncubation/diapotb.remote.cmake
+++
@@ -1,6 +0,0 @@
-#Contact: Gaëlle USSEGLIO
-otb_fetch_module(DiapOTBModule
-  "OTB module for SAR processing in Diapason."
-  GIT_REPOSITORY https://gitlab.orfeo-toolbox.org/remote_modules/diapotb.git
-  GIT_TAG master
-)
