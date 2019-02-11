a7e9288d762a2900a384a2715b4145e4e1b9a205
Stephane ALBERT <stephane.albert@c-s.fr>
2019-02-11 15:42:22 +0100

    ENH: 1834: Added usage and CLI arg check to watch-test-data.sh

--- Scripts/watch-test-data.sh
+++ Scripts/watch-test-data.sh
@@ -4,4 +4,12 @@ bname=`basename $0`
 ext="log"
 tmp_dir=/tmp
 
+
+if [ $# -lt 1 ]
+then
+    echo "${bname} <dir>..." 1>&2
+    exit 1
+fi
+
+
 inotifywait -mq -e open -r -o ${tmp_dir}/inotify.${ext} $*
