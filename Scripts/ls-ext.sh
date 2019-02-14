17ba8bb16906639d7370018e926ea85936a1b614
Stephane ALBERT <stephane.albert@c-s.fr>
2019-02-14 17:38:48 +0100

    ENH: 1834: Improved ls-ext.sh.

--- Scripts/ls-ext.sh
+++ Scripts/ls-ext.sh
@@ -48,34 +48,34 @@ fi
 
 for dir in $*
 do
-    file `find ${dir} -type f` | grep "ASCII" | cut -f1 -d: |
-	(
-	    while read -r filename
-	    do
-		ext=${filename##*.}
-		if [ ${filename} != ${ext} ]
-		then
-		    echo ${ext}
-		fi
-	    done
-	) | sort -u
-
-    # grep -r -m 1 "^" ${dir} | grep "^ASCII file" | sed -r 's:^ASCII\sfile\s(.*)\smatches:\1:g'  |
+    # file `find ${dir} -type f` | grep "ASCII" | cut -f1 -d: |
     # 	(
     # 	    while read -r filename
     # 	    do
     # 		ext=${filename##*.}
-
-    # 		# if filename has and extension...
     # 		if [ ${filename} != ${ext} ]
     # 		then
-    # 		    # ...Output extension to stdout.
     # 		    echo ${ext}
-    # 		elif [ -n ${verbose} ]
-    # 		then
-    # 		     # ...Output it to stderr for further filtering
-    # 		     echo ${filename} >&2
     # 		fi
     # 	    done
     # 	) | sort -u
+
+    grep -r -m 1 "^" ${dir} | grep "^Binary file" | sed -r 's:^Binary\sfile\s(.*)\smatches:\1:g'  |
+    	(
+    	    while read -r filename
+    	    do
+    		ext=${filename##*.}
+
+    		# if filename has and extension...
+    		if [ ${filename} != ${ext} ]
+    		then
+    		    # ...Output extension to stdout.
+    		    echo ${ext}
+    		# elif [ -n ${verbose} ]
+    		# then
+    		#      # ...Output it to stderr for further filtering
+    		#      echo ${filename} >&2
+    		fi
+    	    done
+    	) | sort -u
 done
